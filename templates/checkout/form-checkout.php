7e40b507b798549b3f4fd6130a5bf3701124adbb
Mark Nelson <therealmarknelson@gmail.com>
2015-04-27 17:18:04 -0600

    html formatting correction on purchase and course page

--- templates/checkout/form-checkout.php
+++ templates/checkout/form-checkout.php
@@ -53,161 +53,163 @@ if($coupon_session) {
 ?>
 
 <form action="" method="post">
-<div class="llms-checkout-wrapper">
-	<div class="llms-checkout">
-	<?php echo  '<h4>' .__( 'Confirm Purchase', 'lifterlms' ) . '</h4>'; ?>
+	<div class="llms-checkout-wrapper">
+		<div class="llms-checkout">
+		<?php echo  '<h4>' .__( 'Confirm Purchase', 'lifterlms' ) . '</h4>'; ?>
 
-	<!-- Product information -->
-	<div class="llms-title-wrapper">
-		<h4 class="llms-title"><?php echo $product->post_title; ?></h4>
-	</div>
-
-	<?php do_action( 'lifterlms_after_checkout_form_title' ); ?>
-
-	<!-- pricing options -->
-	<div class="llms-price-wrapper">
-		<div class="llms-payment-options llms-notice-box">
-			<?php do_action( 'lifterlms_checkout_form_before_payment_options' ); ?>
-			<?php
-			$i = 0;
-			$checked = false;
-			foreach ($payment_options as $key => $value) :
-				if ($value == 'single') :
-				$i++;
-				?>
-					<p class="llms-payment-option llms-option">
-						<input id="llms-payment-option_<?php echo $value; ?>"
-							class="llms-price-option-radio"
-							type="radio"
-							name="payment_option"
-							value="<?php echo $value . '_' . $key; ?>"
-							<?php if ($i == 1) { echo 'checked="checked"'; $checked = 'true'; } ?>
-						/>
-						<label for="llms-payment-option_<?php echo $value; ?>">
-							<span class="llms-radio"></span>
-							<?php
-								echo $single_html_price;
-							?>
-						</label>
-					</p>
-				<?php
-				elseif ($value == 'recurring') :
-					$subs = $product_obj->get_subscriptions();
-
-					if (!empty($subs)) :
-
-						foreach ($subs as $id => $sub) : ?>
-							<p class="llms-payment-option llms-option">
-								<input id="llms-payment-option_<?php echo $value . '_' . $id; ?>"
-									class="llms-price-option-radio"
-									type="radio"
-									name="payment_option"
-									value="<?php echo $value . '_' . $id; ?>"
-									<?php if ($i == 1 && !$checked) { echo 'checked="checked'; } ?>
-								/>
-								<label for="llms-payment-option_<?php echo $value . '_' . $id; ?>">
-									<span class="llms-radio"></span>
-									<?php
-										echo $product_obj->get_subscription_price_html($sub);
-									?>
-								</label>
-							</p>
-						<?php endforeach; ?>
-					<?php endif; ?>
-				<?php else: ?>
-					<?php
-					/**
-					 * Allow addons / plugins / themes to define custom payment options
-					 * This action will be called to allow them to output some custom html for the payment options
-					 */
-					?>
-					<?php do_action( 'lifterlms_checkout_payment_option_'.$value, $product_obj, $value ); ?>
-				<?php endif; ?>
-			<?php endforeach; ?>
-			<?php do_action( 'lifterlms_checkout_form_after_payment_options' ); ?>
+		<!-- Product information -->
+		<div class="llms-title-wrapper">
+			<h4 class="llms-title"><?php echo $product->post_title; ?></h4>
 		</div>
-	</form>
 
-	<!-- Coupon code entry form -->
-	<div class="llms-coupon-entry llms-notice-box">
-		<?php llms_print_notice( $info_message, 'notice' ); ?>
-		<form id="llms-checkout-coupon" method="post" style="display:none">
-			<input type="text" name="coupon_code" class="llms-input-text" placeholder="<?php _e( 'Enter coupon code', 'lifterlms' ); ?>" id="coupon_code" value="" />
-			<div class="llms-clear-box llms-center-content">
-				<input type="submit" class="button llms-button" name="llms_apply_coupon" value="<?php _e( 'Apply Coupon', 'lifterlms' ); ?>" />
-				<input type="hidden" name="product_id" value="<?php echo $product->ID; ?>" />
-			</div>
-			<div class="clear"></div>
-			<?php wp_nonce_field( 'llms-checkout-coupon' ); ?>
-		</form>
-	</div>
+		<?php do_action( 'lifterlms_after_checkout_form_title' ); ?>
 
-	</div>
-	<!-- display the final price -->
-	<div class="llms-final-price-wrapper llms-clear-box">
-		<h3 class="llms-price"><span class="llms-price-label"><?php _e('You Pay:', 'lifterlms'); ?></span><span class="llms-final-price"></span></h3>
-	</div>
-
-	<input type="hidden" name="product_id" value="<?php echo $product->ID; ?>" />
-  	<input type="hidden" name="product_price" value="<?php echo $product_obj->get_price(); ?>" />
-  	<input type="hidden" name="product_sku" value="<?php echo $product_obj->get_sku(); ?>" />
-  	<input type="hidden" name="product_title" value="<?php echo $product->post_title; ?>" />
-
-	<div class="llms-price-wrapper">
-		<div class="llms-payment-methods llms-notice-box">
-
-			<?php
-			if ( $available_gateways = LLMS()->payment_gateways()->get_available_payment_gateways() ) {
-
-				if ( count( $available_gateways ) ) :
-					$ii = 0;
-					current( $available_gateways )->set_current();
-
-					foreach ( $available_gateways as $gateway ) :
-						$ii++;
-					echo ''
-						?>
-						<p class="payment_method_<?php echo $gateway->id; ?> llms-option">
-							<input id="payment_method_<?php echo $gateway->id; ?>"
-								type="radio" class="input-radio"
-								name="payment_method"
-								value="<?php echo $gateway->payment_type; ?>_<?php echo esc_attr( $gateway->id ); ?>"
-								data-payment-type="<?php echo $gateway->payment_type; ?>"
-								<?php if ( ! empty( $_POST['payment_method'] ) ) :
-									 	if ( $_POST['payment_method'] == ($gateway->payment_type . '_' . esc_attr( $gateway->id) ) ) :
-									 		echo 'CHECKED';
-									 	endif;
-								 	elseif ($ii == 1) :
-								 		echo 'CHECKED';
-
-								endif; ?>
+		<!-- pricing options -->
+		<div class="llms-price-wrapper">
+			<div class="llms-payment-options llms-notice-box">
+				<?php do_action( 'lifterlms_checkout_form_before_payment_options' ); ?>
+				<?php
+				$i = 0;
+				$checked = false;
+				foreach ($payment_options as $key => $value) :
+					if ($value == 'single') :
+					$i++;
+					?>
+						<p class="llms-payment-option llms-option">
+							<input id="llms-payment-option_<?php echo $value; ?>"
+								class="llms-price-option-radio"
+								type="radio"
+								name="payment_option"
+								value="<?php echo $value . '_' . $key; ?>"
+								<?php if ($i == 1) { echo 'checked="checked"'; $checked = 'true'; } ?>
 							/>
-							<label for="payment_method_<?php echo $gateway->id; ?>">
+							<label for="llms-payment-option_<?php echo $value; ?>">
 								<span class="llms-radio"></span>
-								<?php echo $gateway->get_title(); ?>
+								<?php
+									echo $single_html_price;
+								?>
 							</label>
 						</p>
+					<?php
+					elseif ($value == 'recurring') :
+						$subs = $product_obj->get_subscriptions();
+
+						if (!empty($subs)) :
+
+							foreach ($subs as $id => $sub) : ?>
+								<p class="llms-payment-option llms-option">
+									<input id="llms-payment-option_<?php echo $value . '_' . $id; ?>"
+										class="llms-price-option-radio"
+										type="radio"
+										name="payment_option"
+										value="<?php echo $value . '_' . $id; ?>"
+										<?php if ($i == 1 && !$checked) { echo 'checked="checked'; } ?>
+									/>
+									<label for="llms-payment-option_<?php echo $value . '_' . $id; ?>">
+										<span class="llms-radio"></span>
+										<?php
+											echo $product_obj->get_subscription_price_html($sub);
+										?>
+									</label>
+								</p>
+							<?php endforeach; ?>
+						<?php endif; ?>
+					<?php else: ?>
 						<?php
-					endforeach;
-				endif;
-			}
-			else {
-				echo '<p>' . __( 'Payment processing is currently disabled.', 'lifterlms' ) . '</p>';
-			}
-			?>
+						/**
+						 * Allow addons / plugins / themes to define custom payment options
+						 * This action will be called to allow them to output some custom html for the payment options
+						 */
+						?>
+						<?php do_action( 'lifterlms_checkout_payment_option_'.$value, $product_obj, $value ); ?>
+					<?php endif; ?>
+				<?php endforeach; ?>
+				<?php do_action( 'lifterlms_checkout_form_after_payment_options' ); ?>
+			</div>
+		</form>
+
+		<!-- Coupon code entry form -->
+		<div class="llms-coupon-entry llms-notice-box">
+			<?php llms_print_notice( $info_message, 'notice' ); ?>
+			<form id="llms-checkout-coupon" method="post" style="display:none">
+				<input type="text" name="coupon_code" class="llms-input-text" placeholder="<?php _e( 'Enter coupon code', 'lifterlms' ); ?>" id="coupon_code" value="" />
+				<div class="llms-clear-box llms-center-content">
+					<input type="submit" class="button llms-button" name="llms_apply_coupon" value="<?php _e( 'Apply Coupon', 'lifterlms' ); ?>" />
+					<input type="hidden" name="product_id" value="<?php echo $product->ID; ?>" />
+				</div>
+				<div class="clear"></div>
+				<?php wp_nonce_field( 'llms-checkout-coupon' ); ?>
+			</form>
 		</div>
 
-		<?php apply_filters('lifterlms_form_checkout_cc', llms_get_template( 'checkout/form-checkout-cc.php', 'lifterlms') ); ?>
-
-		<div class="llms-clear-box llms-center-content">
-			<?php if ( count( $available_gateways ) ) : ?>
-			<input class="llms-button" type="submit" class="button" name="create_order_details" value="<?php _e( 'Buy Now', 'lifterlms' ); ?>" />
-			<?php endif; ?>
+		</div>
+		<!-- display the final price -->
+		<div class="llms-final-price-wrapper llms-clear-box">
+			<h3 class="llms-price"><span class="llms-price-label"><?php _e('You Pay:', 'lifterlms'); ?></span><span class="llms-final-price"></span></h3>
 		</div>
 
-		<?php wp_nonce_field( 'create_order_details' ); ?>
-		<input type="hidden" name="action" value="create_order_details" />
-		<?php do_action( 'lifterlms_after_checkout_form' ); ?>
+		<input type="hidden" name="product_id" value="<?php echo $product->ID; ?>" />
+	  	<input type="hidden" name="product_price" value="<?php echo $product_obj->get_price(); ?>" />
+	  	<input type="hidden" name="product_sku" value="<?php echo $product_obj->get_sku(); ?>" />
+	  	<input type="hidden" name="product_title" value="<?php echo $product->post_title; ?>" />
+
+		<div class="llms-price-wrapper">
+			<div class="llms-payment-methods llms-notice-box">
+
+				<?php
+				if ( $available_gateways = LLMS()->payment_gateways()->get_available_payment_gateways() ) {
+
+					if ( count( $available_gateways ) ) :
+						$ii = 0;
+						current( $available_gateways )->set_current();
+
+						foreach ( $available_gateways as $gateway ) :
+							$ii++;
+						echo ''
+							?>
+							<p class="payment_method_<?php echo $gateway->id; ?> llms-option">
+								<input id="payment_method_<?php echo $gateway->id; ?>"
+									type="radio" class="input-radio"
+									name="payment_method"
+									value="<?php echo $gateway->payment_type; ?>_<?php echo esc_attr( $gateway->id ); ?>"
+									data-payment-type="<?php echo $gateway->payment_type; ?>"
+									<?php if ( ! empty( $_POST['payment_method'] ) ) :
+										 	if ( $_POST['payment_method'] == ($gateway->payment_type . '_' . esc_attr( $gateway->id) ) ) :
+										 		echo 'CHECKED';
+										 	endif;
+									 	elseif ($ii == 1) :
+									 		echo 'CHECKED';
+
+									endif; ?>
+								/>
+								<label for="payment_method_<?php echo $gateway->id; ?>">
+									<span class="llms-radio"></span>
+									<?php echo $gateway->get_title(); ?>
+								</label>
+							</p>
+							<?php
+						endforeach;
+					endif;
+				}
+				else {
+					echo '<p>' . __( 'Payment processing is currently disabled.', 'lifterlms' ) . '</p>';
+				}
+				?>
+			</div>
+
+			<?php apply_filters('lifterlms_form_checkout_cc', llms_get_template( 'checkout/form-checkout-cc.php', 'lifterlms') ); ?>
+
+			<div class="llms-clear-box llms-center-content">
+				<?php if ( count( $available_gateways ) ) : ?>
+				<input class="llms-button" type="submit" class="button" name="create_order_details" value="<?php _e( 'Buy Now', 'lifterlms' ); ?>" />
+				<?php endif; ?>
+			</div>
+
+			<?php wp_nonce_field( 'create_order_details' ); ?>
+			<input type="hidden" name="action" value="create_order_details" />
+			<?php do_action( 'lifterlms_after_checkout_form' ); ?>
+			</div>
 		</div>
 	</div>
 </form>
+
--- templates/course/syllabus.php
+++ templates/course/syllabus.php
@@ -19,184 +19,80 @@ $html .= '<div class="clear"></div>';
 $html .= '<div class="llms-lesson-tooltip"id="lockedTooltip"></div>';
 $html .= '<div class="llms-syllabus-wrapper">';
 
-//get section data
-$sections = $course->get_children_sections();
-if ( !$sections ) {
+	//get section data
+	$sections = $course->get_children_sections();
+	if ( !$sections ) {
 
-	$html .= LLMS_Language::output('This course does not have any sections.');
+		$html .= LLMS_Language::output('This course does not have any sections.');
 
-} else {
+	} else {
 
-	foreach ( $sections as $section_child ) {
-		$section = new LLMS_Section( $section_child->ID);
+		foreach ( $sections as $section_child ) {
+			$section = new LLMS_Section( $section_child->ID);
 
-		$html .= '<h3 class="llms-h3 llms-section-title">' . $section->post->post_title . '</h3>';
+			$html .= '<h3 class="llms-h3 llms-section-title">' . $section->post->post_title . '</h3>';
 
-		//get lesson data
-		$lessons = $section->get_children_lessons();
+			//get lesson data
+			$lessons = $section->get_children_lessons();
 
-		if ( !$lessons ) {
+			if ( !$lessons ) {
 
-			$html .= LLMS_Language::output('This section does not have any lessons.');
+				$html .= LLMS_Language::output('This section does not have any lessons.');
 
-		} else {
+			} else {
 
-			foreach( $lessons as $lesson_child ) {
-				$lesson = new LLMS_Lesson($lesson_child->ID);
+				foreach( $lessons as $lesson_child ) {
+					$lesson = new LLMS_Lesson($lesson_child->ID);
 
-				//determine if lesson is complete to show complete icon
-				if( $lesson->is_complete() ) {
-					$check = '<span class="llms-lesson-complete"><i class="fa fa-check-circle"></i></span>';
-					$complete = ' is-complete';
-				} else {
-					$complete = $check = '';
+					//determine if lesson is complete to show complete icon
+					if( $lesson->is_complete() ) {
+						$check = '<span class="llms-lesson-complete"><i class="fa fa-check-circle"></i></span>';
+						$complete = ' is-complete';
+					} else {
+						$complete = $check = '';
+					}
+
+					//set permalink
+					$permalink = 'javascript:void(0)';
+					$page_restricted = llms_page_restricted($course->id);
+					$title = '';
+					$linkclass = '';
+
+					if ( ! $page_restricted['is_restricted'] ) {
+					 	$permalink = get_permalink( $lesson->id );	
+					 	$linkclass = 'llms-lesson-link';
+					}
+					else {
+						$title = LLMS_Language::output( 'Take this course to unlock this lesson' );
+						$linkclass = 'llms-lesson-link-locked';
+					}			
+
+					$html .= '<div class="llms-lesson-preview' . $complete . '">';
+					
+					$html .= '<a class="' . $linkclass . '" title = "'. $title . '" href="' . $permalink . '">';
+					$html .= $check;
+					$html .= '<div class="lesson-information">';
+					$html .= '<h5 class="llms-h5 llms-lesson-title">' . $lesson->post->post_title . '</h5>';
+					$html .= '<span class="llms-lesson-counter">' . $lesson->get_order() . ' of ' . count($lessons) . '</span>';
+					$html .= '<p class="llms-lesson-excerpt">'.llms_get_excerpt($lesson->id).'</p>';
+					$html .= '</div>';
+					$html .= '</a>';
+
+					$html .= '</div>'; //end lesson-preview
+					
+					//$html .= '</div>';
+
+		
 				}
 
-				//set permalink
-				$permalink = 'javascript:void(0)';
-				$page_restricted = llms_page_restricted($course->id);
-				$title = '';
-				$linkclass = '';
 
-				if ( ! $page_restricted['is_restricted'] ) {
-				 	$permalink = get_permalink( $lesson->id );	
-				 	$linkclass = 'llms-lesson-link';
-				}
-				else {
-					$title = LLMS_Language::output( 'Take this course to unlock this lesson' );
-					$linkclass = 'llms-lesson-link-locked';
-				}			
-
-				$html .= '<div class="llms-lesson-preview' . $complete . '">';
-				
-				$html .= '<a class="' . $linkclass . '" title = "'. $title . '" href="' . $permalink . '">';
-				$html .= $check;
-				$html .= '<div class="lesson-information">';
-				$html .= '<h5 class="llms-h5 llms-lesson-title">' . $lesson->post->post_title . '</h5>';
-				$html .= '<span class="llms-lesson-counter">' . $lesson->get_order() . ' of ' . count($lessons) . '</span>';
-				$html .= '<p class="llms-lesson-excerpt">'.llms_get_excerpt($lesson->id).'</p>';
-				$html .= '</div>';
-				$html .= '</a>';
-
-				$html .= '</div>'; //end lesson-preview
-
-	
 			}
 
 		}
+
 	}
-
-}
+$html .= '<div class="clear"></div>';
+$html .= '</div>';
 echo $html;
-
-
-
-
-
-
-
-
-// $post_id = $post->ID;
-// $course = new LLMS_Course($post->ID);
-// ?>
-<div class="clear"></div>
-<div class="llms-lesson-tooltip"id="lockedTooltip"></div>
-<div class="llms-syllabus-wrapper">
-
-<?php
-
-// $array = array();
-// $lessons = array();
-
-// $syllabus = $course->get_syllabus();
-
-// if ($syllabus ) {
-
-// 	foreach( $syllabus as $key => $value ) :
-
-// 		array_push($array, $syllabus[$key]['section_id']);
-
-// 	endforeach;
-// }
-// else {
-// 	echo __( 'This course does not have any lessons.', 'lifterlms' );
-// }
-
-
-// $the_stuff = get_section_data($array);
-
-// $i = 0;
-// foreach($the_stuff as $key => $value) :
-
-// 	$lessons_array = array();
-
-// 	echo '<h3 class="llms-h3 llms-section-title">' . $value->post_title . '</h3>';
-
-// if($syllabus[$i]['lessons']) {
-
-// 	foreach( $syllabus[$i]['lessons'] as $key => $value) :
-
-// 		array_push($lessons_array, $syllabus[$i]['lessons'][$key]['lesson_id']);
-
-// 	endforeach;
-// }
-
-// 	$the_lessons = get_lesson_data($lessons_array);
-
-
-// 	$lesson_i = 1; // lesson iterator
-// 	$total_lessons = count($the_lessons);
-// 	foreach($the_lessons as $key => $value) :
-
-// 		if ( $value->ID == $post_id ){
-// 			echo $value->post_title;
-// 		}
-// 		else {
-
-// 			$lesson = new LLMS_Lesson($value->ID);
-// 			if( $lesson->is_complete() ) {
-// 				$check = '<span class="llms-lesson-complete"><i class="fa fa-check-circle"></i></span>';
-// 				$complete = ' is-complete';
-// 			} else {
-// 				$complete = $check = '';
-// 			}
-
-// 			//set permalink
-// 			$permalink = 'javascript:void(0)';
-// 			$page_restricted = llms_page_restricted($post_id);
-// 			$title = '';
-// 			$linkclass = '';
-
-// 			if ( ! $page_restricted['is_restricted'] ) {
-// 			 	$permalink = get_permalink( $value->ID );	
-// 			 	$linkclass = 'llms-lesson-link';
-// 			}
-// 			else {
-// 				$title = 'Take this course to unlock this lesson';
-// 				$linkclass = 'llms-lesson-link-locked';
-// 			}			
-
-// 			echo '
-// 				<div class="llms-lesson-preview' . $complete . '">
-// 					<a class="' . $linkclass . '" title = "'. $title . '" href="' . $permalink . '">
-// 						' . $check . '
-// 						<div class="lesson-information">
-// 							<h5 class="llms-h5 llms-lesson-title">' . $value->post_title . '</h5>
-// 							<span class="llms-lesson-counter">' . $lesson_i . ' of ' . $total_lessons . '</span>
-// 							<p class="llms-lesson-excerpt">'.llms_get_excerpt($value->ID).'</p>
-// 						</div>
-// 					</a>
-// 				</div>
-// 			';
-
-// 		}
-// 	$lesson_i++;
-// 	endforeach;
-
-// 	$i++;
-
-// endforeach;
-
-
 ?>
-</div>
+
