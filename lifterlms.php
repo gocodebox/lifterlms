4c5c600cb32184c1999c5bbd2596b0f2b973e925
Mark Nelson <therealmarknelson@gmail.com>
2015-06-05 00:21:21 -0600

    1.2.7 update and release notes

--- lifterlms.php
+++ lifterlms.php
@@ -3,12 +3,12 @@
 * Plugin Name: LifterLMS
 * Plugin URI: http://lifterlms.com/
 * Description: lifterLMS is the easiest way for anyone to create a Learning Management System on the Wordpress platform.
-* Version: 1.2.6
+* Version: 1.2.7
 * Author: codeBOX
 * Author URI: http://gocodebox.com
 *
 * Requires at least: 3.8
-* Tested up to: 4.1
+* Tested up to: 4.2.2
 *
 * @package 		LifterLMS
 * @category 	Core
@@ -29,7 +29,7 @@ if ( ! class_exists( 'LifterLMS') ) :
  */
 final class LifterLMS {
 
-	public $version = '1.2.6';
+	public $version = '1.2.7';
 
 	protected static $_instance = null;
 
