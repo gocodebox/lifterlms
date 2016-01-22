ce8ddf601574186a4d81bbf48b8af1f3314d8b4d
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-01-22 15:18:12 -0800

    major update to wc integration

--- includes/integrations/class.llms.integration.woocommerce.php
+++ includes/integrations/class.llms.integration.woocommerce.php
@@ -19,26 +19,91 @@ class LLMS_Integration_Woocommerce {
 		$this->available = $this->is_available();
 		$this->installed = $this->is_installed();
 
-		$this->enabled = ($this->available && $this->installed) ? true : false;
+		$this->enabled = ( $this->available && $this->installed ) ? true : false;
 
-		if ($this->enabled) {
-			add_action('woocommerce_order_status_completed',array($this,'process_order'));
-			add_action('woocommerce_after_my_account',array($this,'my_courses_content'));
+		if ( $this->enabled ) {
 
-			add_filter( 'lifterlms_registration_redirect',array($this, 'woocommerce_login_redirect'), 10, 1);
+			add_action( 'woocommerce_order_status_completed', array( $this, 'process_order' ) );
+
+			// add llms content templates to the WC my account page template
+			add_action( 'woocommerce_after_my_account', array( $this, 'wc_my_courses_content' ) );
+
+			// redirect LLMS registration & login to the WC Account page
+			add_filter( 'lifterlms_registration_redirect', array( $this, 'llms_login_redirect' ), 10, 1);
+
+			// redirect llms checkout requests to the wc cart
+			add_action( 'wp', array( $this, 'llms_checkout_redirect' ) );
+
 		}
-		
+
 	}
+
+
+
+	/**
+	 * Retrieve a WC Product Instance by
+	 * @param  string $sku  product sku to search by
+	 * @return mixed        false if not found OR a WC_Product instance
+	 */
+	function get_wc_product_by_sku( $sku )
+	{
+
+		global $wpdb;
+
+		$id = $wpdb->get_var( $wpdb->prepare(
+			"SELECT id FROM $wpdb->posts AS p
+			 LEFT JOIN $wpdb->postmeta AS pm ON p.id = pm.post_id
+			 WHERE pm.meta_key = '_sku' AND pm.meta_value = '%s' AND p.post_type = 'product'
+			 LIMIT 1",
+			$sku
+		) );
+
+		if( is_numeric( $id ) ) {
+			return new WC_Product( $id );
+		} else {
+			return false;
+		}
+
+	}
+
+
+	function get_llms_product_by_sku( $sku )
+	{
+
+		$r = new WP_Query( array(
+
+			'meta_query' => array(
+				array(
+					'compare' => '=',
+					'key' => '_sku',
+					'value' => $sku,
+				),
+			),
+			'post_type' => array( 'course', 'llms_membership' ),
+			'posts_per_page' => 1,
+
+		) );
+
+		if( $r->have_posts() ) {
+
+			return $r->posts[0]->ID;
+
+		}
+
+		return false;
+
+	}
+
 
 	/**
 	 * Checks checks if the LLMS WooCommerce integration is enabled
 	 * @return boolean
 	 */
-	public function is_available() {
-		if(get_option('lifterlms_woocommerce_enabled') == 'yes') {
-			return true;
-		}
-		return false;
+	public function is_available()
+	{
+
+		return ( get_option('lifterlms_woocommerce_enabled') == 'yes' ) ? true : false;
+
 	}
 
 
@@ -46,30 +111,87 @@ class LLMS_Integration_Woocommerce {
 	 * Checks if the WooCommerce plugin is installed & activated
 	 * @return boolean
 	 */
-	public function is_installed() {
+	public function is_installed()
+	{
 
-		if(class_exists('WooCommerce')) {
-			return true;
-		}
-		return false;
+		return ( class_exists('WooCommerce') ) ? true : false;
+
 	}
 
-	public function my_courses_content() {
-		llms_get_template( 'myaccount/my-courses.php' );
-		llms_get_template( 'myaccount/my-certificates.php' );
-		llms_get_template( 'myaccount/my-achievements.php' );
-	}
 
 
 
 	/**
-	 * Overrides lifterLMS account login redirect to send users to WooCommerce account page. 
-	 * 
-	 * @param  [type] $redirect_to [url of page to redirect to]
-	 * 
-	 * @return string $redirect_to
+	 * Add some LifterLMS content to the WC My Account Page
+	 * @return void
 	 */
-	function woocommerce_login_redirect($redirect_to) {
+	public function wc_my_courses_content()
+	{
+
+		llms_get_template( 'myaccount/my-courses.php' );
+		llms_get_template( 'myaccount/my-certificates.php' );
+		llms_get_template( 'myaccount/my-achievements.php' );
+
+		if( get_option( 'lifterlms_enable_myaccount_memberships_list', 'no' ) === 'yes' ) {
+
+			llms_get_template( 'myaccount/my-memberships.php' );
+
+		}
+
+	}
+
+
+
+
+
+
+
+	/**
+	 * Redirect applicable traffic to the LifterLMS Checkout page to the WooCommerce cart
+	 *
+	 * 		When a user requests the LifterLMS Checkout page, will locate a WC Product via LifterLMS Product SKU Matching
+	 * 		If a WC Product is found, it will add it to the WC Cart and redirect to the WC cart
+	 *
+	 * @return void
+	 */
+	public function llms_checkout_redirect( )
+	{
+
+		// only run this if we're on the is_llms_checkout page
+		if( !is_llms_checkout() && !is_llms_account_page() ) {
+			return;
+		}
+
+		$type = get_post_type( $_GET['product-id'] );
+
+		$course = new LLMS_Course( $_GET['product-id'] );
+		$sku = $course->get_sku();
+
+		if( $sku ) {
+
+			$product = $this->get_wc_product_by_sku( $sku );
+
+			if( $product ) {
+
+				WC()->cart->add_to_cart( $product->id, 1 );
+
+				wp_redirect( wc_get_page_permalink( 'cart' ) );
+				wp_exit();
+
+			}
+
+		}
+
+	}
+
+	/**
+	 * Overrides LifterLMS account login redirect to send users to WooCommerce account page.
+	 *
+	 * @param  string $redirect_to 	url of page to redirect to
+	 * @return string
+	 */
+	function llms_login_redirect($redirect_to)
+	{
 		$myaccount_page_id = get_option( 'woocommerce_myaccount_page_id' );
 
 		if ( $myaccount_page_id ) {
@@ -81,122 +203,81 @@ class LLMS_Integration_Woocommerce {
 	     return $redirect_to;
 	}
 
+
+
+
+
+
+
+
+
 	/**
 	 * Processes lifterLMS order when WooCommerce order is marked complete
-	 * 
+	 *
 	 * @param  int $order_id [ID of the WooCommerce order]
-	 * 
+	 *
 	 * @return void
 	 */
-	public function process_order($order_id) {
+	public function process_order( $order_id ) {
 		global $post;
 
 		$wc_order = new WC_Order( $order_id );
-		$items = $wc_order->get_items();
+		$user_id = $wc_order->get_user_id();
 
-
-		$order = new stdClass();
-
-		foreach ( $items as $item ) {
-
-			$wc_product_id    		= $item['product_id']; 
-
-			$order->payment_method	= 'woocommerce';
-			$order->product_title	= $item['name']; 
-			$order->product_price	= $item['line_total']; //$_POST['product_price'];
-			
-
-			$order->order_completed = 'yes';
-			$order->total 			= $item['line_total'];
-			$order->currency 		= get_lifterlms_currency();
-
+		// if no user id exists we do nothing. Gotta have a user to assign the course to.
+		if ( empty( $user_id ) ) {
+			return;
 		}
 
-		//get user id
-		$wc_order_meta = get_post_meta($order_id);
-		$order->user_id     		= $wc_order_meta['_customer_user'][0];
-
-		//if no user id exists we do nothing. Gotta have a user to assign the course to. 
-		if (empty($order->user_id)) {
-
-			return false;
-		}
-
-		//get postmeta for product
-		$product = get_post_meta($wc_product_id);
-		$wc_sku = $product['_sku'][0];//$_POST['product_sku'];
-
-		$args = array(
-		  'posts_per_page' => 1,
-		  'post_type' => 'llms_membership',
-		  'meta_query'  => array(
-		    	array(
-		      		'key' => '_sku',
-		      		'value' => $wc_sku,
-		      		'compare' => '='
-	    		)
-	  		)
-		);
-		$memberships = get_posts($args);
-
-		if ($memberships)
+		foreach ( $wc_order->get_items() as $item )
 		{
-			foreach ($memberships as $membership)
-			{
-				$sku = get_post_meta($membership->ID, '_sku', true);
-				$order->product_id 		= $membership->ID;
-			    $order->product_sku 	= $sku;
+
+			$product = new WC_Product( $item['product_id'] );
+			$sku = $product->get_sku();
+
+			$llms_product_id = $this->get_llms_product_by_sku( $sku );
+
+			// continue if no associated llms product
+			if( !$llms_product_id ) {
+				continue;
 			}
-		}
-		else
-		{
-			$args = array(
-			  'posts_per_page' => 1,
-			  'post_type' => 'course',
-			  'meta_query'  => array(
-			    	array(
-			      		'key' => '_sku',
-			      		'value' => $wc_sku,
-			      		'compare' => '='
-		    		)
-		  		)
-			);
-			$courses = get_posts($args);
 
-			foreach ($courses as $course)
-			{
-				$sku = get_post_meta($course->ID, '_sku', true);
-				$order->product_id 		= $course->ID;
-			    $order->product_sku 	= $sku;
-                $order->payment_type    = 'woocommerce';
-                $order->payment_option  = 'single';
+			//check if user is already a student in the course
+			$person = new LLMS_Person;
+			$postmetas = $person->get_user_postmeta_data( $user_id, $llms_product_id );
+
+			$enrolled = false;
+			if ( isset( $postmetas['_status'] ) ) {
+
+				$enrolled = ( $postmetas['_status']->meta_value == 'Enrolled' ) ? true : false;
+
 			}
-		}
 
-		// exit if there is not matching sku
-		if ( empty($order->product_sku) || empty($order->product_id)) {
+			// if user is enrolled, continue
+			if( $enrolled ) {
+				continue;
+			}
 
-			return false;
-		}
+			// create the order
+			$llms_order = new stdClass();
+			$llms_order->user_id         = $user_id;
+			$llms_order->payment_method	 = 'woocommerce';
+			$llms_order->product_title	 = $item['name'];
+			$llms_order->product_price	 = $item['line_total'];
+			$llms_order->order_completed = 'yes';
+			$llms_order->payment_option  = 'single';
+			$llms_order->total 			 = $item['line_total'];
+			$llms_order->currency 		 = $wc_order->get_order_currency();
+			$llms_order->product_id 	 = $llms_product_id;
+			$llms_order->product_sku 	 = $sku;
 
-		//check if user is already a student in the course
-		$user = new LLMS_Person;
-		$user_postmetas = $user->get_user_postmeta_data( $order->user_id, $order->product_id );
 
-		$user_previously_enrolled = false;
-		if ( isset($user_postmetas['_status']) ) {
-			$user_previously_enrolled = true ? $user_postmetas['_status']->meta_value == 'Enrolled' : false;
-	
-		}
+			$llms_checkout = LLMS()->checkout();
+			$llms_checkout->process_order( $llms_order );
+			$llms_checkout->update_order( $llms_order );
 
-		//if there is an sku match and the user isn't already enrolled then create the order
-		if ( ! $user_previously_enrolled ) {
+			do_action( 'lifterlms_order_process_success', $llms_order );
 
-			$lifterlms_checkout = LLMS()->checkout();
-			$lifterlms_checkout->process_order($order);
-			$lifterlms_checkout->update_order($order);
-
-			do_action( 'lifterlms_order_process_success', $order );
 		}
 
 	}
