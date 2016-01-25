f94e4be8d70941bcb4f0228958640bb98c0f04f1
PSmolic <psmolic@gmail.com>
2016-01-25 19:42:33 +0100

    Add multi select to useres search

--- _private/js/llms-metabox-students.js
+++ _private/js/llms-metabox-students.js
@@ -1,6 +1,10 @@
 jQuery(document).ready(function($) {
     jQuery(".add-student-select").select2({
         width: '100%',
+        multiple: true,
+        allowClear: true,
+        maximumSelectionLength: 10,
+        placeholder: "Select a student",
         ajax: {
             url: "admin-ajax.php",
             method: 'POST',
@@ -24,11 +28,14 @@ jQuery(document).ready(function($) {
                 };
             },
             cache: true,
-        }
+        },
     });
 
     jQuery(".remove-student-select").select2({
         width: '100%',
+        multiple: true,
+        maximumSelectionLength: 10,
+        placeholder: "Select a student",
         ajax: {
             url: "admin-ajax.php",
             method: 'POST',
