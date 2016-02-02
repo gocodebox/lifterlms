01c81717453ce67ed3db5192571cc2048f2f806d
PSmolic <psmolic@gmail.com>
2016-02-02 14:21:49 +0100

    Prevent user form saving until he selects course or membership

--- _private/js/llms-metabox-voucher.js
+++ _private/js/llms-metabox-voucher.js
@@ -22,12 +22,12 @@ $(document).ready(function () {
 
                 if(qty > 50) {
                     alert("You can only generate 50 rows at a time");
-                    retrun;
+                    return;
                 }
                 if(codesAddedSinceLastSave > 50) {
                     alert("Please save before adding any more codes, limit is 50 at a time");
                     codesAddedSinceLastSave -= parseInt(qty);
-                    retrun;
+                    return;
                 }
 
                 for (var i = 1; i <= parseInt(qty); i++) {
@@ -57,9 +57,15 @@ $(document).ready(function () {
 
     window.onbeforeunload = function() {
         return changeNotSaved ? "If you leave this page you will lose your unsaved changes." : null;
-    }
+    };
 
     $('input[type=submit]').click(function (e) {
+        //if course or membership is not selected, don't allow user to save
+        if(!($('#_llms_voucher_courses').val() || $('#_llms_voucher_membership').val())) {
+            alert('Please select course or membership before saving.');
+            return false;
+        }
+
         changeNotSaved = false;
     });
 
