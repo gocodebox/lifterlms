b8171d3bf173cfe4b9cc8d4606c22e3896d39eec
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-03-03 16:40:30 -0800

    min support to 4.0

--- lifterlms.php
+++ lifterlms.php
@@ -10,7 +10,7 @@
 * Domain Path: /languages
 * License:     GPLv2
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Requires at least: 3.8
+* Requires at least: 4.0
 * Tested up to: 4.4.2
 *
 * @package 		LifterLMS
