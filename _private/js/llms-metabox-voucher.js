50182e7ce1cf4f669f3066f035e80da61f8cfd06
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-03-17 13:36:05 -0700

    fix undefined variable

--- _private/js/llms-metabox-voucher.js
+++ _private/js/llms-metabox-voucher.js
@@ -40,7 +40,7 @@
                             '<input type="hidden" name="llms_voucher_code_id[]" value="0">' +
                             '</td>' +
                             '<td><span>0 / </span><input type="text" placeholder="Uses" value="' + uses + '" class="llms-voucher-uses" name="llms_voucher_uses[]"></td>' +
-                            '<td><a href="#" class="llms-voucher-delete">' + deleteIcon + '</a></td>' +
+                            '<td><a href="#" class="llms-voucher-delete">' + delete_icon + '</a></td>' +
                             '</tr>';
                     }
                 }
