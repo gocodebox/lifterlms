599ed035f0be21ccb3f1c5929c55c74d57baddee
PSmolic <psmolic@gmail.com>
2016-04-13 15:07:52 +0200

    Move registration code

--- includes/class.llms.person.php
+++ includes/class.llms.person.php
@@ -196,4 +196,134 @@ class LLMS_Person {
 		return $results;
 	}
 
+	public static function create_new_person() {
+
+		if ('no' === get_option( 'lifterlms_registration_generate_username' )) {
+
+			$_username = $_POST['username'];
+
+		} else {
+
+			$_username = '';
+		}
+
+		if ('yes' === get_option( 'lifterlms_registration_require_name' )) {
+			$_firstname = $_POST['firstname'];
+			$_lastname = $_POST['lastname'];
+
+		} else {
+			$_firstname = '';
+			$_lastname = '';
+		}
+
+		if ('yes' === get_option( 'lifterlms_registration_require_address' )) {
+			$_billing_address_1 = $_POST['billing_address_1'];
+			$_billing_address_2 = $_POST['billing_address_2'];
+			$_billing_city = $_POST['billing_city'];
+			$_billing_state = $_POST['billing_state'];
+			$_billing_zip = $_POST['billing_zip'];
+			$_billing_country = $_POST['billing_country'];
+
+		} else {
+			$_billing_address_1 = '';
+			$_billing_address_2 = '';
+			$_billing_city = '';
+			$_billing_state = '';
+			$_billing_zip = '';
+			$_billing_country = '';
+		}
+
+		if ('yes' == get_option( 'lifterlms_registration_add_phone' )) {
+			$_phone = $_POST['phone'];
+		}
+
+		$_password = $_POST['password'];
+		$_password2 = $_POST['password_2'];
+
+		$_agree_to_terms = ( ! empty( $_POST['agree_to_terms'] )) ? true : false;
+
+		try {
+
+			$validation_error = new WP_Error();
+			$validation_error = apply_filters('lifterlms_user_registration_errors',
+					$validation_error,
+					$_username,
+					$_firstname,
+					$_lastname,
+					$_password,
+					$_password2,
+					$_POST['email'],
+					$_billing_address_1,
+					$_billing_city,
+					$_billing_state,
+					$_billing_zip,
+					$_billing_country,
+					$_agree_to_terms
+			);
+
+			if ($validation_error->get_error_code()) {
+
+				throw new Exception( '<strong>' . __( 'Error', 'lifterlms' ) . ':</strong> ' . $validation_error->get_error_message() );
+
+			}
+
+		} catch (Exception $e) {
+
+			llms_add_notice( $e->getMessage(), 'error' );
+			return;
+
+		}
+
+		$username = ! empty( $_username ) ? llms_clean( $_username ) : '';
+		$firstname = ! empty( $_firstname ) ? llms_clean( $_firstname ) : '';
+		$lastname = ! empty( $_lastname ) ? llms_clean( $_lastname ) : '';
+		$email = ! empty( $_POST['email'] ) ? sanitize_email( $_POST['email'] ) : '';
+		if ('yes' === get_option( 'lifterlms_registration_confirm_email' )) {
+			$email2 = ! empty( $_POST['email_confirm'] ) ? sanitize_email( $_POST['email_confirm'] ) : '';
+		} else {
+			$email2 = $email;
+		}
+		$password = $_password;
+		$password2 = $_password2;
+
+		$billing_address_1 = ! empty( $_billing_address_1 ) ? llms_clean( $_billing_address_1 ) : '';
+		$billing_address_2 = ! empty( $_billing_address_2 ) ? llms_clean( $_billing_address_2 ) : '';
+		$billing_city = ! empty( $_billing_city ) ? llms_clean( $_billing_city ) : '';
+		$billing_state = ! empty( $_billing_state ) ? llms_clean( $_billing_state ) : '';
+		$billing_zip = ! empty( $_billing_zip ) ? llms_clean( $_billing_zip ) : '';
+		$billing_country = ! empty( $_billing_country ) ? llms_clean( $_billing_country ) : '';
+		$phone = ! empty( $_phone ) ? llms_clean( $_phone ) : '';
+		$agree_to_terms = $_agree_to_terms;
+
+		// Anti-spam trap
+		if ( ! empty( $_POST['email_2'] )) {
+
+			llms_add_notice( '<strong>' . __( 'ERROR', 'lifterlms' ) . '</strong>: ' . __( 'Anti-spam field was filled in.', 'lifterlms' ), 'error' );
+			return;
+
+		}
+
+		$new_person = llms_create_new_person(
+				$email,
+				$email2,
+				$username,
+				$firstname,
+				$lastname,
+				$password,
+				$password2,
+				$billing_address_1,
+				$billing_address_2,
+				$billing_city,
+				$billing_state,
+				$billing_zip,
+				$billing_country,
+				$agree_to_terms,
+				$phone
+		);
+
+		do_action( 'lifterlms_user_registered', $new_person );
+
+		return $new_person;
+	}
+
 }
