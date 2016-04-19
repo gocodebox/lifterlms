f7f2b042a45eda409a562f552dd6d554900d603f
PSmolic <psmolic@gmail.com>
2016-04-19 12:36:13 +0200

    Add some padding to new part of form

--- templates/checkout/form-login-register.php
+++ templates/checkout/form-login-register.php
@@ -1,10 +1,12 @@
-<div id="llms-login-fields">
-    <?php llms_get_template( 'global/form-login-inner.php' ); ?>
-    <input type="hidden" name="llms-login" value="1">
-    <a href="#" class="llms-toggle" data-parent="llms-login-fields" data-target="llms-register-fields">Register</a>
-</div>
-<div id="llms-register-fields" style="display: none;">
-    <?php llms_get_template( 'global/form-registration-inner.php' ); ?>
-    <input type="hidden" name="llms-registration" value="1">
-    <a href="#" class="llms-toggle" data-parent="llms-register-fields" data-target="llms-login-fields">Login</a>
+<div class="llms-form-wrapper">
+    <div id="llms-login-fields">
+        <?php llms_get_template( 'global/form-login-inner.php' ); ?>
+        <input type="hidden" name="llms-login" value="1">
+        <a href="#" class="llms-toggle" data-parent="llms-login-fields" data-target="llms-register-fields">Register</a>
+    </div>
+    <div id="llms-register-fields" style="display: none;">
+        <?php llms_get_template( 'global/form-registration-inner.php' ); ?>
+        <input type="hidden" name="llms-registration" value="1">
+        <a href="#" class="llms-toggle" data-parent="llms-register-fields" data-target="llms-login-fields">Login</a>
+    </div>
 </div>
