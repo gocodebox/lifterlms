44036698a8eea775b646fdf96c2825bf1b229abb
PSmolic <psmolic@gmail.com>
2016-04-19 12:57:46 +0200

    Login/register language and moving it around

--- templates/checkout/form-login-register.php
+++ templates/checkout/form-login-register.php
@@ -1,12 +1,20 @@
 <div class="llms-form-wrapper">
-    <div id="llms-login-fields">
+    <div id="llms-login-fields" style="display: none;">
+        <div class="llms-notice-box">
+            <?php _e( 'Don\'t have an account?', 'lifterlms'); ?>
+            <a href="#" class="llms-toggle" data-parent="llms-login-fields" data-target="llms-register-fields"><?php _e( 'Sign Up', 'lifterlms'); ?></a>
+        </div>
+
         <?php llms_get_template( 'global/form-login-inner.php' ); ?>
-        <input type="hidden" name="llms-login" value="1">
-        <a href="#" class="llms-toggle" data-parent="llms-login-fields" data-target="llms-register-fields">Register</a>
+        <input type="hidden" disabled name="llms-login" value="1">
     </div>
-    <div id="llms-register-fields" style="display: none;">
+    <div id="llms-register-fields">
+        <div class="llms-notice-box">
+            <?php _e( 'Already registered?', 'lifterlms'); ?>
+            <a href="#" class="llms-toggle" data-parent="llms-register-fields" data-target="llms-login-fields"><?php _e( 'Login', 'lifterlms'); ?></a>
+        </div>
+
         <?php llms_get_template( 'global/form-registration-inner.php' ); ?>
         <input type="hidden" name="llms-registration" value="1">
-        <a href="#" class="llms-toggle" data-parent="llms-register-fields" data-target="llms-login-fields">Login</a>
     </div>
 </div>
