8c20e68266224d8c92b229c745770a1d73ff65fc
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-04-28 14:48:46 -0700

    2.6.2

--- lifterlms.php
+++ lifterlms.php
@@ -3,7 +3,7 @@
 * Plugin Name: LifterLMS
 * Plugin URI: https://lifterlms.com/
 * Description: LifterLMS, the #1 WordPress LMS solution, makes it easy to create, sell, and protect engaging online courses.
-* Version: 2.6.1
+* Version: 2.6.2
 * Author: Mark Nelson, Thomas Patrick Levy, codeBOX, LLC
 * Author URI: http://gocodebox.com
 * Text Domain: lifterlms
