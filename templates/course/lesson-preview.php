7f8f2c0d741c73575d3592211e17261ad8bf7530
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-15 15:30:19 -0700

    unified preview for syllabus and navigation

---
+++ templates/course/lesson-preview.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template for a lesson preview element
+ *
+ *
+ * @author 		LifterLMS
+ * @package 	LifterLMS/Templates
+ * @since       1.0.0
+ * @version     3.0.0 - refactored for sanity's sake
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+?>
+
+<div class="llms-lesson-preview<?php echo $lesson->get_preview_classes(); ?>">
+	<a class="llms-lesson-link" href="<?php echo (  $lesson->is_free() || llms_is_user_enrolled( get_current_user_id(), $lesson->get_parent_course() ) ) ? get_permalink( $lesson->get( 'id' ) ) : '#llms-lesson-locked'; ?>">
+
+		<?php if ( 'course' === get_post_type( get_the_ID() ) ): ?>
+
+			<?php  if ( 'yes' === get_option( 'lifterlms_course_display_outline_lesson_thumbnails', 'no' ) && $thumb = get_the_post_thumbnail( $lesson->get( 'id' ) ) ) : ?>
+				<div class="llms-lesson-thumbnail"><?php echo $thumb; ?></div>
+			<?php endif; ?>
+
+			<aside class="llms-extra">
+				<span class="llms-lesson-counter"><?php printf( _x( '%d of %d', 'lesson order within section', 'lifterlms' ), $lesson->get_order(), $total_lessons ); ?></span>
+				<?php echo $lesson->get_preview_icon_html(); ?>
+			</aside>
+
+		<?php endif; ?>
+
+		<section class="llms-main">
+			<?php if ( 'lesson' === get_post_type( get_the_ID() ) ): ?>
+				<h6 class="llms-pre-text"><?php echo $pre_text; ?></h6>
+			<?php endif; ?>
+			<h5 class="llms-h5 llms-lesson-title"><?php echo get_the_title( $lesson->get( 'id' ) ) ?></h5>
+			<?php if ( llms_get_excerpt( $lesson->get( 'id' ) ) ): ?>
+				<p class="llms-lesson-excerpt"><?php echo llms_get_excerpt( $lesson->get( 'id' ) ); ?></p>
+			<?php endif; ?>
+		</section>
+
+		<div class="clear"></div>
+	</a>
+</div>
