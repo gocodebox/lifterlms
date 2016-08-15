b23765b1f0bbdc5b06824c8e3b1c87db5c39653a
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-15 16:39:21 -0700

    use new lesson api

--- templates/loop/length.php
+++ templates/loop/length.php
@@ -4,6 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) { exit; // Exit if accessed directly
 global $course;
 ?>
 
-<?php if ( $length_html = $course->get_lesson_length() ) : ?>
+<?php if ( $length_html = $course->get( 'length' ) ) : ?>
 	<span class="llms-length"><?php printf( __( 'Course length: %s', 'lifterlms' ), $length_html ); ?></span>
 <?php endif; ?>
