7feaeeecac83348d6591d000ddeb556e7e101141
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-15 16:03:32 -0700

    removed some checkout templates

--- templates/checkout/form-checkout-cc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Checkout Form
- *
- * Credit Card Payment Gateways Should Override this with filter to add thier own forms and validation.
- *
- * @author 		lifterLMS
- * @package 	lifterLMS/Templates
- */
