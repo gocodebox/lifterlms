a2be1e0c84f5b697eccd97278f796253edf7c48d
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-15 16:28:07 -0700

    3.0.0-beta.1

--- _private/js/llms.js
+++ _private/js/llms.js
@@ -34,8 +34,49 @@ var LLMS = window.LLMS || {};
 
 	};
 
+	/**
+	 * our asset enqueue is all screwed up and I'm too tired to fix it
+	 * so we're going to run this little dependency check
+	 * and wait for matchHeight to be available before binding
+	 *
+	 * @param    {Function}  cb  callback function to run when matchheight is ready
+	 * @return   void
+	 * @since    3.0.0
+	 * @version  3.0.0
+	 */
+	LLMS.wait_for_matchHeight = function( cb ) {
+
+		var counter = 0,
+			interval;
+
+		interval = setInterval( function() {
+
+			// if we get to 30 seconds log an error message
+			// and really who cares if the element heights aren't matched
+			if ( counter >= 300 ) {
+
+				console.log( 'cannot match access plan item heights.');
+
+			// if we can't access ye, increment and wait...
+			} else if ( 'undefined' === typeof $.fn.matchHeight ) {
+
+				counter++;
+				return;
+
+			// bind the events, we're good!
+			} else {
+
+				cb();
+
+			}
+
+			clearInterval( interval );
+
+		}, 100 );
+	}
+
 	LLMS.init($);
 
-	
+
 
 })(jQuery);
