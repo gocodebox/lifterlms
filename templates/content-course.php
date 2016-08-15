3f2b3c14fab0be3dc327a2cbe264a79a985cc097
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-15 16:32:18 -0700

    code formatting

--- templates/content-course.php
+++ templates/content-course.php
@@ -30,7 +30,8 @@ $lifterlms_loop['loop']++;
 $classes = array();
 // check if course is complete so we can add a completed class to the link element
 if ( $course->get_percent_complete() == 100) {
-	$classes[] = 'llms-course-complete'; }
+	$classes[] = 'llms-course-complete';
+}
 
 
 if ( 0 == ( $lifterlms_loop['loop'] - 1 ) % $lifterlms_loop['columns'] || 1 == $lifterlms_loop['columns'] ) {
@@ -44,19 +45,11 @@ if ( 0 == $lifterlms_loop['loop'] % $lifterlms_loop['columns'] ) {
 
 	<a class="llms-course-link" href="<?php the_permalink(); ?>">
 
-		<?php
-
-			do_action( 'lifterlms_before_shop_loop_item_title' );
-
-		?>
+		<?php do_action( 'lifterlms_before_shop_loop_item_title' ); ?>
 
 		<h3 class="llms-title"><?php the_title(); ?></h3>
 
-		<?php
-
-			do_action( 'lifterlms_after_shop_loop_item_title' );
-
-		?>
+		<?php do_action( 'lifterlms_after_shop_loop_item_title' ); ?>
 
 	</a>
 
