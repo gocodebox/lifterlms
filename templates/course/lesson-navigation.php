03a982531fe75bbc49c0106e5a72c50852a9edfd
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-15 16:10:17 -0700

    updated to use new lesson preview

--- templates/course/lesson-navigation.php
+++ templates/course/lesson-navigation.php
@@ -5,17 +5,64 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
+
 global $post, $lesson;
 
-if ( ! ($lesson instanceof LLMS_Lesson) ) {
-	$lesson = new LLMS_Lesson( $post->ID ); }
+if ( ! $lesson instanceof LLMS_Lesson ) {
+	$lesson = new LLMS_Lesson( $post->ID );
+}
+
+$prev_id = $lesson->get_previous_lesson();
+$next_id = $lesson->get_next_lesson();
 ?>
 
 <nav class="llms-course-navigation">
 
-<?php
+	<?php if ( $prev_id ): ?>
+
+		<div class="llms-course-nav llms-prev-lesson">
+			<?php llms_get_template( 'course/lesson-preview.php', array(
+				'lesson' => new LLMS_Lesson( $prev_id ),
+				'pre_text' => __( 'Previous Lesson', 'lifterlms' ),
+			) ); ?>
+		</div>
+
+	<?php endif; ?>
+
+	<?php if ( ! $prev_id || ! $next_id ): ?>
+		<div class="llms-course-nav llms-back-to-course">
+			<div class="llms-lesson-preview">
+				<a class="llms-lesson-link" href="<?php echo get_permalink( $lesson->get_parent_course() ); ?>">
+					<section class="llms-main">
+						<h6 class="llms-pre-text"><?php echo __( 'Back to Course', 'lifterlms' ); ?></h6>
+						<h5 class="llms-h5 llms-lesson-title"><?php echo get_the_title( $lesson->get_parent_course() ) ?></h5>
+					</section>
+				</a>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( $next_id ): ?>
+
+		<div class="llms-course-nav llms-next-lesson">
+			<?php llms_get_template( 'course/lesson-preview.php', array(
+				'lesson' => new LLMS_Lesson( $next_id ),
+				'pre_text' => __( 'Next Lesson', 'lifterlms' ),
+			) ); ?>
+		</div>
+
+	<?php endif; ?>
+
+
+
+</nav>
+<div class="clear"></div>
+
+<?php return;
+
+
 if ( $lesson->get_previous_lesson() ) {
-	$previous_lesson_id = $lesson->get_previous_lesson();
+	;
 	$previous_lesson_link = get_permalink( $previous_lesson_id );
 ?>
 
@@ -58,5 +105,4 @@ if ( $lesson->get_next_lesson() ) {
 
 <?php } ?>
 
-</nav>
-<div class="clear"></div>
+
