6108a46e1b6fe30af8c71abc3822533471f68766
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-15 16:24:14 -0700

    need to fix this yet

--- templates/content-single-course-before.php
+++ templates/content-single-course-before.php
@@ -7,8 +7,28 @@
  *
  */
 
+/**
+ * @todo move these notices somewhere else
+ * @var  LLMS_Course
+ */
+$course = new LLMS_Course( get_the_ID() );
+
+if ( 'yes' === $course->get( 'time_period' ) )  {
+	// if the start date hasn't passed yet
+	if ( ! $course->has_date_passed( 'start_date' ) ) {
+
+		llms_add_notice( $course->get( 'course_opens_message' ), 'notice' );
+
+	}
+	// course end date has passed
+	elseif ( $course->has_date_passed( 'end_date' ) ) {
+
+		llms_add_notice( $course->get( 'course_closed_message' ), 'error' );
+
+	}
+}
+
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 llms_print_notices();
 do_action( 'lifterlms_single_course_before_summary' );
-
 ?>
