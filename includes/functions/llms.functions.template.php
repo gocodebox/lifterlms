d2154b9aecd3d841bbeeb7a42e5b2effb1772a80
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-17 13:42:15 -0700

    template overhaul

--- includes/functions/llms.functions.template.php
+++ includes/functions/llms.functions.template.php
@@ -7,26 +7,26 @@
  * @return string [name of file]
  */
 function llms_get_template_part( $slug, $name = '' ) {
-	  $template = '';
+	$template = '';
 
 	if ( $name ) {
-		  $template = locate_template( array( "{$slug}-{$name}.php", LLMS()->template_path() . "{$slug}-{$name}.php" ) );
+		$template = llms_locate_template( "{$slug}-{$name}.php", LLMS()->template_path() . "{$slug}-{$name}.php" );
 	}
 
-	  // Get default slug-name.php
+	// Get default slug-name.php
 	if ( ! $template && $name && file_exists( LLMS()->plugin_path() . "/templates/{$slug}-{$name}.php" ) ) {
-		  $template = LLMS()->plugin_path() . "/templates/{$slug}-{$name}.php";
+		$template = LLMS()->plugin_path() . "/templates/{$slug}-{$name}.php";
 	}
 
 	if ( ! $template ) {
-		  $template = locate_template( array( "{$slug}.php", LLMS()->template_path() . "{$slug}.php" ) );
+		$template = llms_locate_template( "{$slug}.php", LLMS()->template_path() . "{$slug}.php" );
 	}
 
-	  // Allow 3rd party plugin filter template file from their plugin
-	  $template = apply_filters( 'llms_get_template_part', $template, $slug, $name );
+	// Allow 3rd party plugin filter template file from their plugin
+	$template = apply_filters( 'llms_get_template_part', $template, $slug, $name );
 
 	if ( $template ) {
-		  load_template( $template, false );
+		load_template( $template, false );
 	}
 }
 
@@ -42,22 +42,21 @@ function llms_get_template_part_contents( $slug, $name = '' ) {
 	  $template = '';
 
 	if ( $name ) {
-		  $template = locate_template( array( "{$slug}-{$name}.php", LLMS()->template_path() . "{$slug}-{$name}.php" ) );
+		$template = llms_locate_template( "{$slug}-{$name}.php", LLMS()->template_path() . "{$slug}-{$name}.php" );
 	}
 
-	  // Get default slug-name.php
+	// Get default slug-name.php
 	if ( ! $template && $name && file_exists( LLMS()->plugin_path() . "/templates/{$slug}-{$name}.php" ) ) {
-		  $template = LLMS()->plugin_path() . "/templates/{$slug}-{$name}.php";
+		$template = LLMS()->plugin_path() . "/templates/{$slug}-{$name}.php";
 	}
 
 	if ( ! $template ) {
-		  $template = locate_template( array( "{$slug}.php", LLMS()->template_path() . "{$slug}.php" ) );
+		$template = llms_locate_template( "{$slug}.php", LLMS()->template_path() . "{$slug}.php" );
 	}
 
-	  // Allow 3rd party plugin filter template file from their plugin
+	// Allow 3rd party plugin filter template file from their plugin
 	if ( $template ) {
-		  return $template;
-		  //load_template( $template, false );
+		return $template;
 	}
 }
 
@@ -105,31 +104,38 @@ function llms_get_template_ajax( $template_name, $args = array(), $template_path
 /**
  * Locate Template
  *
- * @param  string $template_name [name of template]
- * @param  string $template_path [dir path to template]
- * @param  string $default_path  [default path]
- *
- * @return mixed $template, $template_name, $template_path
+ * @param   string  $template_name name of template
+ * @param   string  $template_path dir path to template
+ * @param   string  $default_path  default path
+ * @return  string
+ * @since   1.0.0
+ * @version 3.0.0 - only returns path if template exists
  */
 function llms_locate_template( $template_name, $template_path = '', $default_path = '' ) {
 	if ( ! $template_path ) {
-		  $template_path = LLMS()->template_path();
+		$template_path = LLMS()->template_path();
 	}
 
 	if ( ! $default_path ) {
-		  $default_path = LLMS()->plugin_path() . '/templates/';
+		$default_path = LLMS()->plugin_path() . '/templates/';
 	}
 
-	  // check theme and template directories for the template
-	  $override_path = llms_get_template_override( $template_name );
+	// check theme and template directories for the template
+	$override_path = llms_get_template_override( $template_name );
 
-	  // Get default template
-	  $path = ($override_path) ? $override_path : $default_path;
+	// Get default template
+	$path = ($override_path) ? $override_path : $default_path;
 
-	  $template = $path . $template_name;
+	$template = $path . $template_name;
 
-	  // Return template
-	  return apply_filters( 'lifterlms_locate_template', $template, $template_name, $template_path );
+	if ( ! file_exists( $template ) ) {
+
+		$template = '';
+
+	}
+
+	// Return template
+	return apply_filters( 'lifterlms_locate_template', $template, $template_name, $template_path );
 }
 
 /**
@@ -140,23 +146,24 @@ function llms_locate_template( $template_name, $template_path = '', $default_pat
  */
 function llms_get_template_override( $template = '' ) {
 
-	  /**
-	   * Allow themes and plugins to determine which folders to look in for theme overrides
-	   */
-	  $dirs = apply_filters( 'lifterlms_theme_override_directories', array(
-			get_stylesheet_directory() . '/lifterlms',
-			get_template_directory() . '/lifterlms',
-	  ) );
+	/**
+	* Allow themes and plugins to determine which folders to look in for theme overrides
+	*/
+	$dirs = apply_filters( 'lifterlms_theme_override_directories', array(
+		get_stylesheet_directory() . '/lifterlms',
+		get_template_directory() . '/lifterlms',
+	) );
 
-	  foreach ( $dirs as $dir ) {
+	foreach ( $dirs as $dir ) {
 
-			$path = $dir . '/';
+		$path = $dir . '/';
 
-		  if ( file_exists( $path . $template ) ) {
-				return $path;
-			}
-
+	 	if ( file_exists( $path . $template ) ) {
+			return $path;
 		}
 
-		return false;
+	}
+
+	return false;
 }
+
