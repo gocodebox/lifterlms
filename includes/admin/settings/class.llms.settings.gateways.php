1d8d1da4f0d424185775b574dda1600f46524aa6
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-17 14:01:31 -0700

    removed paypal settings

--- includes/admin/settings/class.llms.settings.gateways.php
+++ includes/admin/settings/class.llms.settings.gateways.php
@@ -34,69 +34,7 @@ class LLMS_Settings_Gateways extends LLMS_Settings_Page {
 
 			$base_slug = ($gateways_page_id > 0 && get_page( $gateways_page_id )) ? get_page_uri( $gateways_page_id ) : 'gateways';
 
-			return apply_filters( 'lifterlms_gateway_settings', array(
-
-				array( 'type' => 'sectionstart', 'id' => 'gateways_options', 'class' => 'top' ),
-
-				array( 'title' => __( 'Paypal Settings', 'lifterlms' ), 'type' => 'title', 'desc' => __( 'Enable Paypal payment gateway', 'lifterlms' ), 'id' => 'gateway_paypal_options' ),
-
-				array(
-				'desc'          => __( 'Enable Paypal', 'lifterlms' ),
-				'id'            => 'lifterlms_gateway_enable_paypal',
-				'default'       => 'no',
-				'type'          => 'checkbox',
-				'checkboxgroup' => 'start',
-				'autoload'      => false,
-				),
-
-				array(
-				'desc'          => __( 'Enable Sandbox Mode', 'lifterlms' ),
-				'id'            => 'lifterlms_gateways_paypal_enable_sandbox',
-				'default'       => 'no',
-				'type'          => 'checkbox',
-				'checkboxgroup' => '',
-				'autoload'      => false,
-				),
-
-				array(
-					'desc'          => __( 'Enable Debug Mode. <i>Display Response Messages on Checkout Screen</i>', 'lifterlms' ),
-					'id'            => 'lifterlms_gateways_paypal_enable_debug',
-					'default'       => false,
-					'type'          => 'checkbox',
-					'checkboxgroup' => 'end',
-					'autoload'      => false,
-				),
-
-				array(
-				'title' => __( 'API Username', 'lifterlms' ),
-				'desc' 		=> __( 'API Username', 'lifterlms' ),
-				'id' 		=> 'lifterlms_gateways_paypal_email',
-				'type' 		=> 'text',
-				'default'	=> '',
-				'desc_tip'	=> true,
-				),
-
-				array(
-				'title' => __( 'API Password', 'lifterlms' ),
-				'desc' 		=> __( 'API Password', 'lifterlms' ),
-				'id' 		=> 'lifterlms_gateways_paypal_password',
-				'type' 		=> 'text',
-				'default'	=> '',
-				'desc_tip'	=> true,
-				),
-
-				array(
-				'title' => __( 'API Signature', 'lifterlms' ),
-				'desc' 		=> __( 'API Signature', 'lifterlms' ),
-				'id' 		=> 'lifterlms_gateways_paypal_signature',
-				'type' 		=> 'text',
-				'default'	=> '',
-				'desc_tip'	=> true,
-				),
-
-				array( 'type' => 'sectionend', 'id' => 'gateways_options' ),
-				)
-			);
+			return apply_filters( 'lifterlms_gateway_settings', array() );
 	}
 
 	/**
