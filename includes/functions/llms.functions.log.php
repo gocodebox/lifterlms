5904f9ac5bfa2852e03d99c8dff16b5bd055a40a
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-24 10:10:32 -0700

    log functions get their own file

---
+++ includes/functions/llms.functions.log.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Logging & Related Functions
+ *
+ * @since   3.0.0
+ * @version 3.0.0
+ */
+
+
+/**
+ * Retrive the full path to the log file for a given log handle
+ * @param    string  $handle  log handle
+ * @return   string
+ * @since    3.0.0
+ * @version  3.0.0
+ */
+function llms_get_log_path( $handle ) {
+
+	return trailingslashit( LLMS_LOG_DIR ) . $handle . '-' . sanitize_file_name( wp_hash( $handle ) ) . '.log';
+
+}
+
+/**
+ * Log arbitrary messages to a log file
+ * @param  mixed   $message   data to log
+ * @param  string  $handle    allow creation of multiple log files by handle
+ * @return boolean
+ * @since  1.0.0
+ * @version  3.0.0
+ */
+function llms_log( $message, $handle = 'llms' ) {
+
+	$r = false;
+
+	// open the file (creates it if it doesn't already exist)
+	if ( $fh = fopen( llms_get_log_path( $handle ), 'a' ) ) {
+
+		// print array or objects with print_r
+		if ( is_array( $message ) || is_object( $message ) ) {
+			$message = print_r( $message, true );
+		}
+
+		$r = fwrite( $fh, date_i18n( 'm-d-Y @ H:i:s -' ) . ' ' . $message . "\n" );
+
+		fclose( $fh );
+
+	}
+
+	return $r;
+
+}
