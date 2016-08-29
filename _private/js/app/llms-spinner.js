739474af0cb0db3c5d65a9a7320d22d8b59a983c
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-08-29 16:40:31 -0700

    docs

--- _private/js/app/llms-spinner.js
+++ _private/js/app/llms-spinner.js
@@ -1,9 +1,22 @@
 /* global LLMS, $ */
 
+/**
+ * Add Spinners for AJAX events
+ * @since 3.0.0
+ * @version 3.0.0
+ */
 LLMS.Spinner = {
 
-	// default is 40px spinner (pass null for size)
-	// small is 20px spinner (pass "small")
+	/**
+	 * Get an exiting spinner element or create a new one
+	 * @param    obj      $el   jQuery selector of the parent element that should hold and be mased by a spinnner
+	 * @param    string   size  size or the spinner [default|small]
+	 *                          default is 40px
+	 *                          small is 20px
+	 * @return   obj
+	 * @since 3.0.0
+	 * @version 3.0.0
+	 */
 	get: function( $el, size ) {
 
 		// look for an existing spinner
@@ -12,10 +25,10 @@ LLMS.Spinner = {
 		// no spinner inside $el
 		if ( !$spinner.length ) {
 
-			size = ' ' + size || '';
+			size = ( size ) ? size : 'default';
 
 			// create the spinner
-			$spinner = $( '<div class="llms-spinning"><i class="llms-spinner' + size + '"></i></div>' );
+			$spinner = $( '<div class="llms-spinning"><i class="llms-spinner ' + size + '"></i></div>' );
 
 			// add it to the dom
 			$el.append( $spinner );
@@ -27,6 +40,17 @@ LLMS.Spinner = {
 
 	},
 
+	/**
+	 * Start spinner(s) inr=side a given element
+	 * Creates them if they don't exist!
+	 * @param   obj      $el   jQuery selector of the parent element that should hold and be mased by a spinnner
+	 * @param   string   size  size or the spinner [default|small]
+	 *                          default is 40px
+	 *                          small is 20px
+	 * @return  void
+	 * @since   3.0.0
+	 * @version 3.0.0
+	 */
 	start: function( $el, size ) {
 
 		var self = this;
@@ -39,6 +63,13 @@ LLMS.Spinner = {
 
 	},
 
+	/**
+	 * Stor spinners within an element
+	 * @param   obj      $el   jQuery selector of the parent element that should hold and be mased by a spinnner
+	 * @return  void
+	 * @since   3.0.0
+	 * @version 3.0.0
+	 */
 	stop: function( $el ) {
 
 		var self = this;
