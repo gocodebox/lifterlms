5d8067038ce10caa6f2f52d4b4caa71341a0cec3
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-09-16 18:42:32 -0700

    phpcbf

--- templates/content-single-question.php
+++ templates/content-single-question.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 llms_print_notices();
 
 $loop = new WP_Query( array(
-	'post_type' => 'llms_question' ,
+	'post_type' => 'llms_question',
 	'p' => $args['question_id'],
 	'posts_per_page' => 1,
 ) );
