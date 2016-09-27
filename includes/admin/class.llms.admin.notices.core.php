6109538b0e45d24a9b1a5894f63a7cc2660c6cad
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-09-27 15:47:52 -0700

    sidebar support notice

--- includes/admin/class.llms.admin.notices.core.php
+++ includes/admin/class.llms.admin.notices.core.php
@@ -17,7 +17,11 @@ class LLMS_Admin_Notices_Core {
 	 */
 	public function __construct() {
 
-		add_action( 'admin_init', array( $this, 'gateway_notice' ) );
+		add_action( 'admin_init', array( $this, 'gateways' ) );
+
+		// theme sidebar support
+		add_action( 'switch_theme', array( $this, 'sidebar_support' ) );
+		add_action( 'lifterlms_updated', array( $this, 'sidebar_support' ) );
 
 	}
 
@@ -27,7 +31,7 @@ class LLMS_Admin_Notices_Core {
 	 * @since    3.0.0
 	 * @version  3.0.0
 	 */
-	public function gateway_notice() {
+	public function gateways() {
 		$id = 'no-gateways';
 		if ( ! LLMS()->payment_gateways()->has_gateways( true ) ) {
 			$html = __( 'No LifterLMS Payment Gateways are currently enabled. Students will only be able to enroll in courses or memberships with free access plans.', 'lifterlms' ) . '<br><br>';
@@ -40,6 +44,40 @@ class LLMS_Admin_Notices_Core {
 		} elseif ( LLMS_Admin_Notices::has_notice( $id ) ) {
 			LLMS_Admin_Notices::delete_notice( $id );
 		}
+	}
+
+
+	/**
+	 * Check theme support for LifterLMS Sidebars
+	 * @return   void
+	 * @since    3.0.0
+	 * @version  3.0.0
+	 */
+	public function sidebar_support() {
+
+		$id = 'sidebars';
+
+		if ( ! current_theme_supports( 'lifterlms-sidebars' ) && ! in_array( get_option( 'template' ), llms_get_core_supported_themes() ) ) {
+
+			$msg = sprintf(
+				__( '<strong>Your theme does not declare support for LifterLMS Sidebars.</strong> Please see our %sintegration guide%s or check out our %sLaunchPad%s theme which is designed specifically for use with LifterLMS', 'lifterlms' ),
+				'<a href="https://lifterlms.com/docs/lifterlms-sidebar-support/?utm_source=notice&utm_medium=product&utm_content=sidebarsupport&utm_campaign=lifterlmsplugin" target="_blank">', '</a>',
+				'<a href="https://lifterlms.com/product/launchpad/?utm_source=notice&utm_medium=product&utm_content=launchpad&utm_campaign=lifterlmsplugin" target="_blank">', '</a>'
+			);
+
+			LLMS_Admin_Notices::add_notice( $id, $msg, array(
+				'dismissible' => true,
+				'dismiss_for_days' => 365,
+				'remindable' => false,
+				'type' => 'warning',
+			) );
+
+		} elseif ( LLMS_Admin_Notices::has_notice( $id ) ) {
+
+			LLMS_Admin_Notices::delete_notice( $id );
+
+		}
+
 	}
 
 }
