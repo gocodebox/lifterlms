fda02aa6320fc7497e99660790c3f66d7a40c65b
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-10-28 14:57:17 -0700

    remove  global

--- templates/lesson/video.php
+++ templates/lesson/video.php
@@ -2,11 +2,13 @@
 /**
  * Lesson Video embed
  * @since    1.0.0
- * @version  3.1.0
+ * @version  3.1.1
  */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-global $post, $lesson;
+global $post;
+
+$lesson = new LLMS_Lesson( $post );
 
 if ( ! $lesson->get( 'video_embed' ) ) { return; }
 ?>
