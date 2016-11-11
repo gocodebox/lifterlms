4fc23dd0e339af731b2f22410327d69362131fbc
Thomas Patrick Levy <thomasplevy@gmail.com>
2016-11-10 16:46:08 -0800

    js months are 0 indexed, remeber that one next time dumbie

--- _private/js/llms-analytics.js
+++ _private/js/llms-analytics.js
@@ -509,9 +509,7 @@
 			date = parts[0].split( '-' );
 			time = parts[1].split( ':' );
 
-			var r = new Date( date[0], date[1] - 1, date[2], time[0], time[1], time[2] );
-
-			return r;
+			return new Date( date[0], date[1] - 1, date[2], time[0], time[1], time[2] );
 
 		};
 
