6dc1eebabfa3329f17eda74bf6a75705d0321627
Tyler Kemme <tpkemme@Tylers-MacBook-Pro.local>
2016-12-20 09:00:32 -0600

    added button in lifter settings that clears the cache from the students analytics table

--- includes/admin/settings/class.llms.settings.general.php
+++ includes/admin/settings/class.llms.settings.general.php
@@ -146,6 +146,14 @@ class LLMS_Settings_General extends LLMS_Settings_Page {
 				'type' => 'custom-html-no-wrap',
 			),
 
+            array(
+                'desc' => __( 'Clear Students Table cache.  Clear cached data from the students table in the analytics section.', 'lifterlms' ),
+                'name' => 'clear-cache',
+                'title' => __( 'Clear Student Table Cache', 'lifterlms' ),
+                'type' 		=> 'button',
+                'value' => __( 'Clear Cache', 'lifterlms' ),
+            ),
+
 			array(
 				'id' => 'section_tools',
 				'type' => 'sectionend',
@@ -167,6 +175,18 @@ class LLMS_Settings_General extends LLMS_Settings_Page {
 		if ( isset( $_POST['clear-sessions'] ) ) {
 			session_unset();
 		}
+
+		if ( isset( $_POST['clear-cache'] ) ) {
+
+            global $wpdb;
+
+            // Delete all cached student data
+            $wpdb->query( $wpdb->prepare(
+                "DELETE FROM {$wpdb->prefix}usermeta WHERE meta_key = %s or meta_key = %s ",
+                    'llms_overall_progress', 'llms_overall_grade'
+            ) );
+
+        }
 
 		if ( isset( $_POST['reset-tracking'] ) ) {
 			update_option( 'llms_allow_tracking', 'no' );
