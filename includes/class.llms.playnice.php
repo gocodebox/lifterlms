b61e414135b2d59296d0309685332eb12fc86360
Tyler Kemme <tpkemme@Tylers-MacBook-Pro.local>
2016-12-20 04:56:19 -0600

    added a function in class.llms.placenice.php that loads the necessary frontend assets to make lifterlms work with optimizepress

--- includes/class.llms.playnice.php
+++ includes/class.llms.playnice.php
@@ -10,43 +10,77 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 class LLMS_PlayNice {
 
-	/**
-	 * Constructor
-	 * @since    3.1.3
-	 * @version  3.1.3
-	 */
-	public function __construct() {
+    /**
+     * Constructor
+     * @since    3.1.3
+     * @version  3.1.3
+     */
+    public function __construct() {
 
-		add_filter( 'wpseo_premium_post_redirect_slug_change', array( $this, 'wp_seo_premium_redirects' ) );
+        add_filter( 'wpseo_premium_post_redirect_slug_change', array( $this, 'wp_seo_premium_redirects' ) );
 
-	}
+        add_action( 'op_liveeditor_init' , array( $this, 'wp_optimizepress_live_editor' ) );
+    }
 
-	/**
-	 * WP Seo Premium Redirect Manager Conflict
-	 *
-	 * the redirect manager bugs out and creates broken redirects from a course/membership to the homepage
-	 * when an access plan is updated
-	 *
-	 * this prevents that
-	 *
-	 * @param    bool     $bool  default is always false, which means a redirect will be created
-	 * @return   boolean
-	 * @since    3.1.3
-	 * @version  3.1.3
-	 */
-	public function wp_seo_premium_redirects( $bool ) {
+    /**
+     * OptimizePress LiveEditor fix
+     *
+     * The live editor for OptimizePress does not work because it is trying to load a frontend environment
+     * in the admin area and needs access lifterlms frontend files
+     *
+     * This function loads all frontend files when the optimizepress live editor is initalized
+     *
+     * @return void
+     */
+    public function wp_optimizepress_live_editor(){
 
-		$screen = get_current_screen();
+        // These files are necessary to get optimizepress ajax to play nicely in the liveeditor
+        include_once( 'class.llms.ajax.php' );
+        include_once( 'class.llms.ajax.handler.php');
 
-		if ( ! empty( $screen->post_type ) && in_array( $screen->post_type, array( 'course', 'llms_membership' ) ) ) {
+        // These files are all necesarry to get the liveeditor to open
+        include_once( 'llms.template.functions.php' );
+        include_once 'class.llms.https.php';
 
-			$bool = true;
+        include_once( 'class.llms.template.loader.php' );
+        include_once( 'class.llms.frontend.assets.php' );
+        include_once( 'class.llms.frontend.forms.php' );
+        include_once( 'class.llms.frontend.password.php' );
+        include_once( 'class.llms.person.php' );
+        include_once( 'class.llms.shortcodes.php' );
 
-		}
+        include_once( 'shortcodes/class.llms.shortcode.my.account.php' );
+        include_once( 'shortcodes/class.llms.shortcode.checkout.php' );
 
-		return $bool;
 
-	}
+    }
+
+    /**
+     * WP Seo Premium Redirect Manager Conflict
+     *
+     * the redirect manager bugs out and creates broken redirects from a course/membership to the homepage
+     * when an access plan is updated
+     *
+     * this prevents that
+     *
+     * @param    bool     $bool  default is always false, which means a redirect will be created
+     * @return   boolean
+     * @since    3.1.3
+     * @version  3.1.3
+     */
+    public function wp_seo_premium_redirects( $bool ) {
+
+        $screen = get_current_screen();
+
+        if ( ! empty( $screen->post_type ) && in_array( $screen->post_type, array( 'course', 'llms_membership' ) ) ) {
+
+            $bool = true;
+
+        }
+
+        return $bool;
+
+    }
 
 }
 
