b7852714a34d432a292b9903323279cf618feb1a
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-01-25 13:03:32 -0800

    fix during

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -8,17 +8,12 @@ if [ $1 == 'before' ]; then
 
 elif [ $1 == 'during' ]; then
 
-	## Only run on latest stable PHP box (defined in .travis.yml).
-	if [[ ${TRAVIS_PHP_VERSION} == ${PHP_LATEST_STABLE} ]]; then
+	# lint
+	find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l;
 
-		# lint
-		find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l;
+	# phpcs
+	composer run-script phpcs
 
-		# phpcs
-		composer run-script phpcs
-
-		phpunit
-
-	fi
+	phpunit
 
 fi
