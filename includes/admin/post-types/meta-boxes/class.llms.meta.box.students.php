8fc8e8549e380bca3a64cc746e8eda75bd1f0f6c
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-02-09 15:53:27 -0800

    remove unused function

--- includes/admin/post-types/meta-boxes/class.llms.meta.box.students.php
+++ includes/admin/post-types/meta-boxes/class.llms.meta.box.students.php
@@ -2,7 +2,9 @@
 /**
 * Students Metabox for Courses & Memberships
 *
-* Add & remove
+* Add & remove students
+* @since    3.0.0
+* @version  3.4.0
 */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
@@ -37,40 +39,11 @@ class LLMS_Meta_Box_Students extends LLMS_Admin_Metabox {
 		return array();
 	}
 
-	public static function get_students_data( $post, $page = 1 ) {
-
-		$object = false;
-
-		$limit = 20;
-		$skip = ( $page - 1 ) * $limit;
-		$query_limit = $limit + 1; // get an extra result to check if we have more pages
-
-		$students = array(
-			'more' => false,
-			'page' => intval( $page ),
-			'students' => array(),
-		);
-
-		$results = llms_get_enrolled_students( $post->ID, array_keys( llms_get_enrollment_statuses() ), $query_limit, $skip );
-
-		// if we have more results than the true limit, we have another page to grab
-		if ( count( $results ) > $limit ) {
-			$students['more'] = true;
-			// remove the extra result
-			array_pop( $results );
-		}
-
-		$students['students'] = $results;
-
-		return $students;
-
-	}
-
 	/**
 	 * Custom metabox output function
 	 * @return   void
 	 * @since    3.0.0
-	 * @version  3.0.0
+	 * @version  3.4.0
 	 */
 	public function output() {
 
