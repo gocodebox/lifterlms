1113799167fc9aad8a89da2a547524a88218f572
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-03-07 12:36:50 -0800

    better error handling

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -8,6 +8,9 @@ if [ $1 == 'before' ]; then
 
 elif [ $1 == 'during' ]; then
 
+	# ensure that we exit as soon as we encounter an error
+	set -e
+
 	# lint
 	find -L . -path ./vendor -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l;
 
