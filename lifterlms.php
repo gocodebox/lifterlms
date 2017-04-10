1adb16fb61ebbfff024874674e585ce14e7eba54
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-04-10 12:48:55 -0700

    fix voucher export issue & add some docs

--- lifterlms.php
+++ lifterlms.php
@@ -3,7 +3,7 @@
  * Plugin Name: LifterLMS
  * Plugin URI: https://lifterlms.com/
  * Description: LifterLMS, the #1 WordPress LMS solution, makes it easy to create, sell, and protect engaging online courses.
- * Version: 3.6.1
+ * Version: 3.6.2
  * Author: Thomas Patrick Levy, codeBOX LLC
  * Author URI: http://gocodebox.com
  * Text Domain: lifterlms
@@ -35,7 +35,7 @@ require_once 'vendor/autoload.php';
  */
 final class LifterLMS {
 
-	public $version = '3.6.1';
+	public $version = '3.6.2';
 
 	protected static $_instance = null;
 
