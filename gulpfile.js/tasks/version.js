bde784f27ade5fcbd090a38c85e11b6d88485f7e
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-04-27 11:53:49 -0700

    version updater

---
+++ gulpfile.js/tasks/version.js
@@ -0,0 +1,33 @@
+/**
+ * -----------------------------------------------------------
+ * Versioner
+ * -----------------------------------------------------------
+ *
+ * Update placeholder @since & @version docblocks
+ * witn an actual version numbers
+ *
+ * Usage: gulp versioner -V 9.9.9
+ */
+
+var   gulp    = require( 'gulp' )
+	, replace = require( 'gulp-replace' )
+	, argv    = require( 'yargs' ).argv
+	, gutil   = require( 'gulp-util' )
+;
+
+gulp.task( 'versioner', function() {
+
+	var the_version = argv.V;
+
+	if ( ! the_version ) {
+		gutil.log( gutil.colors.red( 'Missing version number. Try `gulp versioner -V 9.9.9`' ) );
+		return;
+	}
+
+	return gulp.src( './includes/notifications/*.php', { base: './' } )
+		.pipe( replace( /(\* @(since|version) +\[version\])/g, function( string ) {
+			return string.replace( '[version]', the_version );
+		} ) )
+		.pipe( gulp.dest( './' ) );
+
+} );
