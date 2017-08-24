f33ac942d59ce9adcdf75bb6f3f19dc1705f34db
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-08-24 14:30:35 -0700

    must be able to view reports to use metabox

--- includes/admin/post-types/meta-boxes/class.llms.meta.box.students.php
+++ includes/admin/post-types/meta-boxes/class.llms.meta.box.students.php
@@ -4,13 +4,19 @@
 *
 * Add & remove students
 * @since    3.0.0
-* @version  3.4.0
+* @version  [version]
 */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 class LLMS_Meta_Box_Students extends LLMS_Admin_Metabox {
 
+	/**
+	 * Capability to check in order to display the metabox to the user
+	 * @var    string
+	 * @since  [version]
+	 */
+	public $capability = 'view_lifterlms_reports';
 
 	/**
 	 * Configure the metabox settings
