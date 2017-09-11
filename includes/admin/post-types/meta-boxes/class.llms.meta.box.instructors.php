bda93944c8d4090eff577303e336d3722abdd843
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-09-11 15:16:51 -0700

    instructors metabox

--- includes/admin/post-types/meta-boxes/class.llms.meta.box.instructors.php
+++ includes/admin/post-types/meta-boxes/class.llms.meta.box.instructors.php
@@ -1,9 +1,9 @@
 <?php
 /**
-* Automation Creation Interface
-* @since   1.0.0
-* @version 1.0.1
-*/
+ * Automation Creation Interface
+ * @since   [version]
+ * @version [version]
+ */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
@@ -12,33 +12,25 @@ class LLMS_Metabox_Instructors extends LLMS_Admin_Metabox {
 	/**
 	 * Configure the metabox
 	 * @return  void
-	 * @since   1.0.0
-	 * @version 1.0.0
+	 * @since   [version]
+	 * @version [version]
 	 */
 	public function configure() {
 
 		$this->id = 'llms-instructors';
 		$this->title = __( 'Instructors', 'lifterlms' );
 		$this->screens = array( 'course', 'llms_membership' );
-		// $this->context = 'normal';
-		// $this->priority = 'high';
-
-		// add_action( 'admin_head', array( $this, 'output_styles' ) );
-		// add_action( 'admin_print_footer_scripts', array( $this, 'output_scripts') );
+		$this->capability = 'publish_courses';
 
 	}
 
 	/**
 	 * Define metabox fields
 	 * @return  array
-	 * @since   1.0.0
-	 * @version 1.0.0
+	 * @since   [version]
+	 * @version [version]
 	 */
 	public function get_fields() {
-
-		// $primary = llms_get_instructor( $this->post->post_author );
-
-		return array();
 
 		return array(
 			array(
@@ -48,24 +40,43 @@ class LLMS_Metabox_Instructors extends LLMS_Admin_Metabox {
 						'button' => array(
 							'text' => __( 'Add Instructor', 'lifterlms' ),
 						),
-						'handler' => 'llms_pa_automation_store',
+						'handler' => 'instructors_mb_store',
 						'header' => array(
 							'default' => __( 'New Instructor', 'lifterlms' ),
 						),
-						'id' => $this->prefix . 'instructors',
+						'id' => $this->prefix . 'instructors_data',
 						'label' => '',
 						'type' => 'repeater',
 						'fields' => array(
 							array(
 								'allow_null' => false,
-								'class' => 'llms-select2',
-								'id' => $this->prefix . 'instructor',
+								'data_attributes' => array(
+									'roles' => 'administrator,lms_manager,instructor,instructors_assistant',
+								),
+								'class' => 'llms-select2-student',
+								'group' => 'd-2of3',
+								'id' => $this->prefix . 'id',
 								'type' => 'select',
-								'label' => __( 'Publication Schedule', 'lifterlms' ),
+								'label' => __( 'Instructor', 'lifterlms' ),
+							),
+							array(
+								'group' => 'd-1of6',
+								'class' => 'input-full',
+								'default' => __( 'Author', 'lifterlms' ),
+								'id' => $this->prefix . 'label',
+								'type' => 'text',
+								'label' => __( 'Label', 'lifterlms' ),
+							),
+							array(
+								'allow_null' => false,
+								'class' => 'llms-select2',
+								'group' => 'd-1of6',
+								'id' => $this->prefix . 'visibility',
+								'type' => 'select',
+								'label' => __( 'Visibility', 'lifterlms' ),
 								'value' => array(
-									'instant' => __( 'Instant (when automation begins)', 'lifterlms' ),
-									'start' => __( '# of days after automation begins', 'lifterlms' ),
-									'date' => __( 'On specific date', 'lifterlms' ),
+									'visible' => esc_html__( 'Visible', 'lifterlms' ),
+									'hidden' => esc_html__( 'Hidden', 'lifterlms' ),
 								),
 							),
 						),
@@ -76,56 +87,14 @@ class LLMS_Metabox_Instructors extends LLMS_Admin_Metabox {
 
 	}
 
-
-	public function output() {
-
-		$roles = array(
-			'author' => esc_html__( 'Author', 'lifterlms' ),
-			'instructor' => esc_html__( 'Instructor', 'lifterlms' ),
-		);
-
-		$author = llms_make_select2_student_array( array( $this->post->post_author ) );
-		$author = array_shift( $author );
-		?>
-
-		<div class="llms-metabox" id="llms-instructors">
-
-			<div class="llms-metabox-section d-all">
-
-				<div class="d-1of3">
-					<label for="post_author_override">
-						<?php _e( 'Primary Instructor', 'lifterlms' ); ?>
-					</label>
-					<select class="llms-select2-student" name="post_author_override" type="select" data-roles="administrator,lms_manager,instructor,instructors_assistant">
-						<option value="<?php echo $author['key']; ?>" selected="selected"><?php echo $author['title']; ?></option>
-					</select>
-				</div>
-
-				<div class="d-1of3">
-					<label for="post_author_role">
-						<?php _e( 'Instructor Role', 'lifterlms' ); ?>
-					</label><br>
-					<select name="post_author_role">
-						<?php foreach ( $roles as $key => $val ) : ?>
-							<option value="<?php echo $key; ?>"><?php echo $val; ?></option>
-						<?php endforeach; ?>
-					</select>
-				</div>
-
-				<div class="d-1of3">
-					<label>
-						<?php _e( 'Visibility', 'lifterlms' ); ?>
-					</label><br>
-					<select name="post_author_visibility">
-						<option value="visible"><?php esc_html_e( 'Visible', 'lifterlms' ); ?></option>
-						<option value="visible"><?php esc_html_e( 'Hidden', 'lifterlms' ); ?></option>
-					</select>
-				</div>
-
-			</div>
-		</div>
-		<?php
-	}
+	/**
+	 * Empty save function prevents repeater field from saving empty meta field
+	 * @param    int     $post  WP_Post ID
+	 * @return   void
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	public function save( $post ) {}
 
 }
 
