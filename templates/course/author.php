d204f3207e900a0cd62e54475f03dd6cdc18bde8
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-09-11 15:26:56 -0700

    author updates

--- templates/course/author.php
+++ templates/course/author.php
@@ -1,17 +1,28 @@
 <?php
 /**
  * LifterLMS Course Author Info
- *
  * @since   3.0.0
- * @version 3.0.0
+ * @version [version]
  */
 if ( ! defined( 'ABSPATH' ) ) { exit; } // End if().
+
+$course = llms_get_post( get_the_ID() );
+$instructors = $course->get_instructors( true );
+if ( ! $instructors ) {
+	return;
+}
 ?>
 
-<h3 class="llms-meta-title"><?php _e( 'Author Information', 'lifterlms' ); ?></h3>
-
-<?php
-echo llms_get_author( array(
-	'avatar_size' => 48,
-	'bio' => true,
-) );
+<section class="llms-instructor-info">
+	<h3 class="llms-meta-title"><?php echo _n( 'Course Instructor', 'Course Instructors', count( $instructors ), 'lifterlms' ); ?></h3>
+	<div class="llms-instructors">
+		<?php foreach ( array_reverse( $instructors ) as $instructor ) : ?>
+			<?php echo llms_get_author( array(
+				'avatar_size' => 100,
+				'bio' => true,
+				'label' => $instructor['label'],
+				'user_id' => $instructor['id'],
+			) ); ?>
+		<?php endforeach ; ?>
+	</div>
+</section>
