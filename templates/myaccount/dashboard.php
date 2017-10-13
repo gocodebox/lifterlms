52f710f16d232849f03ef80f2be8beb1ab64e8c6
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-10-12 17:25:44 -0700

    remove commented out code

--- templates/myaccount/dashboard.php
+++ templates/myaccount/dashboard.php
@@ -30,27 +30,3 @@ llms_print_notices();
 	?>
 
 </div>
-
-<?php return; ?>
-
-
-	<?php do_action( 'lifterlms_before_student_dashboard_tab' ); ?>
-
-	<?php printf( __( '<p>Hello <strong>%1$s</strong></p>', 'lifterlms' ), $current_user->display_name ); ?>
-
-	<?php echo apply_filters( 'lifterlms_account_greeting', '' ); ?>
-
-	<?php do_action( 'lifterlms_after_student_dashboard_greeting' ); ?>
-
-	<?php llms_get_template( 'myaccount/my-courses.php', array(
-		'courses' => $courses,
-		'student' => $student,
-	) ); ?>
-
-	<?php llms_get_template( 'myaccount/my-certificates.php' ); ?>
-
-	<?php llms_get_template( 'myaccount/my-achievements.php' ); ?>
-
-	<?php llms_get_template( 'myaccount/my-memberships.php' ); ?>
-
-	<?php do_action( 'lifterlms_after_student_dashboard_tab' ); ?>
