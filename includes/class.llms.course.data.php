be7ebb6d614fe1db045ea9d0c58c1b97b906ad27
Thomas Patrick Levy <thomasplevy@gmail.com>
2017-11-15 12:35:48 -0800

    better course data functions

--- includes/class.llms.course.data.php
+++ includes/class.llms.course.data.php
@@ -53,12 +53,27 @@ class LLMS_Course_Data {
 		);
 	}
 
+	/**
+	 * Retrieve a start or end date based on the period
+	 * @param    string     $period  period [current|previous]
+	 * @param    string     $date    date type [start|end]
+	 * @return   string
+	 * @since    [version]
+	 * @version  [version]
+	 */
 	private function get_date( $period, $date ) {
 
 		return date( 'Y-m-d H:i:s', $this->dates[ $period ][ $date ] );
 
 	}
 
+	/**
+	 * Set the dates pased on a date range period
+	 * @param    string     $period  date range period
+	 * @return   void
+	 * @since    [version]
+	 * @version  [version]
+	 */
 	public function set_period( $period = 'today' ) {
 
 		$now = current_time( 'timestamp' );
@@ -145,7 +160,13 @@ class LLMS_Course_Data {
 
 	}
 
-
+	/**
+	 * Retrieve # of course completions within the period
+	 * @param    string     $period  date period [current|previous]
+	 * @return   int
+	 * @since    [version]
+	 * @version  [version]
+	 */
 	public function get_completions( $period = 'current' ) {
 
 		global $wpdb;
@@ -165,6 +186,13 @@ class LLMS_Course_Data {
 
 	}
 
+	/**
+	 * retrive # of course enrollments within the period
+	 * @param    string     $period  date period [current|previous]
+	 * @return   int
+	 * @since    [version]
+	 * @version  [version]
+	 */
 	public function get_enrollments( $period = 'current' ) {
 
 		global $wpdb;
@@ -184,6 +212,14 @@ class LLMS_Course_Data {
 
 	}
 
+	/**
+	 * retrive # of engagements related to the course awarded within the period
+	 * @param    string     $type    engagement type [email|certificate|achievement]
+	 * @param    string     $period  date period [current|previous]
+	 * @return   int
+	 * @since    [version]
+	 * @version  [version]
+	 */
 	public function get_engagements( $type, $period = 'current' ) {
 
 		global $wpdb;
@@ -204,6 +240,13 @@ class LLMS_Course_Data {
 
 	}
 
+	/**
+	 * retrive # of lessons completed within the period
+	 * @param    string     $period  date period [current|previous]
+	 * @return   int
+	 * @since    [version]
+	 * @version  [version]
+	 */
 	public function get_lesson_completions( $period = 'current' ) {
 
 		global $wpdb;
@@ -224,6 +267,64 @@ class LLMS_Course_Data {
 
 	}
 
+	/**
+	 * retrive # of orders placed for the course within the period
+	 * @param    string     $period  date period [current|previous]
+	 * @return   int
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	public function get_orders( $period = 'current' ) {
+
+		$query = $this->orders_query( array(
+			array(
+				'after'     => $this->get_date( $period, 'start' ),
+				'before'    => $this->get_date( $period, 'end' ),
+				'inclusive' => true,
+			),
+		), 1 );
+		return $query->found_posts;
+
+	}
+
+	/**
+	 * retrive total amount of transactions related to orders for the course completed within the period
+	 * @param    string     $period  date period [current|previous]
+	 * @return   float
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	public function get_revenue( $period ) {
+
+		$query = $this->orders_query( -1 );
+		$order_ids = wp_list_pluck( $query->posts, 'ID' );
+
+		$revenue = 0;
+
+		if ( $order_ids ) {
+
+			$order_ids = implode( ',', $order_ids );
+
+			global $wpdb;
+			$revenue = $wpdb->get_var( $wpdb->prepare(
+				"SELECT SUM( m2.meta_value )
+				 FROM $wpdb->posts AS p
+				 LEFT JOIN $wpdb->postmeta AS m1 ON m1.post_id = p.ID AND m1.meta_key = '_llms_order_id' -- join for the ID
+				 LEFT JOIN $wpdb->postmeta AS m2 ON m2.post_id = p.ID AND m2.meta_key = '_llms_amount'-- get the actual amounts
+				 WHERE p.post_type = 'llms_transaction'
+				   AND p.post_status = 'llms-txn-succeeded'
+				   AND m1.meta_value IN ({$order_ids})
+				   AND p.post_modified BETWEEN %s AND %s
+				;",
+				$this->get_date( $period, 'start' ),
+				$this->get_date( $period, 'end' )
+			) );
+
+		}
+
+		return apply_filters( 'llms_course_data_get_revenue', $revenue, $period, $this );
+
+	}
 
 	/**
 	 * Retrieve the number of unenrollments on a given date
@@ -252,21 +353,49 @@ class LLMS_Course_Data {
 
 	}
 
+	/**
+	 * Execute a WP Query to retrieve orders within the given date range
+	 * @param    int        $num_orders  number of orders to retrieve
+	 * @param    array      $dates       date range (passed to WP_Query['date_query'])
+	 * @return   obj
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	private function orders_query( $num_orders = 1, $dates = array() ) {
 
+		$args = array(
+			'post_type' => 'llms_order',
+			'post_status' => array( 'llms-active', 'llms-complete' ),
+			'posts_per_page' => $num_orders,
+			'meta_key' => '_llms_product_id',
+			'meta_value' => $this->course_id,
+		);
+
+		if ( $dates ) {
+			$args['date_query'] = $dates;
+		}
+
+		$query = new WP_Query( $args );
+
+		return $query;
+
+	}
+
+	/**
+	 * Retrieve recent LLMS_User_Postmeta for the course
+	 * @return   array
+	 * @since    [version]
+	 * @version  [version]
+	 */
 	public function recent_events() {
 
-		global $wpdb;
+		$query = new LLMS_Query_User_Postmeta( array(
+			'per_page' => 10,
+			'post_id' => $this->course_id,
+			'types' => 'all',
+		) );
 
-		$ids = implode( ',', $this->get_all_ids() );
-
-		return $wpdb->get_results( "
-			SELECT *
-			FROM {$wpdb->prefix}lifterlms_user_postmeta
-			WHERE post_id IN ( {$ids} )
-			  AND meta_key IN ( '_is_complete', '_status' )
-			ORDER BY updated_date DESC
-			LIMIT 10;"
-		);
+		return $query->get_metas();
 
 	}
 
