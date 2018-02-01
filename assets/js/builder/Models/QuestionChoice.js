3e4b92b7dfa22383c602dad2268b1b752c021744
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-01-31 21:23:22 -0800

    fix relationships, cleanup

--- assets/js/builder/Models/QuestionChoice.js
+++ assets/js/builder/Models/QuestionChoice.js
@@ -7,9 +7,13 @@ define( [ 'Models/Image', 'Models/_Relationships' ], function( Image, Relationsh
 
 	return Backbone.Model.extend( _.defaults( {
 
+		/**
+		 * Model relationships
+		 * @type  {Object}
+		 */
 		relationships: {
 			parent: {
-				model: 'question',
+				model: 'llms_question',
 				type: 'model',
 			},
 			children: {
@@ -24,6 +28,12 @@ define( [ 'Models/Image', 'Models/_Relationships' ], function( Image, Relationsh
 			},
 		},
 
+		/**
+		 * Model defaults
+		 * @return   void
+		 * @since    [version]
+		 * @version  [version]
+		 */
 		defaults: function() {
 			return {
 				id: _.uniqueId( 'temp_' ),
@@ -36,11 +46,40 @@ define( [ 'Models/Image', 'Models/_Relationships' ], function( Image, Relationsh
 			}
 		},
 
-		initialize: function() {
+		/**
+		 * Initializer
+		 * @param    obj   data     object of model attributes
+		 * @param    obj   options  additional options
+		 * @return   void
+		 * @since    [version]
+		 * @version  [version]
+		 */
+		initialize: function( data, options ) {
 
 			this.startTracking();
-			this.init_relationships();
+			this.init_relationships( options );
 
+		},
+
+		/**
+		 * Retrieve the choice's parent question
+		 * @return   obj
+		 * @since    [version]
+		 * @version  [version]
+		 */
+		get_parent: function() {
+			return this.collection.parent;
+		},
+
+		/**
+		 * Determine if "selection" is enabled for the question type
+		 * Choice type questions are selectable by reorder type questions are not but still use choices
+		 * @return   {Boolean}
+		 * @since    [version]
+		 * @version  [version]
+		 */
+		is_selectable: function() {
+			return this.get_parent().get( 'question_type' ).get_choice_selectable();
 		},
 
 	}, Relationships ) );
