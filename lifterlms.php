1cd530213d32a18efdc6c83de06a202648985e30
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-02-01 05:28:45 -0800

    3.16.0-rc.1

--- lifterlms.php
+++ lifterlms.php
@@ -3,7 +3,7 @@
  * Plugin Name: LifterLMS
  * Plugin URI: https://lifterlms.com/
  * Description: LifterLMS, the #1 WordPress LMS solution, makes it easy to create, sell, and protect engaging online courses.
- * Version: 3.16.0-alpha.3
+ * Version: 3.16.0-rc.1
  * Author: Thomas Patrick Levy, codeBOX LLC
  * Author URI: https://lifterlms.com/
  * Text Domain: lifterlms
@@ -34,7 +34,7 @@ require_once 'vendor/autoload.php';
  */
 final class LifterLMS {
 
-	public $version = '3.16.0-alpha.3';
+	public $version = '3.16.0-rc.1';
 
 	protected static $_instance = null;
 
