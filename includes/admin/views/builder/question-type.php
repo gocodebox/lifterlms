cafe9319930eec0b1b2959e4f7b93bbc98a9a9f6
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-01-31 21:44:30 -0800

    upsell advanced question types

--- includes/admin/views/builder/question-type.php
+++ includes/admin/views/builder/question-type.php
@@ -7,8 +7,13 @@
 ?>
 <script type="text/html" id="tmpl-llms-question-type-template">
 
-	<button class="llms-element-button small llms-add-question" data-id="{{{ data.id }}}">
-		<i class="fa fa-{{{ data.icon }}}" aria-hidden="true"></i> {{{ data.name }}}
+	<# if ( data.get( 'upgrade' ) ) { #>
+		<a class="llms-type-unavailable tip--top-right" href="{{{ data.get( 'upgrade' ) }}}" data-tip="<?php esc_attr_e( 'Install the LifterLMS Advanced Quizzes add-on to enable this question type', 'lifterlms' ); ?>" target="_blank">
+	<# } #>
+	<button class="llms-element-button small llms-add-question" data-id="{{{ data.get( 'id' ) }}}">
+		<i class="fa fa-{{{ data.get( 'icon' ) }}}" aria-hidden="true"></i> {{{ data.get( 'name' ) }}}
 	</button>
-
+	<# if ( data.get( 'upgrade' ) ) { #>
+		</a>
+	<# } #>
 </script>
