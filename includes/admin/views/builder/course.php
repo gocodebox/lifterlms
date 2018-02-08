34ccdcd047b20e8f11c47507b64b3bc0431cd709
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-02-08 10:27:46 -0800

    make the course icons at builder top static

--- includes/admin/views/builder/course.php
+++ includes/admin/views/builder/course.php
@@ -2,22 +2,22 @@
 /**
  * Builder main course view
  * @since   3.16.0
- * @version 3.16.0
+ * @version [version]
  */
 ?>
 <script type="text/html" id="tmpl-llms-course-template">
 
-	<header class="llms-builder-header">
+	<header class="llms-builder-header llms-course-header">
 
 		<h1 class="llms-headline">
 			<span class="llms-input llms-editable-title" contenteditable="true" data-original-content="{{{ data.get( 'title' ) }}}" type="text">{{{ data.get( 'title' ) }}}</span>
 		</h1>
 
-		<div class="llms-action-icons">
+		<div class="llms-action-icons static">
 			<# if ( data.get_edit_post_link() ) { #>
-				<a class="llms-action-icon" href="{{{ data.get_edit_post_link() }}}"><span class="fa fa-pencil"></span></a>
+				<a class="llms-action-icon tip--bottom-right" data-tip="<?php esc_attr_e( 'WordPress editor', 'lifterlms' ); ?>" href="{{{ data.get_edit_post_link() }}}"><i class="fa fa-pencil"></i></a>
 			<# } #>
-			<a class="llms-action-icon" href="{{{ data.get( 'permalink' ) }}}"><span class="fa fa-external-link"></span></a>
+			<a class="llms-action-icon tip--bottom-right" data-tip="<?php esc_attr_e( 'View course', 'lifterlms' ); ?>" href="{{{ data.get( 'permalink' ) }}}"><i class="fa fa-external-link"></i></a>
 		</div>
 
 	</header>
