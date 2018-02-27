da1f58095bcf78400042a655f2c83baa2e5ab877
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-02-27 15:23:59 -0800

    make trashing and detaching modular

--- assets/js/builder/Views/_Detachable.js
+++ assets/js/builder/Views/_Detachable.js
@@ -1,5 +1,5 @@
 /**
- * Trashable model
+ * Detachable model
  * @type     {Object}
  * @since    [version]
  * @version  [version]
@@ -15,24 +15,24 @@ define( [], function() {
 		 * @version  [version]
 		 */
 		events: {
-			'click a[href="#llms-trash-model"]': 'trash_model',
+			'click a[href="#llms-detach-model"]': 'detach_model',
 		},
 
 		/**
-		 * Remove lesson from course and delete it
+		 * Detaches a model from it's parent (doesn't delete)
 		 * @param    obj   event  js event object
 		 * @return   void
 		 * @since    [version]
 		 * @version  [version]
 		 */
-		trash_model: function( event ) {
+		detach_model: function( event ) {
 
 			if ( event ) {
 				event.preventDefault();
 				event.stopPropagation();
 			}
 
-			var msg = LLMS.l10n.replace( 'Are you sure you want to move this %s to the trash?', {
+			var msg = LLMS.l10n.replace( 'Are you sure you want to detach this %s?', {
 				'%s': this.model.get_l10n_type(),
 			} );
 
@@ -42,8 +42,8 @@ define( [], function() {
 					this.model.collection.remove( this.model );
 				}
 
-				// publish event
-				Backbone.pubSub.trigger( 'model-trashed', this.model );
+				// publish global event
+				Backbone.pubSub.trigger( 'model-detached', this.model );
 
 				// trigger local event so extending views can run other actions where necessary
 				this.trigger( 'model-trashed', this.model );
