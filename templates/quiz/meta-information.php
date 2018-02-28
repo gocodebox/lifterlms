0592bfd8b38d8afc7d3bd12e24c3442eaa9b2498
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-02-28 14:50:31 -0800

    fix copypaste classnames on meta items

--- templates/quiz/meta-information.php
+++ templates/quiz/meta-information.php
@@ -2,7 +2,7 @@
 /**
  * Single Quiz: Meta Information
  * @since    3.9.0
- * @version  3.16.0
+ * @version  [version]
  */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
@@ -17,21 +17,21 @@ $student = llms_get_student();
 <h2 class="llms-quiz-meta-title"><?php _e( 'Quiz Information', 'lifterlms' ); ?></h2>
 <ul class="llms-quiz-meta-info">
 	<?php if ( $passing_percent ) : ?>
-	<li class="llms-quiz-meta-item passing-percent">
+	<li class="llms-quiz-meta-item llms-passing-percent">
 		<?php printf( __( 'Minimum Passing Grade: %s', 'lifterlms' ), '<span class="llms-pass-perc">' . $passing_percent . '%</span>' ); ?>
 	</li>
 	<?php endif; ?>
 
-	<li class="llms-quiz-meta-item passing-percent">
+	<li class="llms-quiz-meta-item llms-attempts">
 		<?php printf( __( 'Remaining Attempts: %s', 'lifterlms' ), '<span class="llms-attempts">' . $student->quizzes()->get_attempts_remaining_for_quiz( $quiz->get( 'id' ) ) . '</span>' ); ?>
 	</li>
 
-	<li class="llms-quiz-meta-item passing-percent">
+	<li class="llms-quiz-meta-item llms-question-count">
 		<?php printf( __( 'Questions: %s', 'lifterlms' ), '<span class="llms-question-count">' . count( $quiz->get_questions( 'ids' ) ) . '</span>' ); ?>
 	</li>
 
 	<?php if ( $quiz->has_time_limit() ) : ?>
-	<li class="llms-quiz-meta-item passing-percent">
+	<li class="llms-quiz-meta-item llms-time-limit">
 		<?php printf( __( 'Time Limit: %s', 'lifterlms' ), '<span class="llms-time-limit">' . $quiz->get_time_limit_string() . '</span>' ); ?>
 	</li>
 	<?php endif; ?>
