59dc3892fed47f95751da510ac87b1e253b96375
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-03-19 15:18:12 -0700

    update versioner to handle frontmatter too

--- gulpfile.js/tasks/version.js
+++ gulpfile.js/tasks/version.js
@@ -15,7 +15,11 @@ var   gulp    = require( 'gulp' )
 	, gutil   = require( 'gulp-util' )
 ;
 
-gulp.task( 'versioner', function() {
+gulp.task( 'versioner', [ 'v-docblocks', 'v-main', 'v-readme' ] );
+
+// Replace @version, @since, & @deprecated [version] placeholders
+// with the submitted version number
+gulp.task( 'v-docblocks', function() {
 
 	var the_version = argv.V;
 
@@ -25,12 +29,46 @@ gulp.task( 'versioner', function() {
 	}
 
 	return gulp.src( [ './lifterlms.php', './includes/**/*.php', './templates/**/*.php', './tests/**/*.php', './_private/**/*.js', './assets/js/builder/**/*.js'  ], { base: './' } )
-		.pipe( replace( /(\* @(since|version) +\[version\])/g, function( string ) {
+		.pipe( replace( /(\* @(since|version|deprecated) +\[version\])/g, function( string ) {
 			return string.replace( '[version]', the_version );
 		} ) )
-		.pipe( replace( /(\* @(since|version) +\[version\])/g, function( string ) {
-			console.log( string );
-			return string.replace( '[version]', the_version );
+		.pipe( gulp.dest( './' ) );
+
+} );
+
+// replace main plugin file Version declaration as well as the public $version variable
+gulp.task( 'v-main', function() {
+
+	var the_version = argv.V;
+
+	if ( ! the_version ) {
+		gutil.log( gutil.colors.red( 'Missing version number. Try `gulp versioner -V 9.9.9`' ) );
+		return;
+	}
+
+	return gulp.src( [ './lifterlms.php'  ], { base: './' } )
+		.pipe( replace( / \* Version: (\d+\.\d+\.\d+)/g, function( match, p1, offset, string ) {
+			return match.replace( p1, the_version );
+		} ) )
+		.pipe( replace( /	public \$version = '(\d+\.\d+\.\d+)';/g, function( match, p1, offset, string ) {
+			return match.replace( p1, the_version );
+		} ) )
+		.pipe( gulp.dest( './' ) );
+
+} );
+
+gulp.task( 'v-readme', function() {
+
+	var the_version = argv.V;
+
+	if ( ! the_version ) {
+		gutil.log( gutil.colors.red( 'Missing version number. Try `gulp versioner -V 9.9.9`' ) );
+		return;
+	}
+
+	return gulp.src( [ './_readme/header.md'  ], { base: './' } )
+		.pipe( replace( /Stable tag: (\d+\.\d+\.\d+)/g, function( match, p1, offset, string ) {
+			return match.replace( p1, the_version );
 		} ) )
 		.pipe( gulp.dest( './' ) );
 
