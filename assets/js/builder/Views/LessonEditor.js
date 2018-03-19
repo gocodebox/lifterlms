5a8e73ae6b0ed1f3d0567f863a6339253bc920f7
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-03-19 10:03:57 -0700

    lesson settings editor

--- assets/js/builder/Views/LessonEditor.js
+++ assets/js/builder/Views/LessonEditor.js
@@ -1,30 +1,73 @@
 /**
- * Sidebar Elements View
- * @since    3.16.0
- * @version  3.16.0
+ * Lesson Editor (Sidebar) View
+ * @since    [version]
+ * @version  [version]
  */
-define( [], function() {
+define( [
+		'Views/_Detachable',
+		'Views/_Editable',
+		'Views/_Trashable'
+	], function(
+		Detachable,
+		Editable,
+		Trashable
+	) {
 
-	return Backbone.View.extend( {
+	return Backbone.View.extend( _.defaults( {
 
 		el: '#llms-editor-lesson',
 
+		/**
+		 * Events
+		 * @type  {Object}
+		 */
+		events: _.defaults( {}, Detachable.events, Editable.events, Trashable.events ),
+
+		/**
+		 * Template function
+		 * @type  {[type]}
+		 */
 		template: wp.template( 'llms-lesson-settings-template' ),
 
+		/**
+		 * Init
+		 * @param    obj   data  parent template data
+		 * @return   void
+		 * @since    [version]
+		 * @version  [version]
+		 */
 		initialize: function( data ) {
 
 			this.model = data.lesson;
 
+			var change_events = [
+				'change:date_available',
+				'change:drip_method',
+				'change:time_available',
+			];
+			_.each( change_events, function( event ) {
+				this.listenTo( this.model, event, this.render );
+			}, this );
+
 		},
 
+		/**
+		 * Render the view
+		 * @return   obj
+		 * @since    [version]
+		 * @version  [version]
+		 */
 		render: function() {
 
 			this.$el.html( this.template( this.model ) );
+
+			this.init_datepickers();
+			this.init_selects();
 
 			return this;
 
 		},
 
-	} );
+	}, Detachable, Editable, Trashable ) );
 
 } );
