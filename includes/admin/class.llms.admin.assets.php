0d8dfc2bd9fa6d5ad4b485ff4eae09ba4c9d7f0d
Saurabh Shukla <saurabh@yapapaya.com>
2018-03-28 11:22:32 +0530

    Recognize debug mode for loading un-minified css

--- includes/admin/class.llms.admin.assets.php
+++ includes/admin/class.llms.admin.assets.php
@@ -21,6 +21,10 @@ class LLMS_Admin_Assets {
 	 */
 	public function __construct() {
 
+		$debug = ( defined( 'SCRIPT_DEBUG') ) ? SCRIPT_DEBUG : false;
+
+		self::$min = ( $debug ) ? 'min' : '';
+
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
 		add_action( 'admin_print_scripts', array( $this, 'admin_print_scripts' ) );
