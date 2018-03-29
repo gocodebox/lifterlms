c2ec5787ee85b86ab9b3e623aa29d15c9900b15b
Saurabh Shukla <saurabh@yapapaya.com>
2018-03-29 10:57:42 +0530

    Load unminified deatetimepicker js in debug mode

--- includes/admin/class.llms.admin.assets.php
+++ includes/admin/class.llms.admin.assets.php
@@ -239,7 +239,7 @@ class LLMS_Admin_Assets {
 
 			wp_enqueue_script( 'webui-popover', LLMS_PLUGIN_URL . 'assets/vendor/webui-popover/jquery.webui-popover' . LLMS_ASSETS_SUFFIX . '.js', array( 'jquery' ), LLMS()->version, true );
 			wp_enqueue_style( 'llms-datetimepicker', LLMS_PLUGIN_URL . 'assets/vendor/datetimepicker/jquery.datetimepicker.min.css' );
-			wp_enqueue_script( 'llms-datetimepicker', LLMS_PLUGIN_URL . 'assets/vendor/datetimepicker/jquery.datetimepicker.full.min.js', array( 'jquery' ), LLMS()->version, true );
+			wp_enqueue_script( 'llms-datetimepicker', LLMS_PLUGIN_URL . 'assets/vendor/datetimepicker/jquery.datetimepicker.full' . LLMS_ASSETS_SUFFIX . '.js', array( 'jquery' ), LLMS()->version, true );
 
 			if ( apply_filters( 'llms_builder_use_heartbeat', true ) ) {
 				wp_enqueue_script( 'heartbeat' );
