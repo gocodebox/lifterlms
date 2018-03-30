2b0cd27370a5d68d198ed8efb8b9e0308ecf1508
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-03-30 14:54:32 -0700

    3.17.1

--- languages/lifterlms-js-pot.php
+++ languages/lifterlms-js-pot.php
@@ -120,7 +120,7 @@ $strings = array(
 	/**
 	 * file: assets/js/builder/Models/Lesson.js
 	 * @since    3.13.0
-	 * @version  [version]
+	 * @version  3.17.1
 	 */
 	'New Lesson' => esc_html__( 'New Lesson', 'lifterlms' ),
 	'lessons' => esc_html__( 'lessons', 'lifterlms' ),
@@ -150,7 +150,7 @@ $strings = array(
 	/**
 	 * file: assets/js/builder/Schemas/Lesson.js
 	 * @since    3.17.0
-	 * @version  [version]
+	 * @version  3.17.1
 	 */
 	'General Settings' => esc_html__( 'General Settings', 'lifterlms' ),
 	'Video Embed URL' => esc_html__( 'Video Embed URL', 'lifterlms' ),
@@ -179,7 +179,7 @@ $strings = array(
 	/**
 	 * file: assets/js/builder/Views/_Editable.js
 	 * @since    3.16.0
-	 * @version  [version]
+	 * @version  3.17.1
 	 */
 	'Select an image' => esc_html__( 'Select an image', 'lifterlms' ),
 	'Use this image' => esc_html__( 'Use this image', 'lifterlms' ),
@@ -194,7 +194,7 @@ $strings = array(
 	/**
 	 * file: assets/js/builder/Views/Assignment.js
 	 * @since    3.17.0
-	 * @version  [version]
+	 * @version  3.17.1
 	 */
 	'%1$s Assignment' => esc_html__( '%1$s Assignment', 'lifterlms' ),
 	'Add Existing Assignment' => esc_html__( 'Add Existing Assignment', 'lifterlms' ),
@@ -240,7 +240,7 @@ $strings = array(
 	/**
 	 * file: assets/js/builder/Views/SettingsFields.js
 	 * @since    3.17.0
-	 * @version  [version]
+	 * @version  3.17.1
 	 */
 	'Use SoundCloud or Spotify audio URLS.' => esc_html__( 'Use SoundCloud or Spotify audio URLS.', 'lifterlms' ),
 	'Permalink' => esc_html__( 'Permalink', 'lifterlms' ),
