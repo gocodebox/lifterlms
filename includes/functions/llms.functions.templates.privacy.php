b3e5273ebc500ba9a62719594d822c5e7ef03aa2
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-05-07 11:55:10 -0700

    add privacy and t&c related functions

---
+++ includes/functions/llms.functions.templates.privacy.php
@@ -0,0 +1,91 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Privacy related template functions
+ * @since    [version]
+ * @version  [version]
+ */
+
+/**
+ * Get the HTML for the Terms field displayed on reg forms
+ * @param    boolean    $echo  [description]
+ * @param    boolean    $echo   echo the data if true, return otherwise
+ * @return   void|string
+ * @since    3.0.0
+ * @version  [version]
+ */
+if ( ! function_exists( 'llms_agree_to_terms_form_field' ) ) {
+
+	function llms_agree_to_terms_form_field( $echo = true ) {
+
+		$ret = '';
+
+		if ( llms_are_terms_and_conditions_required() ) {
+
+			$page_id = get_option( 'lifterlms_terms_page_id', false );
+			$ret = llms_form_field( array(
+				'columns' => 12,
+				'description' => '',
+				'default' => 'no',
+				'id' => 'llms_agree_to_terms',
+				'label' => llms_get_terms_notice( true ),
+				'last_column' => true,
+				'required' => true,
+				'type'  => 'checkbox',
+				'value' => 'yes',
+			), false );
+
+		}
+
+		$ret = apply_filters( 'llms_agree_to_terms_form_field', $ret, $echo );
+
+		if ( $echo ) {
+
+			echo $ret;
+			return;
+
+		}
+
+		return $ret;
+
+	}
+}// End if().
+
+
+if ( ! function_exists( 'llms_privacy_policy_form_field' ) ) {
+
+	function llms_privacy_policy_form_field( $echo = true ) {
+
+		$ret = '';
+
+		$notice = llms_get_privacy_notice( true );
+		if ( $notice ) {
+			$ret = llms_form_field( array(
+				'columns' => 12,
+				// 'description' => '',
+				// 'default' => 'no',
+				// 'id' => 'llms_agree_to_terms',
+				'label' => $notice,
+				'last_column' => true,
+				// 'required' => true,
+				'type'  => 'html',
+				// 'value' => 'yes',
+			), false );
+		}
+
+		$ret = apply_filters( 'llms_privacy_policy_form_field', $ret, $echo );
+
+		if ( $echo ) {
+
+			echo $ret;
+			return;
+
+		}
+
+		return $ret;
+
+	}
+}// End if().
