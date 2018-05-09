d814f1ce8303b96baf2d46e62d7f8a6ba864dea6
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-05-09 15:56:48 -0700

    docs

--- includes/class.llms.certificates.php
+++ includes/class.llms.certificates.php
@@ -1,59 +1,65 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+defined( 'ABSPATH' ) || exit;
 
 /**
-* Base Certificates Class
-*
-* Queries appropriate certificates
-*
-* @author codeBOX
-* @project lifterLMS
-*/
+ * Certificates
+ * @see      LLMS()->certificates()
+ * @since    1.0.0
+ * @version  [version]
+ */
 class LLMS_Certificates {
 
-	public $certs;
-
+	/**
+	 * Instance
+	 * @var  null
+	 */
 	protected static $_instance = null;
 
+	/**
+	 * Instance singleton
+	 * @return   void
+	 * @since    1.0.0
+	 * @version  1.0.0
+	 */
 	public static function instance() {
 		if ( is_null( self::$_instance ) ) {
-			self::$_instance = new self(); }
+			self::$_instance = new self();
+		}
 		return self::$_instance;
 	}
 
 	/**
 	 * Constructor
-	 * Initialize class and add actions
+	 * @return   void
+	 * @since    1.0.0
+	 * @version  1.0.0
 	 */
-	function __construct() {
-
+	private function __construct() {
 		$this->init();
-
 	}
 
 	/**
 	 * Initialize Class
-	 * @return void
+	 * @return   void
+	 * @since    1.0.0
+	 * @version  1.0.0
 	 */
-	function init() {
-
+	public function init() {
 		include_once( 'class.llms.certificate.php' );
-
 		$this->certs['LLMS_Certificate_User'] = include_once( 'certificates/class.llms.certificate.user.php' );
 	}
 
 	/**
 	 * Award a certificate to a user
 	 * Calls trigger method passing arguments
-	 *
-	 *
-	 * @param  int $person_id        [ID of the current user]
-	 * @param  int $achievement      [Achivement template post ID]
-	 * @param  int $related_post_id  Post ID of the related engagment (eg lesson id)
-	 *
-	 * @return void
+	 * @param    int   $person_id        [ID of the current user]
+	 * @param    int   $achievement      [Achivement template post ID]
+	 * @param    int   $related_post_id  Post ID of the related engagment (eg lesson id)
+	 * @return   void
+	 * @since    1.0.0
+	 * @version  1.0.0
 	 */
-	function trigger_engagement( $person_id, $certificate_id, $related_post_id ) {
+	public function trigger_engagement( $person_id, $certificate_id, $related_post_id ) {
 		$certificate = $this->certs['LLMS_Certificate_User'];
 		$certificate->trigger( $person_id, $certificate_id, $related_post_id );
 	}
