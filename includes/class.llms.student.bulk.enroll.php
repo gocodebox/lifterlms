36c23b40eaa15ef5fbd4b85a1365caf167d69e42
Saurabh Shukla <saurabh@yapapaya.com>
2018-06-14 16:52:52 +0530

    Create class for handling bulk enrollment

---
+++ includes/class.llms.student.bulk.enroll.php
@@ -0,0 +1,15 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Bulk Enrollment Class
+ *
+ * @since	[version]
+ * @version	[version]
+ */
+class LLMS_Student_Bulk_Enroll {
+
+
+}
+
+return new LLMS_Student_Bulk_Enroll();
