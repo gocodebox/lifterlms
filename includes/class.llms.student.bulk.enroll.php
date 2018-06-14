41305de6c08a744d31bed93d8b7b2a755723e3d1
Saurabh Shukla <saurabh@yapapaya.com>
2018-06-14 16:54:31 +0530

    Display product selection input

--- includes/class.llms.student.bulk.enroll.php
+++ includes/class.llms.student.bulk.enroll.php
@@ -9,6 +9,41 @@ defined( 'ABSPATH' ) || exit;
  */
 class LLMS_Student_Bulk_Enroll {
 
+	/**
+	 * Constructor
+	 *
+	 * @since	[version]
+	 * @version	[version]
+	 */
+	public function __construct() {
+		// hook into extra ui on users table to display product selection
+		add_action( 'manage_users_extra_tablenav', array( $this, 'display_product_selection_for_bulk_users' ) );
+
+	}
+
+	/**
+	 * Displays ui for selecting product to bulk enroll users into
+	 *
+	 * @param	string $which
+	 * @since	[version]
+	 * @version	[version]
+	 */
+	public function display_product_selection_for_bulk_users( $which ) {
+
+		// the attributes need to be different for top and bottom of the table
+		$id = 'bottom' === $which ? 'llms_bulk_enroll_product2' : 'llms_bulk_enroll_product';
+		$submit = 'bottom' === $which ? 'llms_bulk_enroll2' : 'llms_bulk_enroll';
+		?>
+		<div class="alignleft actions">
+			<label class="screen-reader-text" for="_llms_bulk_enroll_product">
+				<?php _e( 'Choose Course/Membership', 'lifterlms' ); ?>
+			</label>
+			<select id="<?php echo $id; ?>" class="llms-bulk-enroll-product" data-post-type="llms_membership,course" name="<?php echo $id; ?>" style="min-width:200px;max-width:auto;">
+			</select>
+			<input type="submit" name="<?php echo $submit; ?>" id="<?php echo $submit; ?>" class="button" value="<?php esc_attr_e( 'Enroll', 'lifterlms' ); ?>">
+		</div>
+		<?php
+	}
 
 }
 
