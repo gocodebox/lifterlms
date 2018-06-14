bdf22f4234d51a86ed4c71f6067c2a56e18a9df5
Saurabh Shukla <saurabh@yapapaya.com>
2018-06-14 17:45:38 +0530

    Include bulk enrollment class in plugin

--- lifterlms.php
+++ lifterlms.php
@@ -261,6 +261,7 @@ final class LifterLMS {
 			require 'includes/abstracts/abstract.llms.admin.metabox.php';
 			include_once( 'includes/admin/class.llms.admin.user.custom.fields.php' );
 			include_once( 'includes/admin/users/class.llms.wp.user.php' );
+			include_once( 'includes/admin/class.llms.student.bulk.enroll.php' );
 
 		}
 
