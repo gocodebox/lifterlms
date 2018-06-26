b7a952997204a8a09048b34ea368c1e5085e9b5f
Saurabh Shukla <saurabh@yapapaya.com>
2018-06-26 22:29:26 +0530

    Fix nonce verification on registration

--- includes/forms/controllers/class.llms.controller.registration.php
+++ includes/forms/controllers/class.llms.controller.registration.php
@@ -53,7 +53,9 @@ class LLMS_Controller_Registration {
 
 		if ( 'POST' !== strtoupper( getenv( 'REQUEST_METHOD' ) ) || empty( $_POST['action'] ) || 'llms_register_person' !== $_POST['action'] || empty( $_POST['_llms_register_person_nonce'] ) ) { return; }
 
-		wp_verify_nonce( $_POST['_llms_register_person_nonce'], 'llms_register_person' );
+		if ( ! llms_verify_nonce( $_POST['_llms_register_person_nonce'], 'llms_register_person' ) ){
+			return;
+		}
 
 		do_action( 'lifterlms_before_new_user_registration' );
 
