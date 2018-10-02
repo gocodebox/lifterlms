980c63a134870aa32e4f605432411e801322798b
Phil Webster <pwebster33@gmail.com>
2018-10-02 10:36:14 -0400

    decode html entities for email titles

--- includes/notifications/views/class.llms.notification.view.purchase.receipt.php
+++ includes/notifications/views/class.llms.notification.view.purchase.receipt.php
@@ -156,7 +156,7 @@ class LLMS_Notification_View_Purchase_Receipt extends LLMS_Abstract_Notification
 			break;
 
 			case '{{PRODUCT_TITLE}}':
-				$code = $order->get( 'product_title' );
+				$code = wp_specialchars_decode( $order->get( 'product_title' ) );
 			break;
 
 			case '{{PRODUCT_TITLE_LINK}}':
