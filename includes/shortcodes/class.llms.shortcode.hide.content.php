74db59e8ef7366dffa437b0c30bcd1758f66de07
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-10-29 15:17:37 -0700

    docs

--- includes/shortcodes/class.llms.shortcode.hide.content.php
+++ includes/shortcodes/class.llms.shortcode.hide.content.php
@@ -1,15 +1,19 @@
 <?php
+defined( 'ABSPATH' ) || exit;
+
 /**
  * LifterLMS Hide Content Shortcode
  *
  * [lifterlms_hide_content]
  *
+ * @example
+ * 		[hide_content id="1"] allows user with access to 1 to access content
+ * 		[hide_content id="1,2,3,4" relation="any"] allows user with access to 1,2,3, OR 4 to access content
+ *   	[hide_content id="1,2,3,4" relation="all"] allows only users with access 1,2,3 AND 4 to access
+ *
  * @since    3.5.1
- * @version  3.5.1
+ * @version  [version]
  */
-
-if ( ! defined( 'ABSPATH' ) ) { exit; }
-
 class LLMS_Shortcode_Hide_Content extends LLMS_Shortcode {
 
 	/**
@@ -23,7 +27,7 @@ class LLMS_Shortcode_Hide_Content extends LLMS_Shortcode {
 	 * with the user submitted attributes and passed to $this->get_output()
 	 * @return   array
 	 * @since    3.5.1
-	 * @version  3.5.1
+	 * @version  [version]
 	 */
 	protected function get_default_attributes() {
 		return array(
@@ -40,22 +44,19 @@ class LLMS_Shortcode_Hide_Content extends LLMS_Shortcode {
 	 * $atts & $content are both filtered before being passed to get_output()
 	 * output is filtered so the return of get_output() doesn't need its own filter
 	 *
-	 * Examples:
-	 * [hide_content id="1,2,3,4" relation="any"] allows member with access to 1,2,3, OR 4 to access content
-	 * [hide_content id="1,2,3,4" relation="all"] allows only members with access 1,2,3 AND 4 to access
-	 *
 	 * @return   string
 	 * @since    3.5.1
-	 * @version  3.5.1
+	 * @version  [version]
 	 */
 	protected function get_output() {
 
-		// backwards compatibility, get membership if set and fallback to the id
+		// backwards compatibility, get membership if set and fallback to the id.
 		$ids = $this->get_attribute( 'membership' ) ? $this->get_attribute( 'membership' ) : $this->get_attribute( 'id' );
-		// Explode, trim whitespace and remove empty values
+
+		// Explode, trim whitespace and remove empty values.
 		$ids = (array) array_map( 'trim', array_filter( explode( ',', $ids ) ) );
 
-		// Assume content is hidden
+		// Assume content is hidden.
 		$hidden = true;
 
 		if ( 'any' === $this->get_attribute( 'relation' ) && ! empty( $ids ) ) {
@@ -65,9 +66,7 @@ class LLMS_Shortcode_Hide_Content extends LLMS_Shortcode {
 					break;
 				}
 			}
-		}
-
-		if ( 'all' === $this->get_attribute( 'relation' ) && ! empty( $ids ) ) {
+		} elseif ( 'all' === $this->get_attribute( 'relation' ) && ! empty( $ids ) ) {
 			$inc = 0;
 			foreach ( $ids as $id ) {
 				if ( llms_is_user_enrolled( get_current_user_id(), $id ) ) {
