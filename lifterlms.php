c058218b2f68f2badaa9df9681e6864ba41ca6b5
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-01-21 13:52:52 -0800

    3.27.0-alpha.1

--- lifterlms.php
+++ lifterlms.php
@@ -3,7 +3,7 @@
  * Plugin Name: LifterLMS
  * Plugin URI: https://lifterlms.com/
  * Description: LifterLMS, the #1 WordPress LMS solution, makes it easy to create, sell, and protect engaging online courses.
- * Version: 3.27.0
+ * Version: 3.27.0-alpha.1
  * Author: LifterLMS
  * Author URI: https://lifterlms.com/
  * Text Domain: lifterlms
@@ -26,7 +26,7 @@ require_once 'vendor/autoload.php';
  */
 final class LifterLMS {
 
-	public $version = '3.27.0';
+	public $version = '3.27.0-alpha.1';
 
 	protected static $_instance = null;
 
