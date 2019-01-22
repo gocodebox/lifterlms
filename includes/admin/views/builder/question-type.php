2053b6c03933ec203ef539f1bef190bd44e9c4ed
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-01-22 11:17:08 -0800

    add ids to buttons

--- includes/admin/views/builder/question-type.php
+++ includes/admin/views/builder/question-type.php
@@ -2,7 +2,7 @@
 /**
  * Builder quiz type template
  * @since   3.16.0
- * @version 3.16.0
+ * @version [version]
  */
 ?>
 <script type="text/html" id="tmpl-llms-question-type-template">
@@ -10,7 +10,7 @@
 	<# if ( data.get( 'upgrade' ) ) { #>
 		<a class="llms-type-unavailable tip--top-right" href="{{{ data.get( 'upgrade' ) }}}" data-tip="<?php esc_attr_e( 'Install the LifterLMS Advanced Quizzes add-on to enable this question type', 'lifterlms' ); ?>" target="_blank">
 	<# } #>
-	<button class="llms-element-button small llms-add-question" data-id="{{{ data.get( 'id' ) }}}">
+	<button class="llms-element-button small llms-add-question" data-id="{{{ data.get( 'id' ) }}}" id="llms-add-question--{{{ data.get( 'id' ) }}}">
 		<i class="fa fa-{{{ data.get( 'icon' ) }}}" aria-hidden="true"></i> {{{ data.get( 'name' ) }}}
 	</button>
 	<# if ( data.get( 'upgrade' ) ) { #>
