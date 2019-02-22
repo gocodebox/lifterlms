3feb14d92450b07ddfab82b684f68ff4905ff72d
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-02-21 16:18:13 -0800

    clean up empty plan message

--- includes/admin/views/access-plans/metabox.php
+++ includes/admin/views/access-plans/metabox.php
@@ -15,7 +15,10 @@ defined( 'ABSPATH' ) || exit;
 <div class="llms-metabox" id="llms-product-options-access-plans">
 
 	<section class="llms-collapsible-group llms-access-plans" id="llms-access-plans">
-		<p class="no-plans-message"><?php printf( __( 'No access plans exist for your %s.', 'lifterlms' ), strtolower( $product->get_post_type_label( 'singular_name' ) ) ); ?></p>
+		<div class="llms-no-plans-msg">
+			<p><?php printf( __( 'Access plans define the payment options and access time-periods available for this %s.', 'lifterlms' ), strtolower( $product->get_post_type_label( 'singular_name' ) ) ); ?></h3>
+			<p><?php printf( __( 'No access plans exist for your %s, click "Add New" to get started.', 'lifterlms' ), strtolower( $product->get_post_type_label( 'singular_name' ) ) ); ?></p>
+		</div>
 		<?php foreach ( $product->get_access_plans( false, false ) as $plan ) : ?>
 			<?php include 'access-plan.php'; ?>
 		<?php endforeach; ?>
