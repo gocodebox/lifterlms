4b8b62aad4f1d00898b5872c8a258bc4dbf73b50
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-02-27 16:41:36 -0800

    Deploy v3.29.1 [ci skip]

---
+++ vendor/ericmann/wp-session-manager/wp-session-manager.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: WP Session Manager
+ * Plugin URI:  http://jumping-duck.com/wordpress/plugins
+ * Description: Prototype session management for WordPress.
+ * Version:     1.2.2
+ * Author:      Eric Mann
+ * Author URI:  http://eamann.com
+ * License:     GPLv2+
+ */
+
+// let users change the session cookie name
+if( ! defined( 'WP_SESSION_COOKIE' ) ) {
+	define( 'WP_SESSION_COOKIE', '_wp_session' );
+}
+
+if ( ! class_exists( 'Recursive_ArrayAccess' ) ) {
+	include 'includes/class-recursive-arrayaccess.php';
+}
+
+// Include utilities class
+if ( ! class_exists( 'WP_Session_Utils' ) ) {
+	include 'includes/class-wp-session-utils.php';
+}
+
+// Include WP_CLI routines early
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	include 'includes/wp-cli.php';
+}
+
+// Only include the functionality if it's not pre-defined.
+if ( ! class_exists( 'WP_Session' ) ) {
+	include 'includes/class-wp-session.php';
+	include 'includes/wp-session.php';
+}
