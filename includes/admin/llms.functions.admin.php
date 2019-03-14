b0714ff3160cb176860a4b1ad64e6bc855116a4b
Saurabh Shukla <saurabh@yapapaya.com>
2019-03-14 23:01:38 +0530

    Add redirection types #768

--- includes/admin/llms.functions.admin.php
+++ includes/admin/llms.functions.admin.php
@@ -144,6 +144,21 @@ function llms_get_sales_page_types() {
 }
 
 /**
+ * Get an array of available course/membership checkout redirection options
+ * @return   array
+ * @since    [version]
+ * @version  [version]
+ */
+function llms_get_checkout_redirection_types() {
+	return apply_filters( 'llms_sales_page_types', array(
+		'membership' => __( 'Redirect to the Membership', 'lifterlms' ), // only for membership-only access plans
+		'self' => __( 'Redirect back to itself', 'lifterlms' ),
+		'page' => __( 'Redirect to a WordPress Page', 'lifterlms' ),
+		'url' => __( 'Redirect to a custom URL', 'lifterlms' ),
+	) );
+}
+
+/**
  * Add a "merge code" button that to auto-add merge codes to email & etc...
  * @param    string     $target  target to add the merge code to
  *                               accepts the ID of a tinymce editor
