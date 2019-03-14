695ae128d5e01a985d78b2f5e97b92f22c0d7a32
Saurabh Shukla <saurabh@yapapaya.com>
2019-03-15 00:28:48 +0530

    Add redirect parameter to membership plan permalinks for members-only course Access Plans

--- templates/product/access-plan-restrictions.php
+++ templates/product/access-plan-restrictions.php
@@ -10,10 +10,24 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 <?php if ( $plan->has_availability_restrictions() ) : ?>
+	<?php
+	$redirection = $plan->get_redirection_url();
+	if( ! empty( $redirection ) ){
+
+	}
+	?>
 	<div class="llms-access-plan-restrictions">
 		<em class="stamp"><?php _e( 'MEMBER PRICING', 'lifterlms' ); ?></em>
 		<ul>
 			<?php foreach ( $plan->get_array( 'availability_restrictions' ) as $mid ) : ?>
+				<?php
+				$membership_link = get_permalink( $mid );
+				if( ! empty( $redirection ) ){
+					$membership_link = add_query_arg( array(
+						'redirect' => $redirection,
+					), $membership_link );
+				}
+				?>
 				<li><a href="<?php echo get_permalink( $mid ); ?>"><?php echo get_the_title( $mid ); ?></a></li>
 			<?php endforeach; ?>
 		</ul>
