90559c57d8870f761b7d310b1ec84e1cd0a07b65
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-04-04 11:47:24 -0700

    Deploy v3.30.1 [ci skip]

---
+++ vendor/gocodebox/action-scheduler/tests/phpunit/schedules/ActionScheduler_IntervalSchedule_Test.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Class ActionScheduler_IntervalSchedule_Test
+ * @group schedules
+ */
+class ActionScheduler_IntervalSchedule_Test extends ActionScheduler_UnitTestCase {
+	public function test_creation() {
+		$time = as_get_datetime_object();
+		$schedule = new ActionScheduler_IntervalSchedule($time, HOUR_IN_SECONDS);
+		$this->assertEquals( $time, $schedule->next() );
+	}
+
+	public function test_next() {
+		$now = time();
+		$start = $now - 30;
+		$schedule = new ActionScheduler_IntervalSchedule( as_get_datetime_object("@$start"), MINUTE_IN_SECONDS );
+		$this->assertEquals( $start, $schedule->next()->format('U') );
+		$this->assertEquals( $now + MINUTE_IN_SECONDS, $schedule->next(as_get_datetime_object())->format('U') );
+		$this->assertEquals( $start, $schedule->next(as_get_datetime_object("@$start"))->format('U') );
+	}
+
+	public function test_is_recurring() {
+		$start = time() - 30;
+		$schedule = new ActionScheduler_IntervalSchedule( as_get_datetime_object("@$start"), MINUTE_IN_SECONDS );
+		$this->assertTrue( $schedule->is_recurring() );
+	}
+}
+ 
