c20502296329fa7298c5bc5a1f3eba6030c168e7
Rocco Aliberti <raliberti84@gmail.com>
2019-05-02 11:17:43 +0200

    just add the $check_availability as third original filter parameter

--- includes/models/model.llms.access.plan.php
+++ includes/models/model.llms.access.plan.php
@@ -6,7 +6,7 @@
  * @package  LifterLMS/Models
  *
  * @since    3.0.0
- * @version  3.30.1
+ * @version  [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -48,7 +48,7 @@ defined( 'ABSPATH' ) || exit;
  * @since 3.0.0
  * @since 3.30.0 Added checkout redirect properties and methods
  * @since 3.30.1 Added method to get the initial price due on checkout.
- * @since [version] Added 'llms_access_plan_get_checkout_url' filter and deprecated 'llms_plan_get_checkout_url' filter.
+ * @since [version] The `$check_availability` parameter was added to the `llms_plan_get_checkout_url` filter.
  */
 class LLMS_Access_Plan extends LLMS_Post_Model {
 
@@ -266,7 +266,6 @@ class LLMS_Access_Plan extends LLMS_Post_Model {
 	 *
 	 * @since    3.0.0
 	 * @since    3.30.0 Added access plan redirection settings.
-	 * @since    [version] Added 'llms_access_plan_get_checkout_url' filter and deprecated 'llms_plan_get_checkout_url' filter.
 	 *
 	 * @param    bool   $check_availability  determine if availability checks should be made (allows retrieving plans on admin panel).
 	 * @return   string
@@ -311,24 +310,14 @@ class LLMS_Access_Plan extends LLMS_Post_Model {
 		 * Filter the checkout URL for an access plan.
 		 *
 		 * @since Unknown
-		 * @deprecated [version], use 'llms_access_plan_get_checkout_url' instead.
+		 * @since [version] The `$check_availability` parameter was added.
 		 *
-		 * @param string $ret The checkout URL.
+		 * @param string $ret      The checkout URL.
 		 * @param LLMS_Access_Plan $this Access plan object.
-		 */
-		$ret = apply_filters( 'llms_plan_get_checkout_url', $ret, $this );
-
-		/**
-		 * Filter the checkout URL for an access plan.
-		 *
-		 * @since [version]
-		 *
-		 * @param string           $ret                The checkout URL.
 		 * @param bool             $check_availability Determine if availability checks should be made.
 		 *                                             (allows retrieving plans on admin panel)
-		 * @param LLMS_Access_Plan $this               Access plan object.
 		 */
-		return apply_filters( 'llms_access_plan_get_checkout_url', $ret, $check_availability, $this );
+		return apply_filters( 'llms_plan_get_checkout_url', $ret, $this, $check_availability );
 
 	}
 
