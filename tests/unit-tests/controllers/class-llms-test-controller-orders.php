037b7b3976862e29fd4a78b7de90cf8c0a963c6c
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-05-08 15:25:25 -0700

    add tests for recurring_charge() method

--- tests/unit-tests/controllers/class-llms-test-controller-orders.php
+++ tests/unit-tests/controllers/class-llms-test-controller-orders.php
@@ -4,7 +4,7 @@
  *
  * @package LifterLMS/Tests
  *
- * @group    orders
+ * @group orders
  *
  * @since 3.19.0
  * @since [version] Update to use latest action-scheduler functions.
@@ -13,6 +13,32 @@ class LLMS_Test_Controller_Orders extends LLMS_UnitTestCase {
 
 	// consider dates equal within 60 seconds
 	private $date_delta = 60;
+
+	public function setUp() {
+
+		parent::setUp();
+		LLMS_Site::update_feature( 'recurring_payments', true );
+
+	}
+
+	/**
+	 * Disable manual gateway recurring payments for mocking error conditions.
+	 *
+	 * @since [version]
+	 *
+	 * @param array $supports Gateway features array.
+	 * @param string $gateway_id Gateway ID.
+	 * @return array
+	 */
+	public function mod_gateway_features( $supports, $gateway_id ) {
+
+		if ( 'manual' === $gateway_id ) {
+			$supports['recurring_payments'] = false;
+		}
+
+		return $supports;
+
+	}
 
 	/**
 	 * Test order completion actions
@@ -214,6 +240,111 @@ class LLMS_Test_Controller_Orders extends LLMS_UnitTestCase {
 		) ) );
 		$this->assertEquals( 'cancelled', $student->get_enrollment_status( $order->get( 'product_id' ) ) );
 		$this->assertEquals( 'llms-cancelled', get_post_status( $order->get( 'id' ) ) );
+
+	}
+
+	/**
+	 * Test gateway-related errors encountered during a recurring_charge attempt.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_recurring_charge_gateway_errors() {
+
+		$plan = $this->get_mock_plan( '200.00', 1 );
+		$order = $this->get_mock_order( $plan );
+
+		$order->set( 'payment_gateway', 'fake-gateway' );
+
+		// starting action numbers.
+		$note_actions = did_action( 'llms_new_order_note_added' );
+		$err_actions = did_action( 'llms_order_recurring_charge_gateway_error' );
+
+		// Trigger recurring payment.
+		do_action( 'llms_charge_recurring_payment', $order->get( 'id' ) );
+
+		$this->assertSame( $note_actions + 1, did_action( 'llms_new_order_note_added' ) );
+		$this->assertSame( $err_actions + 1, did_action( 'llms_order_recurring_charge_gateway_error' ) );
+
+	}
+
+	/**
+	 * Test a recurring payment processed when recurring payments are disabled on the site.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_recurring_charge_staging_mode() {
+
+		// Disable recurring payments.
+		LLMS_Site::update_feature( 'recurring_payments', false );
+
+		$plan = $this->get_mock_plan( '200.00', 1 );
+		$order = $this->get_mock_order( $plan );
+
+		// starting action numbers.
+		$skip_actions = did_action( 'llms_order_recurring_charge_skipped' );
+		$note_actions = did_action( 'llms_new_order_note_added' );
+
+		// Trigger recurring payment.
+		do_action( 'llms_charge_recurring_payment', $order->get( 'id' ) );
+
+		$this->assertSame( $note_actions + 1, did_action( 'llms_new_order_note_added' ) );
+		$this->assertSame( $skip_actions + 1, did_action( 'llms_order_recurring_charge_skipped' ) );
+
+	}
+
+	/**
+	 * Test gateway-related errors encountered during a recurring_charge attempt.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_recurring_charge_gateway_support_disabled() {
+
+		$plan = $this->get_mock_plan( '200.00', 1 );
+		$order = $this->get_mock_order( $plan );
+
+		// Disable recurring payments.
+		add_filter( 'llms_get_gateway_supported_features', array( $this, 'mod_gateway_features' ), 10, 2 );
+
+		// starting action numbers.
+		$err_actions = did_action( 'llms_order_recurring_charge_gateway_payments_disabled' );
+		$note_actions = did_action( 'llms_new_order_note_added' );
+
+		// Trigger recurring payment.
+		do_action( 'llms_charge_recurring_payment', $order->get( 'id' ) );
+
+		$this->assertSame( $note_actions + 1, did_action( 'llms_new_order_note_added' ) );
+		$this->assertSame( $err_actions + 1, did_action( 'llms_order_recurring_charge_gateway_payments_disabled' ) );
+
+		// Re-enable recurring payments.
+		remove_filter( 'llms_get_gateway_supported_features', array( $this, 'mod_gateway_features' ), 10, 2 );
+
+	}
+
+	/**
+	 * Test gateway-related errors encountered during a recurring_charge attempt.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_recurring_charge_success() {
+
+		$plan = $this->get_mock_plan( '200.00', 1 );
+		$order = $this->get_mock_order( $plan );
+
+		// starting action numbers.
+		$actions = did_action( 'llms_manual_payment_due' );
+
+		// Trigger recurring payment.
+		do_action( 'llms_charge_recurring_payment', $order->get( 'id' ) );
+
+		$this->assertSame( $actions + 1, did_action( 'llms_manual_payment_due' ) );
 
 	}
 
