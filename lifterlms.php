a4f83cf9cd095088be8b262abd143bd57155c055
Saurabh Shukla <saurabh@yapapaya.com>
2019-06-19 15:25:16 +0530

    Add module loading support

--- lifterlms.php
+++ lifterlms.php
@@ -43,6 +43,8 @@ final class LifterLMS {
 	public $query = null;
 	public $session = null;
 
+	public $modules_loaded = array();
+
 	/**
 	 * Main Instance of LifterLMS
 	 * Ensures only one instance of LifterLMS is loaded or can be loaded.
@@ -103,6 +105,82 @@ final class LifterLMS {
 
 		// Loaded action
 		do_action( 'lifterlms_loaded' );
+
+	}
+
+	/**
+	 * Loads Modules.
+	 *
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	private function load_modules(){
+
+		/**
+		 * Filters list of LifterLMS modules to load.
+		 *
+		 * @since	[version]
+		 * @version	[version]
+		 */
+		$final_modules = apply_filters( 'lifterlms_modules', $this->load_module_info() );
+
+		$modules_loaded = array();
+
+		foreach( $final_modules as $module ){
+
+			// define the constant as true if it hasn't been defined by the user in wp-config.php or similar.
+			if( ! defined( $module[ 'constant' ] ) ){
+				define( $module[ 'constant' ] , true );
+			}
+
+			// if the constant's value is true and the class file exists, include the module class
+			if( constant( $module[ 'constant' ] ) === true && file_exists( $module[ 'file_path' ] ) ){
+				include_once $module[ 'file_path' ];
+			}
+
+			$this->modules_loaded[ $module[ 'name' ] ] = $module;
+
+		}
+	}
+
+	/**
+	 * Loads Module Information.
+	 *
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	private function load_module_info(){
+
+		// get a list of directories inside the modules directory.
+		$directories = glob( LLMS_PLUGIN_DIR . 'includes/modules/*' , GLOB_ONLYDIR );
+
+		$modules = array();
+
+		// loop through every directory
+		foreach( $directories as $module ){
+
+			// the name of the module is the same as the name of the directory. eg "certificate-builder"
+			$module_name = basename( $module );
+
+			// the name of the class file is similar. eg "class-llms-certificate-builder.php"
+			$module_class_file_path = "$module/class-llms-$module_name.php";
+
+			// the constant name also uses similar conventions. eg "LLMS_CERTIFICATE_BUILDER"
+			$module_constant_name = 'LLMS_' . strtoupper( str_replace( '-', '_', $module_name ) );
+
+			$modules[ $module_name ] = array(
+				'name' => $module_name,
+				'file_path' => $module_class_file_path,
+				'constant' => $module_constant_name
+			);
+
+			unset( $module_name );
+			unset( $module_class_file_path );
+			unset( $module_constant_name );
+
+		}
+
+		return $modules;
 
 	}
 
