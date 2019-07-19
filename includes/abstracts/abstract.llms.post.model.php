9ac4e20b3d9c6b3d575d5f6d49a6995b0f832f29
pondermatic <GitHub@happycorner.us>
2019-07-19 10:00:36 -0500

    Revert behavior of clone_post() and update DocBlock.

--- includes/abstracts/abstract.llms.post.model.php
+++ includes/abstracts/abstract.llms.post.model.php
@@ -330,16 +330,14 @@ abstract class LLMS_Post_Model implements JsonSerializable {
 
 	/**
 	 * Clones the Post if the post is cloneable
-	 * @return   WP_Error|int|false WP_Error, WP Post ID of the clone (new) post, or false if post is not cloneable
+	 * @return   WP_Error|int|null WP_Error, WP Post ID of the clone (new) post, or null if post is not cloneable.
 	 * @since    3.3.0
-	 * @since    [version] Returns false if post is not cloneable.
-	 * @version  [version]
 	 */
 	public function clone_post() {
 
 		// if post type doesn't support cloning, don't proceed
 		if ( ! $this->is_cloneable() ) {
-			return false;
+			return null;
 		}
 
 		$this->allowed_post_tags_set();
