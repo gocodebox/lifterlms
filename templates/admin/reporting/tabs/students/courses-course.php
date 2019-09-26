f8e311671c16739f4b4304a25827a91b343aaabc
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-09-26 11:30:37 -0700

    update template to use newer interface elements, add tooltips for course link, add link to course reporting

--- templates/admin/reporting/tabs/students/courses-course.php
+++ templates/admin/reporting/tabs/students/courses-course.php
@@ -6,17 +6,17 @@
  *
  * @since Unknown
  * @since 3.35.0 Access `$_GET` data via `llms_filter_input()`.
- * @version 3.35.0
+ * @since [version] Upgrade UI to utilize reporting widgets.
+ *               Add edit link tooltip and update icon.
+ *               Add a link to view full course reporting screen.
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
-
-if ( ! is_admin() ) {
-	exit;
-}
+is_admin() || exit;
 
 $course_id = llms_filter_input( INPUT_GET, 'course_id', FILTER_SANITIZE_NUMBER_INT );
-$course    = new LLMS_Course( $course_id );
+$course    = llms_get_post( $course_id );
 $table     = new LLMS_Table_Student_Course();
 $table->get_results(
 	array(
@@ -26,60 +26,100 @@ $table->get_results(
 );
 ?>
 
-<h2 class="llms-stab-title">
-	<?php echo $course->get( 'title' ); ?>
-	<?php echo $table->get_post_link( $course->get( 'id' ), '<span class="dashicons dashicons-admin-links"></span>' ); ?>
-</h2>
+<?php do_action( 'llms_reporting_student_single_course_before_content' ); ?>
 
-<div class="llms-widget-row">
+<div class="llms-reporting-tab-content">
 
-	<div class="llms-widget-1-5">
-		<div class="llms-widget alt">
-			<p class="llms-label"><?php _e( 'Enrollment Status', 'lifterlms' ); ?></p>
-			<h2><?php echo llms_get_enrollment_status_name( $student->get_enrollment_status( $course_id ) ); ?></h2>
-		</div>
-	</div>
+	<section class="llms-reporting-tab-main llms-reporting-widgets">
 
-	<div class="llms-widget-1-5">
-		<div class="llms-widget alt">
-			<p class="llms-label"><?php _e( 'Progress', 'lifterlms' ); ?></p>
-			<h2><?php echo $student->get_progress( $course_id, 'course' ); ?>%</h2>
-		</div>
-	</div>
+		<header>
+			<h3>
+				<?php
+					// Translators: %s = Course title.
+					printf( __( 'Course: %s', 'lifterlms' ), $course->get( 'title' ) );
+				?>
+				<?php
+					echo $table->get_post_link(
+						$course->get( 'id' ),
+						'<span class="tip--top-right" data-tip="' . esc_attr__( 'Edit course', 'lifterlms' ) . '"><i class="fa fa-pencil" aria-hidden="true"></i></span>'
+					);
+				?>
+				<a href="<?php echo LLMS_Admin_Reporting::get_current_tab_url( array( 'tab' => 'courses', 'course_id' => $course_id ) ); ?>">
+					<span class="tip--top-right" data-tip="<?php esc_attr_e( 'View course reports', 'lifterlms' ); ?>">
+						<i class="fa fa-pie-chart" aria-hidden="true"></i>
+					</span>
+				</a>
+			</h3>
+		</header>
+		<?php
 
-	<div class="llms-widget-1-5">
-		<div class="llms-widget alt">
-			<p class="llms-label"><?php _e( 'Current Grade', 'lifterlms' ); ?></p>
-			<?php $grade = $student->get_grade( $course_id ); ?>
-			<h2><?php echo $grade; ?><?php echo is_numeric( $grade ) ? '%' : ''; ?></h2>
-		</div>
-	</div>
+		do_action( 'llms_reporting_single_student_course_before_widgets', $student );
 
-	<div class="llms-widget-1-5">
-		<div class="llms-widget alt">
-			<p class="llms-label"><?php _e( 'Enrollment Date', 'lifterlms' ); ?></p>
-			<h2><?php echo $student->get_enrollment_date( $course_id, 'enrolled' ); ?></h2>
-		</div>
-	</div>
+		LLMS_Admin_Reporting::output_widget(
+			array(
+				'cols'      => 'd-1of5',
+				'icon'      => 'calendar',
+				'id'        => 'llms-reporting-student-course-enrollment-date',
+				'data'      => $student->get_enrollment_date( $course_id, 'enrolled' ),
+				'data_type' => 'date',
+				'text'      => __( 'Enrollment Date', 'lifterlms' ),
+			)
+		);
 
-	<div class="llms-widget-1-5">
-		<div class="llms-widget alt">
-		<?php if ( $student->is_complete( $course_id, 'course' ) ) : ?>
-			<p class="llms-label"><?php _e( 'Completion Date', 'lifterlms' ); ?></p>
-			<h2><?php echo $student->get_completion_date( $course_id, 'M d, Y' ); ?></h2>
-		<?php else : ?>
-			<p class="llms-label"><?php _e( 'Last Activity', 'lifterlms' ); ?></p>
-			<h2><?php echo $student->get_enrollment_date( $course_id, 'updated' ); ?></h2>
-		<?php endif; ?>
-		</div>
-	</div>
+		$enrollment_status = $student->get_enrollment_status( $course_id );
+		LLMS_Admin_Reporting::output_widget(
+			array(
+				'cols'      => 'd-1of5',
+				'icon'      => 'enrolled' === $enrollment_status ? 'check-circle' : 'exclamation-triangle',
+				'id'        => 'llms-reporting-student-course-enrollment-status',
+				'data'      => llms_get_enrollment_status_name( $enrollment_status ),
+				'data_type' => 'text',
+				'text'      => __( 'Enrollment Status', 'lifterlms' ),
+			)
+		);
+
+		$is_complete = $student->is_complete( $course_id, 'course' );
+		LLMS_Admin_Reporting::output_widget(
+			array(
+				'cols'      => 'd-1of5',
+				'icon'      => 'calendar',
+				'id'        => 'llms-reporting-student-course-completed-date',
+				'data'      => $is_complete ? $student->get_completion_date( $course_id ) : $student->get_enrollment_date( $course_id, 'updated' ),
+				'data_type' => 'date',
+				'text'      => $is_complete ? __( 'Completed Date', 'lifterlms' ) : __( 'Last Activity Date', 'lifterlms' ),
+			)
+		);
+
+		LLMS_Admin_Reporting::output_widget(
+			array(
+				'cols'      => 'd-1of5',
+				'icon'      => 'line-chart',
+				'id'        => 'llms-reporting-student-course-progress',
+				'data'      => $student->get_progress( $course_id, 'course' ),
+				'data_type' => 'percentage',
+				'text'      => __( 'Progress', 'lifterlms' ),
+			)
+		);
+
+		$grade = $student->get_grade( $course_id );
+		LLMS_Admin_Reporting::output_widget(
+			array(
+				'cols'      => 'd-1of5',
+				'icon'      => 'graduation-cap',
+				'id'        => 'llms-reporting-student-course-grade',
+				'data'      => $grade,
+				'data_type' => is_numeric( $grade ) ? 'percentage' : 'text',
+				'text'      => __( 'Grade', 'lifterlms' ),
+			)
+		);
+
+		do_action( 'llms_reporting_single_student_course_after_widgets', $student );
+		?>
+
+		<?php echo $table->get_table_html(); ?>
+
+	</section>
 
 </div>
 
-<?php echo $table->get_table_html(); ?>
-<?php return; ?>
-<p>
-	<?php printf( __( 'Progress: %s', 'lifterlms' ), LLMS_Admin_Reporting::get_course_data( $course, $student, 'progress' ) ); ?> |
-	<?php printf( __( 'Grade: %s', 'lifterlms' ), LLMS_Admin_Reporting::get_course_data( $course, $student, 'grade' ) ); ?> |
-	<?php printf( __( 'Completed: %s', 'lifterlms' ), LLMS_Admin_Reporting::get_course_data( $course, $student, 'completed' ) ); ?>
-</p>
+<?php do_action( 'llms_reporting_student_single_course_after_content' ); ?>
