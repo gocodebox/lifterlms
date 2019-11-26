d228fbfe02ae15a4ccfc8b715fc2ac4a90d151fd
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-11-26 14:50:37 -0800

    add tests for testing settings page

---
+++ tests/unit-tests/admin/settings/class-llms-test-settings-page.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * Test LLMS_Settings_Page
+ *
+ * @package LifterLMS/Tests
+ *
+ * @group admin
+ * @group settings_page
+ *
+ * @since [version]
+ * @version [version]
+ */
+class LLMS_Test_Settings_Page extends LLMS_Unit_Test_Case {
+
+	/**
+	 * Setup the test case.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+
+		parent::setUp();
+		include_once LLMS_PLUGIN_DIR . 'includes/admin/settings/class.llms.settings.page.php';
+
+		// Setup a mock settings page.
+		$this->page = new class() extends LLMS_Settings_Page {
+			public $id = 'mock';
+			public function get_label() {
+				return 'Mock';
+			}
+		};
+
+	}
+
+	/**
+	 * Test constructor
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_constructor() {
+
+		$this->assertEquals( 'Mock', $this->page->label );
+
+		// Tab should be registered.
+		$this->assertEquals( $this->page->tab_priority, has_action( 'lifterlms_settings_tabs_array', array( $this->page, 'add_settings_page' ) ) );
+
+		// Output action.
+		$this->assertEquals( 10, has_action( 'lifterlms_settings_mock', array( $this->page, 'output' ) ) );
+
+		// Save action.
+		$this->assertEquals( 10, has_action( 'lifterlms_settings_save_mock', array( $this->page, 'save' ) ) );
+
+	}
+
+	/**
+	 * Test add_settings_page() method.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_add_settings_page() {
+
+		// No other pages exist.
+		$this->assertEquals( array(
+			'mock' => 'Mock',
+		), $this->page->add_settings_page( array() ) );
+
+		// Another page exists, add it to the end.
+		$this->assertEquals( array(
+			'fake' => 'Fake',
+			'mock' => 'Mock',
+		), $this->page->add_settings_page( array(
+			'fake' => 'Fake',
+		) ) );
+
+	}
+
+	/**
+	 * Test generation of a settings group with no settings added.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_generate_settings() {
+
+		// No description.
+		$args = array(
+			'mock_options',
+			'Mock Settings',
+		);
+		$settings = LLMS_Unit_Test_Util::call_method( $this->page, 'generate_settings_group', $args );
+
+		$this->assertEquals( 'mock_options', $settings[0]['id'] );
+		$this->assertEquals( 'sectionstart', $settings[0]['type'] );
+
+		$this->assertEquals( 'mock_options_title', $settings[1]['id'] );
+		$this->assertEquals( 'title', $settings[1]['type'] );
+		$this->assertEquals( 'Mock Settings', $settings[1]['title'] );
+		$this->assertEquals( '', $settings[1]['desc'] );
+
+		$this->assertEquals( 'mock_options_end', $settings[2]['id'] );
+		$this->assertEquals( 'sectionend', $settings[2]['type'] );
+
+		// Has a description.
+		$args = array(
+			'mock_options',
+			'Mock Settings',
+			'Mock Settings Description',
+		);
+		$settings = LLMS_Unit_Test_Util::call_method( $this->page, 'generate_settings_group', $args );
+		$this->assertEquals( 'Mock Settings Description', $settings[1]['desc'] );
+
+		// Has a description.
+		$args = array(
+			'mock_options',
+			'Mock Settings',
+			'Mock Settings Description',
+			array(
+				array(
+					'id'   => 'mock_setting',
+					'type' => 'text',
+				),
+			)
+		);
+		$settings = LLMS_Unit_Test_Util::call_method( $this->page, 'generate_settings_group', $args );
+		$this->assertEquals( 'mock_setting', $settings[2]['id'] );
+		$this->assertEquals( 'text', $settings[2]['type'] );
+
+	}
+
+
+	/**
+	 * Test get_label() stub when no ID exists for the class.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_get_label_stub_no_id() {
+
+		// Empty string because no ID defined.
+		$page = new LLMS_Settings_Page();
+		$this->assertEquals( '', $page->get_label() );
+
+	}
+
+	/**
+	 * Test get_label() stub when an ID is set.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_get_label_stub_with_id() {
+
+		// Return ID because the method isn't overriden.
+		$page = new class() extends LLMS_Settings_Page {
+			public $id = 'mock';
+		};
+		$this->assertEquals( 'mock', $page->get_label() );
+
+	}
+
+	/**
+	 * Test the get_sections() stub.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_get_sections() {
+		$this->assertEquals( array(), $this->page->get_sections() );
+	}
+
+	/**
+	 * Test the get_settings() stub.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_get_settings_stub() {
+		$this->assertEquals( array(), $this->page->get_settings() );
+	}
+
+	/**
+	 * Test the output() stub.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_output() {
+		$this->assertOutputEmpty( array( $this->page, 'output' ) );
+	}
+
+	/**
+	 * Test the output_sections_nav() stub when no sections exist.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_output_sections_nav_empty() {
+		$this->assertOutputEmpty( array( $this->page, 'output_sections_nav' ) );
+	}
+
+	/**
+	 * Test the output_sections_nav() stub when sections do exist.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_output_sections_nav() {
+
+		$page = new class() extends LLMS_Settings_Page {
+			public $id = 'mock';
+			public function get_sections() {
+				return array(
+					'section_1' => 'Section 1',
+					'section_2' => 'Section 2',
+				);
+			}
+		};
+
+		$method = array( $page, 'output_sections_nav' );
+
+		$this->assertOutputContains( '<nav class="llms-nav-tab-wrapper llms-nav-text">', $method );
+		$this->assertOutputContains( '<ul class="llms-nav-items">', $method );
+
+		$this->assertOutputContains( 'section=section_1">Section 1</a>', $method );
+		$this->assertOutputContains( 'section=section_2">Section 2</a>', $method );
+
+		$this->assertOutputContains( '</ul>', $method );
+		$this->assertOutputContains( '</nav>', $method );
+
+	}
+
+}
