41c30d40c797a5c654e27383c43021715bcd1a21
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-11-26 14:50:26 -0800

    refactor for smaller methods

--- includes/admin/settings/class.llms.settings.engagements.php
+++ includes/admin/settings/class.llms.settings.engagements.php
@@ -44,7 +44,7 @@ class LLMS_Settings_Engagements extends LLMS_Settings_Page {
 	 *
 	 * @since 1.0.0
 	 * @since 3.8.0 Unknown.
-	 * @since [version] Ensure IDs are unique.
+	 * @since [version] Refactor to pull each settings group from its own method.
 	 *
 	 * @return array
 	 */
@@ -53,33 +53,90 @@ class LLMS_Settings_Engagements extends LLMS_Settings_Page {
 		/**
 		 * Modify LifterLMS Admin Settings on the "Engagements" tab,
 		 *
-		 * @since Unknown
+		 * @since 1.0.0
 		 *
 		 * @param array[] $settings Array of settings fields arrays.
 		 */
 		return apply_filters(
 			'lifterlms_engagements_settings',
+			array_merge(
+				$this->get_settings_group_email(),
+				$this->get_settings_group_certs()
+			)
+		);
+
+	}
+
+	/**
+	 * Retrieve fields for the certificates settings group.
+	 *
+	 * @since [version]
+	 *
+	 * @return array[]
+	 */
+	protected function get_settings_group_certs() {
+
+		return $this->generate_settings_group(
+			'certificates_options',
+			__( 'Certificate Settings', 'lifterlms' ),
+			'',
 			array(
-
 				array(
-					'type'  => 'sectionstart',
-					'id'    => 'email_options',
-					'class' => 'top',
+					'title' => __( 'Background Image Settings', 'lifterlms' ),
+					'type' => 'subtitle',
+					'desc' => __( 'Use these sizes to determine the dimensions of certificate background images. After changing these settings, you may need to <a href="http://wordpress.org/extend/plugins/regenerate-thumbnails/" target="_blank">regenerate your thumbnails</a>.', 'lifterlms' ),
+					'id'   => 'cert_bg_image_settings',
 				),
-
 				array(
-					'title' => __( 'Email Settings', 'lifterlms' ),
-					'type'  => 'title',
-					'desc'  => __( 'Settings for all emails sent by LifterLMS. Notification and engagement emails will adhere to these settings.', 'lifterlms' ),
-					'id'    => 'email_options_title',
+					'title'    => __( 'Image Width', 'lifterlms' ),
+					'desc'     => __( 'in pixels', 'lifterlms' ),
+					'id'       => 'lifterlms_certificate_bg_img_width',
+					'default'  => '800',
+					'type'     => 'number',
+					'autoload' => false,
 				),
+				array(
+					'title'    => __( 'Image Height', 'lifterlms' ),
+					'id'       => 'lifterlms_certificate_bg_img_height',
+					'desc'     => __( 'in pixels', 'lifterlms' ),
+					'default'  => '616',
+					'type'     => 'number',
+					'autoload' => false,
+				),
+				array(
+					'title'    => __( 'Legacy compatibility', 'lifterlms' ),
+					'desc'     => __( 'Use legacy certificate image sizes.', 'lifterlms' ) .
+									   '<br><em>' . __( 'Enabling this will override the above dimension settings and set the image dimensions to match the dimensions of the uploaded image.', 'lifterlms' ) . '</em>',
+					'id'       => 'lifterlms_certificate_legacy_image_size',
+					'default'  => 'no',
+					'type'     => 'checkbox',
+					'autoload' => false,
+				),
+			)
+		);
+
+	}
+
+	/**
+	 * Retrieve fields for the email settings group.
+	 *
+	 * @since [version]
+	 *
+	 * @return array[]
+	 */
+	protected function get_settings_group_email() {
+
+		return $this->generate_settings_group(
+			'email_options',
+			__( 'Email Settings', 'lifterlms' ),
+			__( 'Settings for all emails sent by LifterLMS. Notification and engagement emails will adhere to these settings.', 'lifterlms' ),
+			array(
 				array(
 					'title'    => __( 'Sender Name', 'lifterlms' ),
 					'desc'     => '<br>' . __( 'Name to be displayed in From field', 'lifterlms' ),
 					'id'       => 'lifterlms_email_from_name',
 					'type'     => 'text',
 					'default'  => esc_attr( get_bloginfo( 'title' ) ),
-					'desc_tip' => true,
 				),
 				array(
 					'title'    => __( 'Sender Email', 'lifterlms' ),
@@ -87,7 +144,6 @@ class LLMS_Settings_Engagements extends LLMS_Settings_Page {
 					'id'       => 'lifterlms_email_from_address',
 					'type'     => 'email',
 					'default'  => get_option( 'admin_email' ),
-					'desc_tip' => true,
 				),
 				array(
 					'title'    => __( 'Header Image', 'lifterlms' ),
@@ -102,69 +158,10 @@ class LLMS_Settings_Engagements extends LLMS_Settings_Page {
 					'id'       => 'lifterlms_email_footer_text',
 					'type'     => 'textarea',
 					'default'  => '',
-					'desc_tip' => true,
 				),
-
-				array(
-					'type' => 'sectionend',
-					'id'   => 'email_options_end',
-				),
-
-				array(
-					'type'  => 'sectionstart',
-					'id'    => 'certificates_options_start',
-					'class' => 'top',
-				),
-
-				array(
-					'title' => __( 'Certificates Settings', 'lifterlms' ),
-					'type'  => 'title',
-					'desc'  => '',
-					'id'    => 'certificates_options_title',
-				),
-
-				array(
-					'type' => 'desc',
-					'desc' => '<strong>' . __( 'Background Image Settings', 'lifterlms' ) . '</strong><br>' .
-							  __( 'Use these sizes to determine the dimensions of certificate background images. After changing these settings, you may need to <a href="http://wordpress.org/extend/plugins/regenerate-thumbnails/" target="_blank">regenerate your thumbnails</a>.', 'lifterlms' ),
-					'id'   => 'cert_bg_image_settings',
-				),
-
-				array(
-					'title'    => __( 'Image Width', 'lifterlms' ),
-					'desc'     => __( 'in pixels', 'lifterlms' ),
-					'id'       => 'lifterlms_certificate_bg_img_width',
-					'default'  => '800',
-					'type'     => 'number',
-					'autoload' => false,
-				),
-
-				array(
-					'title'    => __( 'Image Height', 'lifterlms' ),
-					'id'       => 'lifterlms_certificate_bg_img_height',
-					'desc'     => __( 'in pixels', 'lifterlms' ),
-					'default'  => '616',
-					'type'     => 'number',
-					'autoload' => false,
-				),
-
-				array(
-					'title'    => __( 'Legacy compatibility', 'lifterlms' ),
-					'desc'     => __( 'Use legacy certificate image sizes.', 'lifterlms' ) .
-									   '<br><em>' . __( 'Enabling this will override the above dimension settings and set the image dimensions to match the dimensions of the uploaded image.', 'lifterlms' ) . '</em>',
-					'id'       => 'lifterlms_certificate_legacy_image_size',
-					'default'  => 'no',
-					'type'     => 'checkbox',
-					'autoload' => false,
-				),
-
-				array(
-					'type' => 'sectionend',
-					'id'   => 'certificates_options_end',
-				),
-
 			)
 		);
+
 	}
 
 }
