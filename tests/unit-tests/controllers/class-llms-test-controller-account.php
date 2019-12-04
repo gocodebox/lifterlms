14f9d29e0cf73e720c49ad8f5e024c37ee03229e
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-12-04 14:38:33 -0800

    fix and improve tests in light of forms updates

--- tests/unit-tests/controllers/class-llms-test-controller-account.php
+++ tests/unit-tests/controllers/class-llms-test-controller-account.php
@@ -3,9 +3,12 @@
  * Tests for the LLMS_Controller_Account class
  *
  * @group controllers
+ * @group controller_account
  *
  * @since 3.19.0
  * @since 3.34.0 Use `LLMS_Unit_Test_Exception_Exit` from tests lib.
+ * @since [version] Create forms before executing tests account update tests.
+ *               Split account update assertions into multiple tests.
  */
 class LLMS_Test_Controller_Account extends LLMS_UnitTestCase {
 
@@ -80,28 +83,29 @@ class LLMS_Test_Controller_Account extends LLMS_UnitTestCase {
 	}
 
 	/**
-	 * Test account update form submission handler
+	 * Test account update form submission handler when form is not submitted
 	 *
-	 * @since 3.19.4
-	 * @since 3.34.0 Use `LLMS_Unit_Test_Exception_Exit` from test lib.
+	 * @since [version]
 	 *
-	 * @return   void
+	 * @return void
 	 */
-	public function test_update() {
+	public function test_update_not_submitted() {
 
-		LLMS_Install::create_pages();
-
-		// form not submitted
 		$this->setup_post( array() );
 		do_action( 'init' );
 		$this->assertEquals( 0, did_action( 'llms_before_user_account_update_submit' ) );
 		$this->assertEquals( 0, did_action( 'lifterlms_user_updated' ) );
 
-		// also not submitted
-		$this->setup_get( array() );
-		do_action( 'init' );
-		$this->assertEquals( 0, did_action( 'llms_before_user_account_update_submit' ) );
-		$this->assertEquals( 0, did_action( 'lifterlms_user_updated' ) );
+	}
+
+	/**
+	 * Test account update form submission handler when user is not logged in
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_update_no_user() {
 
 		// form submitted but user isn't logged in
 		$this->setup_post( array(
@@ -113,6 +117,17 @@ class LLMS_Test_Controller_Account extends LLMS_UnitTestCase {
 		$this->assertEquals( 0, did_action( 'lifterlms_user_updated' ) );
 		llms_clear_notices();
 
+	}
+
+	/**
+	 * Test account update form submission handler when missing required fields.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_update_missing_fields() {
+
 		// create a user
 		$uid = $this->factory->user->create();
 		// sign the user in
@@ -123,10 +138,31 @@ class LLMS_Test_Controller_Account extends LLMS_UnitTestCase {
 			'_llms_update_person_nonce' => wp_create_nonce( 'llms_update_person' ),
 		) );
 		do_action( 'init' );
-		$this->assertEquals( 2, did_action( 'llms_before_user_account_update_submit' ) );
+		$this->assertEquals( 1, did_action( 'llms_before_user_account_update_submit' ) );
 		$this->assertTrue( ( llms_notice_count( 'error' ) >= 1 ) );
 		$this->assertEquals( 0, did_action( 'lifterlms_user_updated' ) );
 		llms_clear_notices();
+
+	}
+
+	/**
+	 * Test account update form submission handler
+	 *
+	 * @since [version]
+	 *
+	 * @runInSeparateProcess
+	 *
+	 * @return void
+	 */
+	public function test_update_success() {
+
+		LLMS_Install::create_pages();
+		LLMS_Forms::instance()->install();
+
+		// create a user
+		$uid = $this->factory->user->create();
+		// sign the user in
+		wp_set_current_user( $uid );
 
 		// update something
 		$this->setup_post( array(
@@ -150,7 +186,7 @@ class LLMS_Test_Controller_Account extends LLMS_UnitTestCase {
 		// run these assertions within actions because the exit() at the end of the redirect will halt program execution
 		// and then we'll never get to these assertions!
 		add_action( 'llms_before_user_account_update_submit', function() {
-			$this->assertEquals( 3, did_action( 'llms_before_user_account_update_submit' ) );
+			$this->assertEquals( 1, did_action( 'llms_before_user_account_update_submit' ) );
 			$this->assertEquals( 0, llms_notice_count( 'error' ) );
 		} );
 		add_action( 'lifterlms_user_updated', function() {
