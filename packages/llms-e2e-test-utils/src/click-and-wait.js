67cefde9159df356fce3851d75a4124b69f6f43e
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-01-21 12:38:57 -0800

    more e2e wips

--- packages/llms-e2e-test-utils/src/click-and-wait.js
+++ packages/llms-e2e-test-utils/src/click-and-wait.js
@@ -14,4 +14,5 @@ export async function clickAndWait( selector, waitUntil ) {
 		page.$eval( selector, el => el.click() ),
 		page.waitForNavigation( { waitUntil: waitUntil } ),
 	] );
+
 }
--- packages/llms-e2e-test-utils/src/index.js
+++ packages/llms-e2e-test-utils/src/index.js
@@ -1,6 +1,8 @@
 export { clickAndWait } from './click-and-wait';
+// export { clickElementByText } from './click-element-by-text';
 export { createUser } from './create-user';
 export { fillField } from './fill-field';
+// export { findElementByText } from './find-element-by-text';
 export { loginStudent } from './login-student';
 export { logoutUser } from './logout-user';
 export { visitPage } from './visit-page';
