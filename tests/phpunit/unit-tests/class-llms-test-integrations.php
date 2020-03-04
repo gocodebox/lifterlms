c428206da9166e7de3493f80f37f46375dc618a5
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-03-04 15:54:33 -0800

    update tests to accommodate Akismet built-in integration

--- tests/phpunit/unit-tests/class-llms-test-integrations.php
+++ tests/phpunit/unit-tests/class-llms-test-integrations.php
@@ -1,17 +1,20 @@
 <?php
 /**
  * Tests for the LLMS_Integrations class
- * @group    integrations
- * @since    3.19.0
- * @version  3.19.0
+ *
+ * @group integrations
+ *
+ * @since 3.19.0
+ * @since [version] Update to accommodate Akismet.
  */
 class LLMS_Test_Integrations extends LLMS_UnitTestCase {
 
 	/**
 	 * test instance() method
-	 * @return   void
-	 * @since    3.19.0
-	 * @version  3.19.0
+	 *
+	 * @since 3.19.0\
+	 *
+	 * @return void
 	 */
 	public function test_instance() {
 
@@ -19,39 +22,40 @@ class LLMS_Test_Integrations extends LLMS_UnitTestCase {
 
 	}
 
-	// public function test_get_integration() {}
-
 	/**
 	 * test init() method
-	 * @return   void
-	 * @since    3.19.0
-	 * @version  3.19.0
+	 *
+	 * @since 3.19.0
+	 *
+	 * @return void
 	 */
 	public function test_init() {
 
 		$instance = LLMS()->integrations();
-		$this->assertEquals( 2, count( $instance->integrations() ) );
+		$this->assertEquals( 3, count( $instance->integrations() ) );
 
 	}
 
 	/**
 	 * Test get available integrations
-	 * @return   void
-	 * @since    3.19.0
-	 * @version  3.19.0
+	 *
+	 * @since 3.19.0
+	 * @since [version] Update to accommodate Akismet.
+	 *
+	 * @return void
 	 */
 	public function test_get_available_integrations() {
 
 		$instance = LLMS()->integrations();
 		$this->assertEquals( array(), $instance->get_available_integrations() );
 
-		// enable an integration
+		// Enable an integration.
 		update_option( 'llms_integration_bbpress_enabled', 'yes' );
 
-		// option is enabled but it's not available b/c deps. don't exist
+		// Option is enabled but it's not available b/c deps. don't exist.
 		$this->assertEquals( 0, count( $instance->get_available_integrations() ) );
 
-		// deps exist now
+		// Deps exist now.
 		$stub = $this->getMockBuilder( 'bbPress' )->getMock();
 		$this->assertEquals( 1, count( $instance->get_available_integrations() ) );
 
@@ -59,14 +63,16 @@ class LLMS_Test_Integrations extends LLMS_UnitTestCase {
 
 	/**
 	 * Test integrations() method
-	 * @return   [type]
-	 * @since    3.19.0
-	 * @version  3.19.0
+	 *
+	 * @since 3.19.0
+	 * @since [version] Update to accommodate Akismet.
+	 *
+	 * @return void
 	 */
 	public function test_integrations() {
 
 		$instance = LLMS()->integrations();
-		$this->assertEquals( 2, count( $instance->integrations() ) );
+		$this->assertEquals( 3, count( $instance->integrations() ) );
 
 	}
 
