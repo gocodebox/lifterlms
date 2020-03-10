d9adcde96ba3d033621b341f39f83fe985d40611
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-03-10 11:57:26 -0700

    fix docs and phpcs warnings for strict comparisons

--- includes/class.llms.comments.php
+++ includes/class.llms.comments.php
@@ -1,25 +1,39 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; }
 /**
  * Custom filters & actions for LifterLMS Comments
  *
- * @since  3.0.0
- * @version  3.0.0
+ * @since 3.0.0
+ * @version [version]
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * LLMS_Comments class
+ *
+ * @since 3.0.0
+ * @since [version] Use strict comparators.
  */
 class LLMS_Comments {
 
+	/**
+	 * Constructor.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
 	public function __construct() {
 
-		// secure order notes
+		// Secure order notes.
 		add_filter( 'comments_clauses', array( __CLASS__, 'exclude_order_comments' ), 10, 1 );
 		add_action( 'comment_feed_join', array( __CLASS__, 'exclude_order_comments_from_feed_join' ) );
 		add_action( 'comment_feed_where', array( __CLASS__, 'exclude_order_comments_from_feed_where' ) );
 
-		// remove order notes when counting comments
+		// Remove order notes when counting comments.
 		add_filter( 'wp_count_comments', array( __CLASS__, 'wp_count_comments' ), 777, 2 );
 
-		// Delete comments count cache whenever there is a new comment or a comment status changes
+		// Delete comments count cache whenever there is a new comment or a comment status changes.
 		add_action( 'wp_insert_comment', array( __CLASS__, 'delete_comments_count_cache' ) );
 		add_action( 'wp_set_comment_status', array( __CLASS__, 'delete_comments_count_cache' ) );
 
@@ -27,12 +41,12 @@ class LLMS_Comments {
 
 	/**
 	 * Delete transient data when inserting new comments or updating comment status
+	 *
 	 * Next time wp_count_comments is called it'll be automatically regenerated
 	 *
-	 * @return   void
-	 * @since    3.0.0
-	 * @version  3.0.0
-	 * @note thanks WooCommerce :-D
+	 * @since 3.0.0
+	 *
+	 * @return void
 	 */
 	public static function delete_comments_count_cache() {
 		delete_transient( 'llms_count_comments' );
@@ -41,18 +55,18 @@ class LLMS_Comments {
 	/**
 	 * Exclude order comments from queries and RSS.
 	 *
-	 * @param  array $clauses
+	 * @since 3.0.0
+	 * @since [version] Use strict comparison for `in_array()`.
+	 *
+	 * @param array $clauses Array of SQL clauses.
 	 * @return array
-	 * @since  3.0.0
-	 * @version  3.0.0
-	 * @see thanks WooCommerce :-D
 	 */
 	public static function exclude_order_comments( $clauses ) {
 
 		global $wpdb, $typenow;
 
-		// allow queries when in the admin
-		if ( is_admin() && in_array( $typenow, array( 'llms_order' ) ) && current_user_can( apply_filters( 'lifterlms_admin_order_access', 'manage_options' ) ) ) {
+		// Allow queries when in the admin.
+		if ( is_admin() && in_array( $typenow, array( 'llms_order' ), true ) && current_user_can( apply_filters( 'lifterlms_admin_order_access', 'manage_options' ) ) ) {
 			return $clauses;
 		}
 
@@ -71,16 +85,16 @@ class LLMS_Comments {
 		$clauses['where'] .= " $wpdb->posts.post_type NOT IN ('" . implode( "','", array( 'llms_order' ) ) . "') ";
 
 		return $clauses;
+
 	}
 
 	/**
 	 * Exclude order comments from queries and RSS.
 	 *
-	 * @param  string $join
+	 * @since 3.0.0
+	 *
+	 * @param string $join SQL join clause.
 	 * @return string
-	 * @since  3.0.0
-	 * @version  3.0.0
-	 * @see thanks WooCommerce :-D
 	 */
 	public static function exclude_order_comments_from_feed_join( $join ) {
 		global $wpdb;
@@ -93,11 +107,10 @@ class LLMS_Comments {
 	/**
 	 * Exclude order comments from queries and RSS.
 	 *
-	 * @param  string $where
+	 * @since 3.0.0
+	 *
+	 * @param string $where SQL where clause.
 	 * @return string
-	 * @since  3.0.0
-	 * @version  3.0.0
-	 * @see thanks WooCommerce :-D
 	 */
 	public static function exclude_order_comments_from_feed_where( $where ) {
 		global $wpdb;
@@ -111,19 +124,19 @@ class LLMS_Comments {
 	/**
 	 * Remove order notes from the count when counting comments
 	 *
-	 * @param    object $stats    original comment stats
-	 * @param    int    $post_id  WP Post ID
-	 * @return   object
-	 * @since    3.0.0
-	 * @version  3.0.0
-	 * @see thanks WooCommerce :-D
+	 * @since 3.0.0
+	 * @since [version] Use strict comparisons.
+	 *
+	 * @param obj $stats   Original comment stats.
+	 * @param int $post_id WP Post ID
+	 * @return obj
 	 */
 	public static function wp_count_comments( $stats, $post_id ) {
 		global $wpdb;
 		if ( 0 === $post_id ) {
 			$trans = get_transient( 'llms_count_comments' );
 			if ( ! $trans ) {
-				$count    = $wpdb->get_results( "SELECT comment_approved, COUNT( * ) AS num_comments FROM {$wpdb->comments} WHERE comment_type = 'llms_order_note' GROUP BY comment_approved;", ARRAY_A );
+				$count    = $wpdb->get_results( "SELECT comment_approved, COUNT( * ) AS num_comments FROM {$wpdb->comments} WHERE comment_type = 'llms_order_note' GROUP BY comment_approved;", ARRAY_A ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
 				$approved = array(
 					'0'            => 'moderated',
 					'1'            => 'approved',
@@ -132,8 +145,8 @@ class LLMS_Comments {
 					'post-trashed' => 'post-trashed',
 				);
 				foreach ( $count as $row ) {
-					// Don't count post-trashed toward totals
-					if ( 'post-trashed' != $row['comment_approved'] && 'trash' != $row['comment_approved'] ) {
+					// Don't count post-trashed toward totals.
+					if ( 'post-trashed' !== $row['comment_approved'] && 'trash' !== $row['comment_approved'] ) {
 						$stats->total_comments -= $row['num_comments'];
 					}
 
@@ -149,7 +162,6 @@ class LLMS_Comments {
 		}
 		return $stats;
 	}
-
 
 }
 return new LLMS_Comments();
