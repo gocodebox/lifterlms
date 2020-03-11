88e55886c7347eae659be3dd816dde6c8da4f055
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-03-11 10:17:41 -0700

    add deprecation warning

--- includes/class.llms.course.basic.php
+++ includes/class.llms.course.basic.php
@@ -1,32 +1,43 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; }
-
 /**
  * Basic course class
+ *
  * Extends LLMS_Course
  *
  * Basic course is the "standard" single purchase course.
  *
+ * @package LifterLMS/Classes
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * LLMS_Course_Basic
+ *
+ * @since 1.0.0
  * @deprecated 3.30.3
  */
 class LLMS_Course_Basic extends LLMS_Course {
 
 	/**
+	 * Course Type
+	 *
 	 * @var string
-	 * @since 1.0.0
 	 */
 	public $course_type;
 
 	/**
-	 * post id
+	 * Course Post ID
 	 *
 	 * @var int
 	 */
 	public $id;
 
 	/**
-	 * post object
+	 * Course post object
 	 *
 	 * @var object
 	 */
@@ -34,11 +45,18 @@ class LLMS_Course_Basic extends LLMS_Course {
 
 	/**
 	 * Constructor
+	 *
 	 * Inherits from LLMS_Course
 	 *
-	 * @param object $course [The course object]
+	 * @since 1.0.0
+	 * @deprecated 3.30.3
+	 *
+	 * @param object $course Course object.
+	 * @return void
 	 */
 	public function __construct( $course ) {
+
+		llms_deprecated_function( 'LLMS_Course_Basic::__construct()', '3.30.3' );
 
 		$this->course_type = 'basic';
 		parent::__construct( $course );
