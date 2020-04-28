7cc2c23ca0508c13fcdc4e81136f791f25ac4be5
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-04-28 13:27:47 -0700

    add tests

--- tests/phpunit/unit-tests/functions/class-llms-test-functions-page.php
+++ tests/phpunit/unit-tests/functions/class-llms-test-functions-page.php
@@ -41,4 +41,49 @@ class LLMS_Test_Functions_Fage extends LLMS_UnitTestCase {
 
 	}
 
+	/**
+	 * Test the llms_get_page_id() function.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_llms_get_page_id() {
+
+		$pages = array(
+			'checkout' => 'checkout',
+			'courses' => 'shop',
+			'myaccount' => 'myaccount',
+			'memberships' => 'memberships',
+		);
+
+		// Options don't exist.
+
+		// Backwards compat.
+		$this->assertEquals( -1, llms_get_page_id( 'shop' ) );
+
+		foreach ( array_keys( $pages ) as $slug ) {
+			$this->assertEquals( -1, llms_get_page_id( $slug ) );
+		}
+
+		// Options do exist.
+		LLMS_Install::create_pages();
+
+		// Backwards compat.
+		$this->assertEquals( get_option( 'lifterlms_shop_page_id' ), llms_get_page_id( 'shop' ) );
+
+		foreach ( $pages as $slug => $option ) {
+
+			$id = llms_get_page_id( $slug );
+
+			// Number.
+			$this->assertTrue( is_int( $id ) );
+
+			// Equals expected option value.
+			$this->assertEquals( get_option( 'lifterlms_' . $option . '_page_id' ), $id );
+
+		}
+
+	}
+
 }
