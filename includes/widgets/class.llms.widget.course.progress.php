52fdbaf61d5b8ac24f0757213565a068da56c288
Rocco Aliberti <raliberti84@gmail.com>
2020-04-28 22:48:15 +0200

    Add attribute check_enrollment to the llms_course_progress shortcode Also add a new option to the course progress widget to display the progressbar only to enrolled students: enabled by default - this seem reasonable because in most of the cases that widget is displayed in lessons sidebars, that, except for the free lessons, are already displayed to the enrolled users only.

--- includes/widgets/class.llms.widget.course.progress.php
+++ includes/widgets/class.llms.widget.course.progress.php
@@ -7,7 +7,7 @@
  * @package LifterLMS/Widgets/Classes
  *
  * @since 1.0.0
- * @version 1.0.0
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -16,6 +16,8 @@ defined( 'ABSPATH' ) || exit;
  * LLMS_Widget_Course_Progress
  *
  * @since 1.0.0
+ * @since [version] Introduced a new option to display/hide the course progress widget to enrolled students only.
+ *                Hidden to not enrolled students by default.
  */
 class LLMS_Widget_Course_Progress extends LLMS_Widget {
 
@@ -39,16 +41,69 @@ class LLMS_Widget_Course_Progress extends LLMS_Widget {
 	}
 
 	/**
+	 * Back-end widget form.
+	 *
+	 * @since [version]
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+
+		// Call widget defaults from parent.
+		parent::form( $instance );
+		$check_enrollment = ( isset( $instance['check_enrollment'] ) ) ? $instance['check_enrollment'] : 1;
+		?>
+		<p>
+			<input <?php checked( 1, $check_enrollment ); ?> class="checkbox llms-course-progress-check-enrollment" id="<?php echo $this->get_field_id( 'check_enrollment' ); ?>" name="<?php echo $this->get_field_name( 'check_enrollment' ); ?>" type="checkbox" value="1">
+			<label for="<?php echo $this->get_field_id( 'check_enrollment' ); ?>">
+				<?php _e( 'Display to enrolled students only?', 'lifterlms' ); ?><br>
+				<em><?php _e( 'When checked the course progress bar will be shown only to those students enrolled in the course.', 'lifterlms' ); ?></em>
+			</label>
+		</p>
+		<?php
+	}
+
+
+	/**
 	 * Widget Content
 	 *
 	 * Overrides parent class
 	 *
 	 * @since 1.0.0
+	 * @since [version] Added the logic to display/hide the course progress widget to enrolled students only, according to the new option.
+	 *                Hidden to not enrolled students by default.
 	 *
+	 * @see LLMS_Widget::widget_contents()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
 	 * @return void
 	 */
 	public function widget_contents( $args, $instance ) {
-		echo do_shortcode( '[lifterlms_course_progress]' );
+		$check_enrollment = ( ! isset( $instance['check_enrollment'] ) ) ? 1 : $instance['check_enrollment'];
+		echo do_shortcode( '[lifterlms_course_progress check_enrollment=' . $check_enrollment . ']' );
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @since [version]
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = parent::update( $new_instance, $old_instance );
+
+		$instance['check_enrollment'] = ( ! empty( $new_instance['check_enrollment'] ) ) ? 1 : 0;
+
+		return $instance;
 	}
 
 }
