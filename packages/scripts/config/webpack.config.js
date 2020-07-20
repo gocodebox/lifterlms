5a1529d5e973f6370a106809a908eb376c61b67f
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-07-20 10:58:51 -0700

    reduce code complexity by splitting logic into helper methods

--- packages/scripts/config/webpack.config.js
+++ packages/scripts/config/webpack.config.js
@@ -1,3 +1,13 @@
+/**
+ * Webpack config
+ *
+ * @package LifterLMS_Groups/Scripts/Dev
+ *
+ * @since 1.3.0
+ * @version [version]
+ */
+
+// Deps.
 const
 	cssExtract = require( 'mini-css-extract-plugin' ),
 	cssRTL     = require( 'webpack-rtl-plugin' ),
@@ -6,13 +16,101 @@ const
 	path       = require( 'path' );
 
 /**
- * Webpack config
+ * Used by dependency extractor to handle requests to convert names of scripts included in the LifterLMS Core.
  *
- * @package LifterLMS_Groups/Scripts/Dev
+ * @since [version]
  *
- * @since 1.3.0
- * @version 1.3.0
+ * @param {string} request External script slug/id.
+ * @return {String|Array} A string
  */
+function requestToExternal( request ) {
+
+	if ( 'llms-quill' === request ) {
+		return 'Quill';
+	} else if ( 'llms-izimodal' === request ) {
+		return [ 'jQuery', 'iziModal' ];
+	} else if ( request.startsWith( 'llms/' ) || request.startsWith( 'LLMS/' ) ) {
+		return request.split( '/' );
+	}
+
+}
+
+/**
+ * Used by dependency extractor to handle requests to scripts included in the LifterLMS Core.
+ *
+ * @since [version]
+ *
+ * @param {string} request External script slug/id.
+ * @return {String|Array} A string
+ */
+function requestToHandle( request ) {
+	if ( request.startsWith( 'llms/' ) || request.startsWith( 'LLMS/' ) ) {
+		return 'llms';
+	}
+}
+
+/**
+ * Configure the `entry` object of the webpack config file.
+ *
+ * @since [version]
+ *
+ * @param {String[]} js     Array of JS file slugs.
+ * @return {Object} Webpack config entry object.
+ */
+function setupEntry( js ) {
+
+	const entry = {};
+	js.forEach( file => {
+		entry[ file ] = path.resolve( process.cwd(), 'assets/src/js/', `${ file }.js` );
+	} );
+
+	return entry;
+
+}
+
+/**
+ * Setup the `plugins` array of the webpack config file.
+ *
+ * @since [version]
+ *
+ * @param {Object[]} plugins Array of plugin objects or classes.
+ * @param {String[]} css     Array of CSS file slugs.
+ * @param {String}   prefix  File prefix.
+ * @return {Object[]} Array of plugin objects or classes.
+ */
+function setupPlugins( plugins, css, prefix ) {
+
+	// Delete the css extractor implemented in the default config (we'll replace it with our own later).
+	plugins.forEach( ( plugin, index ) => {
+		if ( 'MiniCssExtractPlugin' === plugin.constructor.name ) {
+			config.plugins.splice( index, 1 );
+		}
+	} );
+
+	css.forEach( file => {
+
+		// Extract CSS.
+		plugins.push( new cssExtract( {
+			filename: `css/${ prefix }[name].css`,
+		} ) );
+
+		// Generate an RTL CSS file.
+		plugins.push( new cssRTL( {
+			filename: `css/${ prefix }[name]-rtl.css`,
+		} ) );
+
+	} );
+
+	// Add a custom dependency extractor.
+	plugins.push( new depExtract( {
+		requestToExternal,
+		requestToHandle,
+		injectPolyfill: true,
+	} ) );
+
+	return plugins;
+
+}
 
 /**
  * Generates a Webpack config object
@@ -26,68 +124,24 @@ const
  * SASS files should be imported via the JS source file.
  *
  * @since 1.3.0
+ * @since [version] Reduce method size by using helper methods
  *
- * @param {String[]} options.css  Array of CSS file slugs.
- * @param {String[]} options.js   Array of JS file slugs.
- * @param {String} options.prefix File prefix.
+ * @param {String[]} options.css        Array of CSS file slugs.
+ * @param {String[]} options.js         Array of JS file slugs.
+ * @param {String}   options.prefix     File prefix.
+ * @param {String}   options.outputPath Relative path to the output directory.
  * @return {Object} A webpack.config.js object.
  */
-module.exports = ( { css = [], js = [], prefix = 'llms-' } ) => {
-
-	// Setup entry files.
-	const entry = {};
-	js.forEach( file => {
-		entry[ file ] = path.resolve( process.cwd(), 'assets/src/js/', `${ file }.js` );
-	} );
-
-	const plugins = config.plugins;
-	// Delete the css extractor implemented in the default config (we'll replace it with our own later).
-	plugins.forEach( ( plugin, index ) => {
-		if ( 'MiniCssExtractPlugin' === plugin.constructor.name ) {
-			config.plugins.splice( index, 1 );
-		}
-	} );
-
-	// Setup CSS extraction & generate RTL files.
-	css.forEach( file => {
-
-		plugins.push( new cssExtract( {
-			filename: `css/${ prefix }[name].css`,
-		} ) );
-
-		plugins.push( new cssRTL( {
-			filename: `css/${ prefix }[name]-rtl.css`,
-		} ) );
-
-	} );
-
-
-	plugins.push( new depExtract( {
-		injectPolyfill: true,
-		requestToExternal: request => {
-			if ( 'llms-quill' === request ) {
-				return 'Quill';
-			} else if ( 'llms-izimodal' === request ) {
-				return [ 'jQuery', 'iziModal' ];
-			} else if ( request.startsWith( 'llms/' ) || request.startsWith( 'LLMS/' ) ) {
-				return request.split( '/' );
-			}
-		},
-		requestToHandle: request => {
-			if ( request.startsWith( 'llms/' ) || request.startsWith( 'LLMS/' ) ) {
-				return 'llms';
-			}
-		}
-	} ) );
+module.exports = ( { css = [], js = [], prefix = 'llms-', outputPath = 'assets/' } ) => {
 
 	return {
 		...config,
-		entry,
+		entry: setupEntry( js ),
 		output: {
 			filename: `js/${ prefix }[name].js`,
-			path: path.resolve( process.cwd(), 'assets/' ),
+			path: path.resolve( process.cwd(), outputPath ),
 		},
-		plugins,
+		plugins: setupPlugins( config.plugins, css, prefix ),
 	};
 
 }
