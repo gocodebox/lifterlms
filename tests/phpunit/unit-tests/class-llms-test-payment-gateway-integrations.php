4e245fc0986f7ebe3366ee4220a171ae15f1129b
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-07-30 11:45:58 -0700

    increase delta on multi-month chaos

--- tests/phpunit/unit-tests/class-llms-test-payment-gateway-integrations.php
+++ tests/phpunit/unit-tests/class-llms-test-payment-gateway-integrations.php
@@ -7,7 +7,7 @@
  * @since 3.37.6
  * @since 3.37.12 Added additional assertion message information to assist in debug chaos-related failures.
  * @since 3.37.14 Reduce number of tests run for monthly and yearly chaotic simulations.
- * @since [version] Increased delta for `test_recurring_lifecycle_for_month_plan_with_chaos_and_frequency()`.
+ * @since [version] Increased delta for `test_recurring_lifecycle_for_month_plan_with_chaos_and_frequency()` and `test_recurring_lifecycle_for_month_plan_with_chaos()`.
  */
 class LLMS_Test_Payment_Gateway_Integrations extends LLMS_UnitTestCase {
 
@@ -453,6 +453,7 @@ class LLMS_Test_Payment_Gateway_Integrations extends LLMS_UnitTestCase {
 	 *
 	 * @since 3.37.6
 	 * @since 3.37.14 Reduce number of tests run.
+	 * @since [version] Increased delta from 24 to 48 hours.
 	 *
 	 * @medium
 	 *
@@ -469,7 +470,7 @@ class LLMS_Test_Payment_Gateway_Integrations extends LLMS_UnitTestCase {
 		$this->do_order_setup_tests( $order );
 
 		// Run recurring charges for the order.
-		$this->do_n_charges_for_order( $order, 50, 12, 24 );
+		$this->do_n_charges_for_order( $order, 50, 12, 48 );
 
 	}
 
