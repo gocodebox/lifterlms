009636edd46a273bb965cd6a7c207ca6b5961c75
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-09-29 15:16:51 -0700

    add a delay before logging out, fixes page context destroyed errors

--- packages/llms-e2e-test-utils/src/logout-user.js
+++ packages/llms-e2e-test-utils/src/logout-user.js
@@ -11,11 +11,13 @@ const { clickAndWait } = require( './click-and-wait' );
 /**
  * Logout the current user.
  *
- * @since  3.37.8
+ * @since 3.37.8
+ * @since [version] Wait 1 second before navigating to logout page.
  *
  * @return {void}
  */
 export async function logoutUser() {
+	await page.waitFor( 1000 );
 	await page.goto( createURL( 'wp-login.php', 'action=logout' ) );
 	await clickAndWait( 'a' );
 }
