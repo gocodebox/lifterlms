5567cb6fa9dadd6425abf86aecb985775f76a532
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-10-06 09:10:20 -0700

    update docs

--- includes/functions/llms.functions.person.php
+++ includes/functions/llms.functions.person.php
@@ -7,7 +7,7 @@
  * @package LifterLMS/Functions
  *
  * @since 1.0.0
- * @version 4.0.0
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -15,11 +15,11 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Checks LifterLMS user capabilities against an object
  *
- * @param    string $cap     capability name
- * @param    int    $obj_id  WP_Post or WP_User ID
- * @return   boolean
- * @since    3.13.0
- * @version  3.13.0
+ * @since 3.13.0
+ *
+ * @param string $cap    Capability name.
+ * @param int    $obj_id WP_Post or WP_User ID.
+ * @return boolean
  */
 function llms_current_user_can( $cap, $obj_id = null ) {
 
@@ -28,7 +28,7 @@ function llms_current_user_can( $cap, $obj_id = null ) {
 
 	if ( in_array( $cap, $caps ) ) {
 
-		// if the user has the cap, maybe do some additional checks
+		// If the user has the cap, maybe do some additional checks.
 		if ( current_user_can( $cap ) ) {
 
 			switch ( $cap ) {
@@ -66,7 +66,7 @@ function llms_current_user_can( $cap, $obj_id = null ) {
 
 			}
 		}
-	}// End if().
+	}
 
 	return apply_filters( 'llms_current_user_can_' . $cap, $grant, $obj_id );
 
@@ -75,10 +75,11 @@ function llms_current_user_can( $cap, $obj_id = null ) {
 /**
  * Determine whether or not a user can bypass enrollment, drip, and prerequisite restrictions
  *
- * @param    LLMS_Student|WP_User|int $user LLMS_Student, WP_User, or WP User ID, if none supplied get_current_user() will be used
- * @return   boolean
- * @since    3.7.0
- * @version  3.9.0
+ * @since 3.7.0
+ * @since 3.9.0
+ *
+ * @param LLMS_Student|WP_User|int $user LLMS_Student, WP_User, or WP User ID, if none supplied get_current_user() will be used.
+ * @return boolean
  */
 function llms_can_user_bypass_restrictions( $user = null ) {
 
@@ -104,10 +105,11 @@ function llms_can_user_bypass_restrictions( $user = null ) {
 /**
  * Disables admin bar on front end
  *
- * @param  bool $show_admin_bar default value (true).
+ * @since 1.0.0
+ * @since 3.27.0 Unknown
+ *
+ * @param bool $show_admin_bar default value (true).
  * @return bool
- * @since  1.0.0
- * @version 3.27.0
  */
 function llms_disable_admin_bar( $show_admin_bar ) {
 	if ( apply_filters( 'lifterlms_disable_admin_bar', true ) && ! ( current_user_can( 'edit_posts' ) || current_user_can( 'manage_lifterlms' ) ) ) {
@@ -121,15 +123,15 @@ add_filter( 'show_admin_bar', 'llms_disable_admin_bar', 10 );
 /**
  * Enroll a WordPress user in a course or membership
  *
- * @param  int    $user_id    WP User ID
- * @param  int    $product_id WP Post ID of the Course or Membership
- * @param  string $trigger    String describing the event that triggered the enrollment
- * @return bool
+ * @since 2.2.3
+ * @since 3.0.0 Added `$trigger` parameter.
  *
- * @see  LLMS_Student->enroll() the class method wrapped by this function
+ * @see LLMS_Student->enroll() the class method wrapped by this function
  *
- * @since  2.2.3
- * @version 3.0.0 added $trigger parameter
+ * @param int    $user_id    WP User ID.
+ * @param int    $product_id WP Post ID of the Course or Membership.
+ * @param string $trigger    String describing the event that triggered the enrollment.
+ * @return boolean
  */
 function llms_enroll_student( $user_id, $product_id, $trigger = 'unspecified' ) {
 	$student = new LLMS_Student( $user_id );
@@ -139,10 +141,10 @@ function llms_enroll_student( $user_id, $product_id, $trigger = 'unspecified' ) 
 /**
  * Get an LLMS_Instructor
  *
- * @param    mixed $user  WP_User ID, instance of WP_User, or instance of any instructor class extending this class
- * @return   LLMS_Instructor|false LLMS_Instructor instance on success, false if user not found
- * @since    3.13.0
- * @version  3.13.0
+ * @since 3.13.0
+ *
+ * @param mixed $user WP_User ID, instance of WP_User, or instance of any instructor class extending this class.
+ * @return LLMS_Instructor|false LLMS_Instructor instance on success, false if user not found
  */
 function llms_get_instructor( $user = null ) {
 	$student = new LLMS_Instructor( $user );
@@ -152,8 +154,9 @@ function llms_get_instructor( $user = null ) {
 /**
  * Retrieve the minimum accepted password strength for student passwords
  *
+ * @since 3.0.0
+ *
  * @return string
- * @since  3.0.0
  */
 function llms_get_minimum_password_strength() {
 	return apply_filters( 'llms_get_minimum_password_strength', get_option( 'lifterlms_registration_password_min_strength' ) );
@@ -162,8 +165,9 @@ function llms_get_minimum_password_strength() {
 /**
  * Retrieve the translated name of minimum accepted password strength for student passwords
  *
+ * @since 3.0.0
+ *
  * @return string
- * @since  3.0.0
  */
 function llms_get_minimum_password_strength_name() {
 	$strength = llms_get_minimum_password_strength();
@@ -194,10 +198,11 @@ function llms_get_minimum_password_strength_name() {
 /**
  * Get an LLMS_Student
  *
- * @param    mixed $user  WP_User ID, instance of WP_User, or instance of any student class extending this class
- * @return   LLMS_Student|false LLMS_Student instance on success, false if user not found
- * @since    3.8.0
- * @version  3.9.0
+ * @since 3.8.0
+ * @since 3.9.0 Unknown
+ *
+ * @param mixed $user  WP_User ID, instance of WP_User, or instance of any student class extending this class.
+ * @return LLMS_Student|false LLMS_Student instance on success, false if user not found.
  */
 function llms_get_student( $user = null ) {
 	$student = new LLMS_Student( $user );
@@ -207,18 +212,18 @@ function llms_get_student( $user = null ) {
 /**
  * Checks if user is currently enrolled in course
  *
- * @see     LLMS_Student->is_enrolled()
- * @param   int       $user_id     WP_User ID.
- * @param   int|array $product_id  WP Post ID of a Course, Lesson, or Membership or array of multiple IDs.
- * @param   string    $relation    Comparator for enrollment check.
- *                                     All = user must be enrolled in all $product_ids.
- *                                     Any = user must be enrolled in at least one of the $product_ids.
- * @param   bool      $use_cache  If true, returns cached data if available, if false will run a db query.
+ * @since Unknown
+ * @since 3.25.0 Unknown.
  *
- * @return  boolean
+ * @see LLMS_Student->is_enrolled()
  *
- * @since   unknown
- * @version 3.25.0
+ * @param int       $user_id    WP_User ID.
+ * @param int|int[] $product_id WP Post ID of a Course, Lesson, or Membership or array of multiple IDs.
+ * @param string    $relation   Comparator for enrollment check.
+ *                              All = user must be enrolled in all $product_ids.
+ *                              Any = user must be enrolled in at least one of the $product_ids.
+ * @param bool      $use_cache  If true, returns cached data if available, if false will run a db query.
+ * @return boolean
  */
 function llms_is_user_enrolled( $user_id, $product_id, $relation = 'all', $use_cache = true ) {
 	$student = new LLMS_Student( $user_id );
@@ -228,15 +233,15 @@ function llms_is_user_enrolled( $user_id, $product_id, $relation = 'all', $use_c
 /**
  * Checks if the given object is complete for the given student
  *
- * @param  int $user_id      WP User ID of the user
- * @param  int $object_id    WP Post ID of a Course, Section, or Lesson
- * @param  int $object_type  Type, either Course, Section, or Lesson
+ * @since Unknown
+ * @since 3.3.1 Updated to use `LLMS_Student->is_enrolled()`.
  *
- * @see  LLMS_Student->is_complete()
+ * @see LLMS_Student->is_complete()
  *
- * @return bool    true if complete, false otherwise
- *
- * @version  3.3.1  updated to use LLMS_Student->is_enrolled()
+ * @param int $user_id      WP User ID of the user.
+ * @param int $object_id    WP Post ID of a Course, Section, or Lesson.
+ * @param int $object_type  Type, either Course, Section, or Lesson.
+ * @return boolean Returns `true` if complete, otherwise `false`.
  */
 function llms_is_complete( $user_id, $object_id, $object_type = 'course' ) {
 	$s = new LLMS_Student( $user_id );
@@ -246,16 +251,15 @@ function llms_is_complete( $user_id, $object_id, $object_type = 'course' ) {
 /**
  * Mark a lesson, section, course, or track as complete
  *
- * @param  int    $user_id      WP User ID
- * @param  int    $object_id    WP Post ID of the Lesson, Section, Track, or Course
- * @param  int    $object_type  object type [lesson|section|course|track]
- * @param  string $trigger      String describing the event that triggered marking the object as complete
- * @return bool
+ * @since 3.3.1
  *
- * @see    LLMS_Student->mark_complete() the class method wrapped by this function
+ * @see LLMS_Student->mark_complete()
  *
- * @since     3.3.1
- * @version   3.3.1
+ * @param int    $user_id     WP User ID.
+ * @param int    $object_id   WP Post ID of the Lesson, Section, Track, or Course.
+ * @param int    $object_type Object type [lesson|section|course|track].
+ * @param string $trigger     String describing the event that triggered marking the object as complete.
+ * @return boolean
  */
 function llms_mark_complete( $user_id, $object_id, $object_type, $trigger = 'unspecified' ) {
 	$student = new LLMS_Student( $user_id );
@@ -265,16 +269,15 @@ function llms_mark_complete( $user_id, $object_id, $object_type, $trigger = 'uns
 /**
  * Mark a lesson, section, course, or track as incomplete
  *
- * @param  int    $user_id      WP User ID
- * @param  int    $object_id    WP Post ID of the Lesson, Section, Track, or Course
- * @param  int    $object_type  object type [lesson|section|course|track]
- * @param  string $trigger      String describing the event that triggered marking the object as incomplete
- * @return bool
+ * @since 3.5.0
  *
- * @see    LLMS_Student->mark_incomplete() the class method wrapped by this function
+ * @see LLMS_Student->mark_incomplete()
  *
- * @since     3.5.0
- * @version   3.5.0
+ * @param int    $user_id     WP User ID.
+ * @param int    $object_id   WP Post ID of the Lesson, Section, Track, or Course.
+ * @param int    $object_type Object type [lesson|section|course|track].
+ * @param string $trigger     String describing the event that triggered marking the object as incomplete.
+ * @return boolean
  */
 function llms_mark_incomplete( $user_id, $object_id, $object_type, $trigger = 'unspecified' ) {
 	$student = new LLMS_Student( $user_id );
@@ -284,15 +287,15 @@ function llms_mark_incomplete( $user_id, $object_id, $object_type, $trigger = 'u
 /**
  * Register a new user
  *
- * @see  LLMS_Person_Handler::register()
+ * @since 3.0.0
  *
- * @param  array  $data    array of registration data
- * @param  string $screen  the screen to be used for the validation template, accepts "registration" or "checkout"
- * @param  bool   $signon  if true, signon the newly created user
+ * @see LLMS_Person_Handler::register()
+ *
+ * @param array  $data    array of registration data.
+ * @param string $screen  the screen to be used for the validation template, accepts "registration" or "checkout".
+ * @param bool   $signon  if true, signon the newly created user.
  *
  * @return int|WP_Error
- *
- * @since 3.0.0
  */
 function llms_register_user( $data = array(), $screen = 'registration', $signon = true ) {
 	return LLMS_Person_Handler::register( $data, $screen, $signon );
@@ -306,8 +309,8 @@ function llms_register_user( $data = array(), $screen = 'registration', $signon 
  * @since  3.36.0 Pass the `$remember` param to `wp_set_auth_cookie()`.
  * @deprecated [version] Use WP core methods such as `wp_signon()`, `wp_set_current_user()`, and/or `wp_set_auth_cookie()`.
  *
- * @param  int  $user_id  WP_User ID.
- * @param  bool $remember Whether to remember the user.
+ * @param int  $user_id  WP_User ID.
+ * @param bool $remember Whether to remember the user.
  * @return void
  */
 function llms_set_person_auth_cookie( $user_id, $remember = false ) {
@@ -320,16 +323,16 @@ function llms_set_person_auth_cookie( $user_id, $remember = false ) {
 /**
  * Remove a LifterLMS Student from a course or membership
  *
- * @param  int    $user_id     WP User ID
- * @param  int    $product_id  WP Post ID of the Course or Membership
- * @param  string $new_status  the value to update the new status with after removal is complete
- * @param  string $trigger     only remove the student if the original enrollment trigger matches the submitted value
- *                             "any" will remove regardless of enrollment trigger
+ * @since 3.0.0
+ *
+ * @see LLMS_Student->unenroll() the class method wrapped by this function
+ *
+ * @param int    $user_id     WP User ID.
+ * @param int    $product_id  WP Post ID of the Course or Membership.
+ * @param string $new_status  The value to update the new status with after removal is complete.
+ * @param string $trigger     Only remove the student if the original enrollment trigger matches the submitted value.
+ *                            Passing "any" will remove regardless of enrollment trigger.
  * @return boolean
- *
- * @see  LLMS_Student->unenroll() the class method wrapped by this function
- *
- * @since  3.0.0
  */
 function llms_unenroll_student( $user_id, $product_id, $new_status = 'expired', $trigger = 'any' ) {
 	$student = new LLMS_Student( $user_id );
@@ -340,13 +343,14 @@ function llms_unenroll_student( $user_id, $product_id, $new_status = 'expired', 
  * Delete LifterLMS Student's Enrollment record related to a given product.
  *
  * @since 3.33.0
- * @param  int    $user_id     WP User ID.
- * @param  int    $product_id  WP Post ID of the Course or Membership.
- * @param  string $trigger     Optional. Only delete the student enrollment if the original enrollment trigger matches the submitted value
- *                             "any" will remove regardless of enrollment trigger.
- * @return boolean Whether or not the enrollment records have been succesfully removed.
  *
  * @see `LLMS_Student->delete_enrollment()` the class method wrapped by this function.
+ *
+ * @param int    $user_id    WP User ID.
+ * @param int    $product_id WP Post ID of the Course or Membership.
+ * @param string $trigger    Optional. Only delete the student enrollment if the original enrollment trigger matches the submitted value.
+ *                           Passing "any" will remove regardless of enrollment trigger.
+ * @return boolean Whether or not the enrollment records have been successfully removed.
  */
 function llms_delete_student_enrollment( $user_id, $product_id, $trigger = 'any' ) {
 	$student = new LLMS_Student( $user_id );
@@ -356,14 +360,14 @@ function llms_delete_student_enrollment( $user_id, $product_id, $trigger = 'any'
 /**
  * Perform validations according to $screen and updates the user
  *
- * @see      LLMS_Person_Handler::update()
+ * @since 3.0.0
+ * @since 3.7.0 Unknown.
  *
- * @param    array  $data   array of user data
- * @param    string $screen  screen to perform validations for, accepts "account" or "checkout"
- * @return   int|WP_Error
+ * @see LLMS_Person_Handler::update()
  *
- * @since    3.0.0
- * @version  3.7.0
+ * @param array  $data   Array of user data.
+ * @param string $screen Screen to perform validations for, accepts "account" or "checkout".
+ * @return int|WP_Error
  */
 function llms_update_user( $data = array(), $screen = 'account' ) {
 	return LLMS_Person_Handler::update( $data, $screen );
