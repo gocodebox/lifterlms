dda0e4cc4df9bb8001b1fc394bb12648b81b5a23
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-11-24 15:10:14 -0700

    Tested to WP core 5.6 & Bumped minimum supported version to 5.1

--- lifterlms.php
+++ lifterlms.php
@@ -17,8 +17,8 @@
  * Domain Path: /languages
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
- * Requires at least: 4.8
- * Tested up to: 5.5
+ * Requires at least: 5.1
+ * Tested up to: 5.6
  *
  * * * * * * * * * * * * * * * * * * * * * *
  *                                         *
