11a200642987f7246f702cebf731c1fff29f2510
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-01-07 12:33:22 -0700

    Allow overriding the WP_BASE_URL

--- packages/scripts/e2e/bootstrap.js
+++ packages/scripts/e2e/bootstrap.js
@@ -12,7 +12,10 @@ if ( ! process.env.WORDPRESS_PORT ) {
 	process.env.WORDPRESS_PORT = '8080';
 }
 
-process.env.WP_BASE_URL = `http://localhost:${ process.env.WORDPRESS_PORT }`;
+// Allow easy override of the default base URL, for example if we want to point to a live URL.
+if ( ! process.env.WP_BASE_URL ) {
+	process.env.WP_BASE_URL = `http://localhost:${ process.env.WORDPRESS_PORT }`;
+}
 
 // The Jest timeout is increased because these tests are a bit slow.
 jest.setTimeout( process.env.PUPPETEER_TIMEOUT || 100000 );
