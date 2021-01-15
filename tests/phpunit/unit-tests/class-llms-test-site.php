d06d8ee9af1e0a2739f37b493550e4f768f72097
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-01-15 15:01:05 -0700

    Allow setting features via constants & doc improvements

--- tests/phpunit/unit-tests/class-llms-test-site.php
+++ tests/phpunit/unit-tests/class-llms-test-site.php
@@ -1,16 +1,21 @@
 <?php
 /**
  * Tests for LLMS_Site
- * @since    3.7.4
- * @version  3.7.4
+ *
+ * @package LifterLMS/Tests
+ *
+ * @group site
+ *
+ * @since 3.7.4
  */
 class LLMS_Test_Site extends LLMS_UnitTestCase {
 
 	/**
 	 * Test clear_lock_url() function
-	 * @return   void
-	 * @since    3.8.0
-	 * @version  3.8.0
+	 *
+	 * @since 3.8.0
+	 *
+	 * @return void
 	 */
 	public function test_clear_lock_url() {
 
@@ -21,16 +26,50 @@ class LLMS_Test_Site extends LLMS_UnitTestCase {
 	}
 
 	/**
+	 * Test check_status() method
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_check_status() {
+
+		$actions  = did_action( 'llms_site_clone_detected' );
+		$original = get_site_url();
+
+		// Not a clone.
+		$this->assertFalse( LLMS_Site::check_status() );
+
+		// Simulate the site being cloned.
+		update_option( 'siteurl', 'http://fakeurl.tld' );
+
+		$this->assertTrue( LLMS_Site::check_status() );
+		$this->assertSame( ++$actions, did_action( 'llms_site_clone_detected' ) );
+
+		// Site has been ignored.
+		update_option( 'llms_site_url_ignore', 'yes' );
+		$this->assertFalse( LLMS_Site::check_status() );
+
+		// Restore URL.
+		update_option( 'siteurl', $original );
+
+	}
+
+	/**
 	 * Test lock url getter and setter functions
-	 * @return   void
-	 * @since    3.8.0
-	 * @version  3.8.0
+	 *
+	 * @since 3.8.0
+	 * @since [version] Added urls with "www".
+	 *
+	 * @return void
 	 */
 	public function test_get_set_lock_url() {
 
 		$urls = array(
 			'https://whatever.com',
 			'http://whatever.com',
+			'https://www.whatever.com',
+			'http://www.whatever.com',
 			'https://w.com',
 			'https://whatever-with-a-dash.net',
 			'http://wh.at',
@@ -44,19 +83,19 @@ class LLMS_Test_Site extends LLMS_UnitTestCase {
 
 			$site_url = get_site_url();
 
-			// this is what the lock url should be
+			// This is what the lock url should be.
 			$lock_url = substr_replace( $site_url, LLMS_Site::$lock_string, strlen( $site_url ) / 2, 0 );
 
-			// make sure they match
+			// Make sure they match.
 			$this->assertEquals( $lock_url, LLMS_Site::get_lock_url() );
 
-			// save it
+			// Save it.
 			LLMS_Site::set_lock_url();
 
-			// make sure it saves the right option
+			// Make sure it saves the right option.
 			$this->assertEquals( $lock_url, get_option( 'llms_site_url' ) );
 
-			// this should match the original URL
+			// This should match the original URL.
 			$this->assertEquals( $site_url, LLMS_Site::get_url() );
 
 		}
@@ -65,54 +104,65 @@ class LLMS_Test_Site extends LLMS_UnitTestCase {
 
 	/**
 	 * Test feature getter and setter functions
-	 * @return   void
-	 * @since    3.8.0
-	 * @version  3.8.0
+	 *
+	 * @since 3.8.0
+	 * @since [version] Test against feature constants.
+	 *
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 *
+	 * @return void
 	 */
 	public function test_get_set_features() {
 
-		// should return an array of defaults even when option doesnt exist
+		// Should return an array of defaults even when option doesnt exist.
 		delete_option( 'llms_site_get_features' );
 		$this->assertTrue( is_array( LLMS_Site::get_features() ) );
 
-		// fake feature always returns false
+		// Fake feature always returns false.
 		$this->assertFalse( LLMS_Site::get_feature( 'mock_feature' ) );
 
-		foreach ( array( 'recurring_payments' ) as $feature ) {
+		// Test getters/setters with a real feature.
+		LLMS_Site::update_feature( 'recurring_payments', true );
+		$this->assertTrue( LLMS_Site::get_feature( 'recurring_payments' ) );
 
-			// test getters/setters
-			LLMS_Site::update_feature( $feature, true );
-			$this->assertTrue( LLMS_Site::get_feature( $feature ) );
+		LLMS_Site::update_feature( 'recurring_payments', false );
+		$this->assertFalse( LLMS_Site::get_feature( 'recurring_payments' ) );
 
-			LLMS_Site::update_feature( $feature, false );
-			$this->assertFalse( LLMS_Site::get_feature( $feature ) );
+		// Constant not set.
+		$this->assertNull( LLMS_Unit_Test_Util::call_method( 'LLMS_Site', 'get_feature_constant', array( 'recurring_payments' ) ) );
+		$this->assertFalse( LLMS_Site::get_feature( 'recurring_payments' ) );
 
-		}
+		// Constant is set.
+		llms_maybe_define_constant( 'LLMS_SITE_FEATURE_RECURRING_PAYMENTS', true );
+		$this->assertTrue( LLMS_Site::get_feature( 'recurring_payments' ) );
 
 	}
 
+
 	/**
 	 * Test is_clone() function
-	 * @return   void
-	 * @since    3.7.4
-	 * @version  3.7.4
+	 *
+	 * @since 3.7.4
+	 *
+	 * @return void
 	 */
 	public function test_is_clone() {
 
 		$original = get_site_url();
 
-		// not a clone because the url is the lock url
+		// Not a clone because the url is the lock url.
 		$this->assertFalse( LLMS_Site::is_clone() );
 
-		// the url has changed
+		// The url has changed.
 		update_option( 'siteurl', 'http://fakeurl.tld' );
 		$this->assertTrue( LLMS_Site::is_clone() );
 
-		// change it back to the original
+		// Change it back to the original.
 		update_option( 'siteurl', $original );
 		$this->assertFalse( LLMS_Site::is_clone() );
 
-		// change the schema (should not be identified as a clone)
+		// Change the schema (should not be identified as a clone).
 		update_option( 'siteurl', set_url_scheme( $original, 'https' ) );
 		$this->assertFalse( LLMS_Site::is_clone() );
 
@@ -120,9 +170,10 @@ class LLMS_Test_Site extends LLMS_UnitTestCase {
 
 	/**
 	 * Test is_clone_ignored() function
-	 * @return   void
-	 * @since    3.8.0
-	 * @version  3.8.0
+	 *
+	 * @since 3.8.0
+	 *
+	 * @return void
 	 */
 	public function test_is_clone_ignored() {
 
