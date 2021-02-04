aa86eb8987078fe852bb761e84093d9bf4ad8c10
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-02-04 16:25:53 -0700

    always waitForSelector before clicking

--- packages/llms-e2e-test-utils/src/click.js
+++ packages/llms-e2e-test-utils/src/click.js
@@ -1,11 +1,13 @@
 /**
  * Click an elements by selector
  *
- * @since 3.39.0
+ * @since 2.0.0
+ * @since [version] Always waitForSelector before clicking the element.
  *
  * @param {String} selector Element selector string.
  * @return {Void}
  */
 export async function click( selector ) {
+	await page.waitForSelector( selector );
 	await page.$eval( selector, el => el.click() );
 }
