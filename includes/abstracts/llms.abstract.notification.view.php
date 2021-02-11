cc8a59cfeb1fb0616a7ca9251ffd0beca18e740d
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-02-11 13:54:39 -0700

    Add logs

--- includes/abstracts/llms.abstract.notification.view.php
+++ includes/abstracts/llms.abstract.notification.view.php
@@ -5,7 +5,7 @@
  * @package LifterLMS/Abstracts/Classes
  *
  * @since 3.8.0
- * @version 3.37.19
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -433,6 +433,7 @@ abstract class LLMS_Abstract_Notification_View extends LLMS_Abstract_Options_Dat
 	 * Retrieve the full HTML to be output for the notification type
 	 *
 	 * @since 3.8.0
+	 * @since [version] Pass `null` to the 3rd-party filter.
 	 *
 	 * @return string|WP_Error If the notification type is not supported, returns an error.
 	 */
