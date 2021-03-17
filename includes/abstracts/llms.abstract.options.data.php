f722dd951dce8b7ab8adfc3615e679417d902e75
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-03-17 10:45:10 -0700

    try to fix the bug

--- includes/abstracts/llms.abstract.options.data.php
+++ includes/abstracts/llms.abstract.options.data.php
@@ -25,20 +25,88 @@ abstract class LLMS_Abstract_Options_Data {
 	protected $option_prefix = 'llms_';
 
 	/**
+	 * Class version
+	 *
+	 * This is used to determine the behavior of the `get_option()` method.
+	 *
+	 * Concrete classes should use version 2 in order to use the new (future default)
+	 * behavior of the method.
+	 *
+	 * @var int
+	 */
+	protected $version = 1;
+
+
+	/**
 	 * Retrieve the value of an option from the database
 	 *
 	 * @since 3.8.0
+	 * @since [version] Changed the behavior of the function when the concrete class defines `$this->version` greater than 1.
 	 *
-	 * @param string $name    Option name (unprefixed).
-	 * @param mixed  $default Default value to use if no option is found.
-	 * @return mixed
+	 * @param string $name     Option name (unprefixed).
+	 * @param mixed  $default  Default value to use if no option is found.
+	 * @return mixed The option value.
 	 */
-	public function get_option( $name, $default = '' ) {
+	public function get_option( $name, $default = false ) {
+
+		// If the class is version 1, use the old method.
+		if ( 1 === $this->version ) {
+			// If only one argument is passed switch the default to the old argument default (an empty string).
+			$default = 1 === func_num_args() ? '' : $default;
+			return $this->get_option_deprecated( $name, $default );
+		}
+
+		$full_name = $this->get_option_name( $name );
+
+		add_filter( "default_option_{$full_name}", array( $this, 'get_option_default_value' ), 10, 3 );
+
+		// Call this way so that the `$passed_default_value` of the filter is accurate based on the number of arguments actually passed.
+		$args = func_num_args() > 1 ? array( $full_name, $default ) : array( $full_name );
+		$val = call_user_func_array( 'get_option', $args );
+		remove_filter( "default_option_{$full_name}", array( $this, 'get_option_default_value' ), 10, 3 );
+
+		return $val;
+
+	}
+
+	/**
+	 * Retrieve the value of an option from the database
+	 *
+	 * This is the "old" (to be deprecated) version of the function.
+	 *
+	 * We will transition extending classes little by little to use the new behavior and deprecate this once
+	 * all classes are fully transitioned.
+	 *
+	 * @since [version]
+	 *
+	 * @param [type] $name [description]
+	 * @param string $default [description]
+	 * @return [type] [description]
+	 */
+	private function get_option_deprecated( $name, $default = '' ) {
 		$val = get_option( $this->get_option_name( $name ), '' );
 		if ( '' === $val ) {
 			return $default;
 		}
 		return $val;
+	}
+
+	/**
+	 * Option default value autoloader
+	 *
+	 * By default, this method does nothing but extending classes can implement an autoloader to pull
+	 * default values from other sources.
+	 *
+	 * @since [version]
+	 *
+	 * @param mixed  $default_value        The default value. If no value is passed to `get_option()`, this will be an empty string.
+	 *                                     Otherwise it will be the default value passed to the method.
+	 * @param string $full_option_name     The full (prefixed) option name.
+	 * @param bool   $passed_default_value Whether or not a default value was passed to `get_option()`.
+	 * @return mixed The default option value.
+	 */
+	public function get_option_default_value( $default_value, $full_option_name, $passed_default_value ) {
+		return $default_value;
 	}
 
 	/**
