74bc6cf3f23c32814bc994ec22ce07b94d84b017
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-04-29 15:20:05 -0700

    Deploy v4.21.1 [ci skip]

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-trunk',
+    'version' => 'dev-trunk',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2d86dd2c67042b1f3bfb9a63d0b7b2089e8eaa28',
+    'name' => 'gocodebox/lifterlms',
+  ),
+  'versions' => 
+  array (
+    'gocodebox/lifterlms' => 
+    array (
+      'pretty_version' => 'dev-trunk',
+      'version' => 'dev-trunk',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2d86dd2c67042b1f3bfb9a63d0b7b2089e8eaa28',
+    ),
+    'lifterlms/lifterlms-blocks' => 
+    array (
+      'pretty_version' => '1.11.1',
+      'version' => '1.11.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'be06243de36766de801b86470f2779374b68dde8',
+    ),
+    'lifterlms/lifterlms-rest' => 
+    array (
+      'pretty_version' => '1.0.0-beta.19',
+      'version' => '1.0.0.0-beta19',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aa8599da427182c42303fb3bac5da4462e26e65e',
+    ),
+    'woocommerce/action-scheduler' => 
+    array (
+      'pretty_version' => '3.1.6',
+      'version' => '3.1.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '275d0ba54b1c263dfc62688de2fa9a25a373edf8',
+    ),
+  ),
+);
