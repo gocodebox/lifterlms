4083bbe18b6110a3839557fa22c2b458de673661
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-05-24 12:37:47 -0700

    Move schemas into subdirectory

---
+++ includes/schemas/llms-user-information-fields.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * User information fields schema
+ *
+ * A list of user information fields used by LifterLMS in various places, namely to build
+ * the editable user information forms (Checkout, Registration, and Edit Account).
+ *
+ * Each item in this list should be an array compatible with the `LLMS_Form_Field` classes
+ * settings array.
+ *
+ * The `group` attribute on each item is used internally to determine the field's origin. This
+ * attribute is an indexed array where the first item is the group's id attribute and the second item
+ * is a human-readable group name. This is used primarily when creating a list of fields for user
+ * information shortcode generation helpers.
+ *
+ * Fields can be added and modified using the `llms_user_information_fields` filter.
+ *
+ * @package LifterLMS/Schemas
+ *
+ * @since [version]
+ * @version [version]
+ *
+ * @see llms_get_user_information_fields() Retrieves the (filtered) schema
+ * @see llms_get_user_information_field() Retrieve a single field from this schema by ID.
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+return array(
+
+	// WordPress Core.
+	array(
+		'id'             => 'user_login',
+		'name'           => 'user_login',
+		'group'          => array( 'wp', __( 'WordPress', 'lifterlms' ) ),
+		'type'           => 'text',
+		'label'          => __( 'Username', 'lifterlms' ),
+		'data_store'     => 'users',
+		'data_store_key' => 'user_login',
+	),
+	array(
+		'id'              => 'email_address',
+		'name'            => 'email_address',
+		'group'          => array( 'wp', __( 'WordPress', 'lifterlms' ) ),
+		'type'           => 'email',
+		'label'           => __( 'Email Address', 'lifterlms' ),
+		'data_store'      => 'users',
+		'data_store_key'  => 'user_email',
+	),
+	array(
+		'id'             => 'password',
+		'name'           => 'password',
+		'group'          => array( 'wp', __( 'WordPress', 'lifterlms' ) ),
+		'type'           => 'password',
+		'label'          => __( 'Password', 'lifterlms' ),
+		'data_store'     => 'users',
+		'data_store_key' => 'user_pass',
+		'meter'             => llms_parse_bool( get_option( 'lifterlms_registration_password_strength', 'yes' ) ),
+		'min_strength'      => get_option( 'lifterlms_registration_password_min_strength', 'strong' ),
+		'html_attrs'        => array(
+			'minlength' => 8,
+		),
+		'meter_description' => sprintf(
+			// Translators: %1$s = Min strength merge code; %2$s = min length merge code.
+			__(
+				'A %1$s password is required with at least %2$s characters. To make it stronger, use both upper and lower case letters, numbers, and symbols.',
+				'lifterlms'
+			),
+			'{min_strength}',
+			'{min_length}'
+		),
+	),
+	array(
+		'id'             => 'first_name',
+		'name'           => 'first_name',
+		'group'          => array( 'wp', __( 'WordPress', 'lifterlms' ) ),
+		'type'          => 'text',
+		'label'          => __( 'First Name', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'first_name',
+	),
+	array(
+		'id'             => 'last_name',
+		'name'           => 'last_name',
+		'group'          => array( 'wp', __( 'WordPress', 'lifterlms' ) ),
+		'type'          => 'text',
+		'label'          => __( 'Last Name', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'last_name',
+	),
+	array(
+		'id'             => 'display_name',
+		'name'           => 'display_name',
+		'group'          => array( 'wp', __( 'WordPress', 'lifterlms' ) ),
+		'type'          => 'text',
+		'label'          => __( 'Display Name', 'lifterlms' ),
+		'data_store'     => 'users',
+		'data_store_key' => 'display_name',
+	),
+
+	// LifterLMS core.
+	array(
+		'id'             => 'llms_billing_address_1',
+		'name'           => 'llms_billing_address_1',
+		'group'          => array( 'llms', __( 'LifterLMS', 'lifterlms' ) ),
+		'type'           => 'text',
+		'label'          => __( 'Address', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'llms_billing_address_1',
+	),
+	array(
+		'id'               => 'llms_billing_address_2',
+		'name'             => 'llms_billing_address_2',
+		'group'            => array( 'llms', __( 'LifterLMS', 'lifterlms' ) ),
+		'type'             => 'text',
+		'label'            => '',
+		'label_show_empty' => true,
+		'data_store'       => 'usermeta',
+		'data_store_key'   => 'llms_billing_address_2',
+		'placeholder'      => __( 'Apartment, suite, etc...', 'lifterlms' ),
+	),
+	array(
+		'id'             => 'llms_billing_city',
+		'name'           => 'llms_billing_city',
+		'group'          => array( 'llms', __( 'LifterLMS', 'lifterlms' ) ),
+		'type'           => 'text',
+		'label'          => __( 'City', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'llms_billing_city',
+	),
+	array(
+		'id'             => 'llms_billing_country',
+		'name'           => 'llms_billing_country',
+		'group'          => array( 'llms', __( 'LifterLMS', 'lifterlms' ) ),
+		'type'           => 'select',
+		'label'          => __( 'Country', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'llms_billing_country',
+		'options_preset' => 'countries',
+		'placeholder'    => __( 'Select a Country', 'lifterlms' ),
+		'classes'        => 'llms-select2',
+	),
+	array(
+		'id'             => 'llms_billing_state',
+		'name'           => 'llms_billing_state',
+		'group'          => array( 'llms', __( 'LifterLMS', 'lifterlms' ) ),
+		'type'           => 'select',
+		'label'          => __( 'State / Region', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'llms_billing_state',
+		'options_preset' => 'states',
+		'placeholder'    => __( 'Select a State / Region', 'lifterlms' ),
+		'classes'        => 'llms-select2',
+	),
+	array(
+		'id'             => 'llms_billing_zip',
+		'name'           => 'llms_billing_zip',
+		'group'          => array( 'llms', __( 'LifterLMS', 'lifterlms' ) ),
+		'type'           => 'text',
+		'label'          => __( 'Postal / Zip Code', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'llms_billing_zip',
+	),
+	array(
+		'id'             => 'llms_phone',
+		'name'           => 'llms_phone',
+		'group'          => array( 'llms', __( 'LifterLMS', 'lifterlms' ) ),
+		'type'           => 'tel',
+		'label'          => __( 'Phone Number', 'lifterlms' ),
+		'data_store'     => 'usermeta',
+		'data_store_key' => 'llms_phone',
+	),
+
+);
