7c1a7676678b9816544686083d7c5b874d9ecdcb
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-06-17 10:50:58 -0700

    Focus on select2 search field before typing, fixes WP 5.8 failing e2e tests

--- packages/llms-e2e-test-utils/src/select2-select.js
+++ packages/llms-e2e-test-utils/src/select2-select.js
@@ -2,6 +2,7 @@
  * Select a value from a select2 dropdown field
  *
  * @since 2.2.1
+ * @since [version] Focus on the search selector prior to typing.
  *
  * @param {String} selector Query selector for the select element.
  * @param {String} value    Option value to select.
@@ -9,11 +10,17 @@
  */
 export async function select2Select( selector, value ) {
 
+	const SEARCH_SELECTOR = '.select2-search__field';
+
 	await page.$eval( selector, ( el ) => {
 		jQuery( el ).select2( 'open' );
 	} );
 
-	await page.waitFor( 1000 );
+	await page.waitForSelector( SEARCH_SELECTOR );
+	await page.focus( SEARCH_SELECTOR );
+
+	// await page.waitFor( 1000 );
+
 	await page.keyboard.type( value );
 	await page.waitFor( 1000 );
 
