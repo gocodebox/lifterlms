5ead81c08bbbf2970a33d7349b1eb30c64277a05
Rocco Aliberti <raliberti84@gmail.com>
2021-07-28 13:13:25 +0200

    Added sending test email logic

--- includes/notifications/controllers/class.llms.notification.controller.upcoming.payment.reminder.php
+++ includes/notifications/controllers/class.llms.notification.controller.upcoming.payment.reminder.php
@@ -34,6 +34,16 @@ class LLMS_Notification_Controller_Upcoming_Payment_Reminder extends LLMS_Abstra
 	);
 
 	/**
+	 * Determines if test notifications can be sent
+	 *
+	 * @var bool
+	 */
+	protected $testable = array(
+		'basic' => false,
+		'email' => true,
+	);
+
+	/**
 	 * Add an action to trigger the notification to send
 	 *
 	 * @since [version]
@@ -61,7 +71,6 @@ class LLMS_Notification_Controller_Upcoming_Payment_Reminder extends LLMS_Abstra
 	public function action_callback( $order_id = null ) {
 
 		// These checks are basically the same we do in LLMS_Controller_Orders::recurring_charge().
-		// TODO: create a new method that can be used by both.
 
 		// Recurring payments disabled as a site feature when in staging mode.
 		if ( ! LLMS_Site::get_feature( 'recurring_payments' ) ) {
@@ -314,6 +323,101 @@ class LLMS_Notification_Controller_Upcoming_Payment_Reminder extends LLMS_Abstra
 				),
 			),
 		);
+
+	}
+
+	/**
+	 * Get an array of LifterLMS Admin Page settings to send test notifications
+	 *
+	 * Retrieves 25 recurring orders with an existing next payment date.
+	 * Orders related to ended plans are retrieved as well.
+	 *
+	 * @since [version]
+	 *
+	 * @param string $type Notification type [basic|email].
+	 * @return array
+	 */
+	public function get_test_settings( $type ) {
+
+		$query = new WP_Query(
+			array(
+				'post_type'      => 'llms_order',
+				'posts_per_page' => 25,
+				'meta_query'     => array(
+					'relation' => 'and',
+					array(
+						'key'     => '_llms_order_type',
+						'value'   => 'recurring',
+						'compare' => '=',
+					),
+					array(
+						'key'     => '_llms_date_next_payment',
+						'compare' => 'EXISTS',
+					),
+				),
+				'no_found_rows' => true,
+				'order_by' => 'ID',
+			)
+		);
+
+		$options = array(
+			'' => '',
+		);
+		foreach ( $query->posts as $post ) {
+			$order   = llms_get_post( $post );
+			$student = llms_get_student( $order->get( 'user_id' ) );
+			if ( $order && $student ) {
+				$options[ $order->get( 'id' ) ] = esc_attr(
+					sprintf(
+						// Translators: %1$d = The Order ID; %2$s The customer's full name; %3$s The product title.
+						__( 'Order #%1$d from %2$s for "%3$s"', 'lifterlms' ),
+						$order->get( 'id' ),
+						$student->get_name(),
+						$order->get( 'product_title' )
+					)
+				);
+			}
+		}
+
+		return array(
+			array(
+				'class'             => 'llms-select2',
+				'custom_attributes' => array(
+					'data-allow-clear' => true,
+					'data-placeholder' => __( 'Select a recurring order', 'lifterlms' ),
+				),
+				'default'           => '',
+				'id'                => 'order_id',
+				'desc'              => '<br/>' . __( 'Send yourself a test notification using information from the selected recurring order.', 'lifterlms' ),
+				'options'           => $options,
+				'title'             => __( 'Send a Test', 'lifterlms' ),
+				'type'              => 'select',
+			),
+		);
+
+	}
+
+	/**
+	 * Send a test notification to the currently logged in users
+	 *
+	 * @since [version]
+	 *
+	 * @param string $type Notification type [basic|email].
+	 * @param array  $data Array of test notification data as specified by $this->get_test_data().
+	 *
+	 * @return int|false
+	 */
+	public function send_test( $type, $data = array() ) {
+
+		if ( empty( $data['order_id'] ) ) {
+			return;
+		}
+
+		$order         = llms_get_post( $data['order_id'] );
+		$this->user_id = $order->get( 'user_id' );
+		$this->post_id = $order->get( 'id' );
+
+		return parent::send_test( $type );
 
 	}
 
