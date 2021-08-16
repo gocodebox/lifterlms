375176da32a612b061b5d75efa45e1c00aaa575c
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-08-16 13:50:38 -0700

    Save remaining payment data

--- includes/admin/post-types/meta-boxes/class.llms.meta.box.order.details.php
+++ includes/admin/post-types/meta-boxes/class.llms.meta.box.order.details.php
@@ -5,7 +5,7 @@
  * @package LifterLMS/Admin/PostTypes/MetaBoxes/Classes
  *
  * @since 3.0.0
- * @version 3.35.0
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -14,7 +14,6 @@ defined( 'ABSPATH' ) || exit;
  * Order Details meta box
  *
  * @since 3.0.0
- * @since 3.35.0 Verify nonces and sanitize `$_POST` data.
  */
 class LLMS_Meta_Box_Order_Details extends LLMS_Admin_Metabox {
 
@@ -53,6 +52,7 @@ class LLMS_Meta_Box_Order_Details extends LLMS_Admin_Metabox {
 	 *
 	 * @since 1.0.0
 	 * @since 3.0.0 Unknown.
+	 * @since [version] Use llms() in favor of deprecated LLMS().
 	 *
 	 * @return void
 	 */
@@ -69,7 +69,7 @@ class LLMS_Meta_Box_Order_Details extends LLMS_Admin_Metabox {
 		$gateway_feature           = $order->is_recurring() ? 'recurring_payments' : 'single_payments';
 		$switchable_gateways       = array();
 		$switchable_gateway_fields = array();
-		foreach ( LLMS()->payment_gateways()->get_supporting_gateways( $gateway_feature ) as $id => $gateway_obj ) {
+		foreach ( llms()->payment_gateways()->get_supporting_gateways( $gateway_feature ) as $id => $gateway_obj ) {
 			$switchable_gateways[ $id ]       = $gateway_obj->get_admin_title();
 			$switchable_gateway_fields[ $id ] = $gateway_obj->get_admin_order_fields();
 		}
@@ -84,19 +84,21 @@ class LLMS_Meta_Box_Order_Details extends LLMS_Admin_Metabox {
 	 * @since 3.0.0
 	 * @since 3.10.0 Unknown.
 	 * @since 3.35.0 Verify nonces and sanitize `$_POST` data.
+	 * @since [version] Update return value from void to int (for testing conditions) and include update remaining payment data when necessary.
 	 *
 	 * @param int $post_id Post ID of the Order.
-	 * @return void
+	 * @return int Returns `-1` on invalid or missing nonce, `0` when an order cannot be found, and
+	 *             `1` otherwise.
 	 */
 	public function save( $post_id ) {
 
 		if ( ! llms_verify_nonce( 'lifterlms_meta_nonce', 'lifterlms_save_data' ) ) {
-			return;
+			return -1;
 		}
 
-		$order = llms_get_post( $this->post );
+		$order = llms_get_post( $post_id );
 		if ( ! $order || ! is_a( $order, 'LLMS_Order' ) ) {
-			return;
+			return 0;
 		}
 
 		$fields = array(
@@ -112,6 +114,76 @@ class LLMS_Meta_Box_Order_Details extends LLMS_Admin_Metabox {
 				$order->set( $key, llms_filter_input( INPUT_POST, $key, FILTER_SANITIZE_STRING ) );
 			}
 		}
+
+		$this->save_remaining_payments( $order );
+
+		return 1;
+
+	}
+
+	/**
+	 * Save remaining payment date for expiring recurring orders
+	 *
+	 * @since [version]
+	 *
+	 * @param LLMS_Order $order Order object
+	 * @return int Returns `-1` when there's for invalid order types, `0` when there's no changes to save, and
+	 *             `1` when remaining payment data is updated.
+	 */
+	protected function save_remaining_payments( $order ) {
+
+		// If it's not a payment plan don't proceed.
+		if ( ! $order->has_plan_expiration() ) {
+			return -1;
+		}
+
+		$payments  = llms_filter_input( INPUT_POST, '_llms_remaining_payments', FILTER_SANITIZE_NUMBER_INT );
+		$remaining = $order->get_remaining_payments();
+
+		// Nothing to save.
+		if ( null === $payments || absint( $payments ) === $remaining || absint( $payments ) < 1 ) {
+			return 0;
+		}
+
+		// Determine how to adjust the billing length.
+		$adjustment = $payments - $remaining;
+		$original   = $order->get( 'billing_length' );
+		$new_length = max( $order->get( 'billing_length' ) + $adjustment, 1 );
+		$period     = $order->get( 'billing_period' );
+
+		// Update the payment plan.
+		$order->set( 'billing_length', $new_length );
+
+		// Record that the payment plan has been modified.
+		$order->add_note(
+			sprintf(
+				/**
+				 * Translators:
+				 *   %1$d is the original billing length.
+				 *   %2$s is the billing period (adjusted for pluralization against the original billing length).
+				 *   %3$d is the new billing length.
+				 *   %4$s is the billing period (adjusted for pluralization against the new billing length).
+ 				 */
+				__( 'The billing length of the order has been modified from %1$d %2$s to %3$d %4$s.', 'lifterlms' ),
+				$original,
+				llms_get_time_period_l10n( $period, $original ),
+				$new_length,
+				llms_get_time_period_l10n( $period, $new_length )
+			)
+		);
+
+		// Store a use note if one was entered.
+		$note = llms_filter_input( INPUT_POST, '_llms_remaining_note', FILTER_SANITIZE_STRING );
+		if ( $note ) {
+			$order->add_note( wp_strip_all_tags( $note ), true );
+		}
+
+		// Restart scheduled payments.
+		if ( ! $order->has_scheduled_payment() ) {
+			$order->maybe_schedule_payment();
+		}
+
+		return 1;
 
 	}
 
