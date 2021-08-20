62a1728b50396c13ba951fa3cec5138ef36b8293
pondermatic <5377968+pondermatic@users.noreply.github.com>
2021-08-20 08:54:32 -0500

    Revert the deletion of the interface files and mark them as deprecated.

---
+++ includes/interfaces/llms.interface.post.video.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * LifterLMS Post Model Video Embeds
+ *
+ * @package LifterLMS/Interfaces
+ *
+ * @since 3.17.0
+ * @version 3.17.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * LLMS_Interface_Post_Video
+ *
+ * @since 3.17.0
+ * @deprecated [version] Use {@see LLMS_Trait_Audio_Video_Embed}.
+ */
+interface LLMS_Interface_Post_Video {
+
+	/**
+	 * Attempt to get oEmbed for an video provider
+	 *
+	 * Falls back to the [video] shortcode if the oEmbed fails
+	 *
+	 * @since 3.17.0
+	 *
+	 * @return string
+	 */
+	public function get_video();
+
+}
