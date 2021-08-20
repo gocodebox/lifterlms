9b651ebe4e8dc286a86414ea8ff56e3fae7ca180
pondermatic <5377968+pondermatic@users.noreply.github.com>
2021-08-20 07:59:11 -0500

    Add testing of the deprecated $_instance property. Replace the $mock property, that held an anonymous object, with a $mock_class property, that holds the class name of the anonymous object.

--- tests/phpunit/unit-tests/traits/llms-test-trait-singleton.php
+++ tests/phpunit/unit-tests/traits/llms-test-trait-singleton.php
@@ -10,24 +10,48 @@
 class LLMS_Test_Singleton_Trait extends LLMS_UnitTestCase {
 
 	/**
-	 * @var LLMS_Trait_Singleton
+	 * Dynamic class name of the mock class.
+	 *
+	 * Even though this property contains a string, it is documented as a class so that it can be used like this:
+	 * `$this->mock_class::instance()`
+	 *
+	 * @since [version]
+	 *
+	 * @var LLMS_Trait_Singleton|object
 	 */
-	protected $mock;
+	protected $mock_class;
 
 	/**
 	 * Setup before running each test in this class.
 	 *
 	 * @since [version]
+	 * @noinspection PhpHierarchyChecksInspection
 	 */
 	public function setUp() {
 
 		parent::setUp();
 
-		$this->mock = new class {
+		# Instantiate an anonymous class that uses the trait to be tested.
+		$mock = new class {
 
 			use LLMS_Trait_Singleton;
 
 			protected $color;
+
+			protected static $_instance = null;
+
+			public static function deprecated_instance() {
+				if ( is_null( self::$_instance ) ) {
+					self::$_instance = new self();
+				}
+
+				return self::$_instance;
+			}
+
+			public static function init() {
+				self::$_instance = null;
+				self::$instance  = null;
+			}
 
 			public function get_color() {
 				return $this->color;
@@ -37,6 +61,53 @@ class LLMS_Test_Singleton_Trait extends LLMS_UnitTestCase {
 				$this->color = $color;
 			}
 		};
+
+		$this->mock_class = get_class( $mock );
+	}
+
+	/**
+	 * Test the {@see LLMS_Trait_Singleton::instance()} method where the exhibiting class has a
+	 * deprecated `$_instance` property and may have it set by a 3rd party extended class.
+	 *
+	 * @since [version]
+	 */
+	public function test_deprecated_instance() {
+
+		# Test where $_instance is not set.
+		$this->mock_class::init();
+		$object             = $this->mock_class::instance();
+		$instance_property  = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, 'instance' );
+		$_instance_property = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, '_instance' );
+		$this->assertEquals( $object, $instance_property );
+		$this->assertEquals( $object, $_instance_property );
+
+		# Test where $_instance is set.
+		$this->mock_class::init();
+		$object             = $this->mock_class::deprecated_instance();
+		$_instance_property = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, '_instance' );
+		$instance_property  = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, 'instance' );
+		$this->assertEquals( $object, $_instance_property );
+		$this->assertNull( $instance_property );
+
+		# Test setting $instance, then $_instance.
+		$this->mock_class::init();
+		$object1            = $this->mock_class::instance();
+		$object2            = $this->mock_class::deprecated_instance();
+		$instance_property  = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, 'instance' );
+		$_instance_property = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, '_instance' );
+		$this->assertEquals( $object1, $object2 );
+		$this->assertEquals( $object1, $instance_property );
+		$this->assertEquals( $object1, $_instance_property );
+
+		# Test setting $_instance, then $instance.
+		$this->mock_class::init();
+		$object1 = $this->mock_class::deprecated_instance();
+		$object2 = $this->mock_class::instance();
+		$instance_property = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, 'instance' );
+		$_instance_property = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, '_instance' );
+		$this->assertEquals( $object1, $object2 );
+		$this->assertEquals( $object1, $instance_property );
+		$this->assertEquals( $object1, $_instance_property );
 	}
 
 	/**
@@ -47,19 +118,23 @@ class LLMS_Test_Singleton_Trait extends LLMS_UnitTestCase {
 	public function test_instance() {
 
 		# Test that the static instance property does not yet have an object.
-		$actual = LLMS_Unit_Test_Util::get_private_property_value( $this->mock, 'instance' );
-		$this->assertIsNotObject( $actual );
+		$this->mock_class::init();
+		$instance_property = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, 'instance' );
+		$this->assertIsNotObject( $instance_property );
 
-		/** Test {@see LLMS_Trait_Singleton::instance()}. */
-		$instance1 = $this->mock->instance();
-		$object = LLMS_Unit_Test_Util::get_private_property_value( $this->mock, 'instance' );
-		$this->assertEquals( $instance1, $object );
+		/**
+		 * Test that {@see LLMS_Trait_Singleton::instance()} instantiates a new object,
+		 * sets it in the static `$instance` property, and returns the new object.
+		 */
+		$object1           = $this->mock_class::instance();
+		$instance_property = LLMS_Unit_Test_Util::get_private_property_value( $this->mock_class, 'instance' );
+		$this->assertEquals( $object1, $instance_property );
 
 		# Test that 2 instances are the same.
-		$instance2 = $this->mock->instance();
-		$instance1->set_color( 'red' );
-		$instance2->set_color( 'green' );
-		$this->assertEquals( $instance1, $instance2 );
-		$this->assertEquals( 'green', $instance1->get_color() );
+		$object1->set_color( 'red' );
+		$object2 = $this->mock_class::instance();
+		$object2->set_color( 'green' );
+		$this->assertEquals( $object1, $object2 );
+		$this->assertEquals( 'green', $object1->get_color() );
 	}
 }
