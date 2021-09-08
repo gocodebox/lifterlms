a44850a16f598909d2da434042864b98ba88e932
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-09-08 15:19:13 -0700

    Use user from setup in favor of creating via createUser()

--- packages/llms-e2e-test-utils/src/select2-select.js
+++ packages/llms-e2e-test-utils/src/select2-select.js
@@ -4,6 +4,8 @@
  * @since 2.2.1
  * @since 2.3.0 Focus on the search selector prior to typing.
  * @since [version] Use `waitForTimeout()` in favor of deprecated `waitFor()`.
+ *              Wait for select2 to be loaded before attempting to open it and wait for select2 dropdown
+ *              to close after selecting an option.
  *
  * @param {String} selector Query selector for the select element.
  * @param {String} value    Option value to select.
@@ -11,12 +13,14 @@
  */
 export async function select2Select( selector, value ) {
 
-	const SEARCH_SELECTOR = '.select2-search__field';
+	// Wait for select2 to load on the element.
+	await page.waitForSelector( `${ selector }.select2-hidden-accessible` );
 
 	await page.$eval( selector, ( el ) => {
 		jQuery( el ).select2( 'open' );
 	} );
 
+	const SEARCH_SELECTOR = '.select2-search__field';
 	await page.waitForSelector( SEARCH_SELECTOR );
 	await page.focus( SEARCH_SELECTOR );
 
@@ -24,5 +28,8 @@ export async function select2Select( selector, value ) {
 	await page.waitForTimeout( 1000 );
 
 	await page.keyboard.press( 'Enter' );
+
+	// Wait for the selection box to close.
+	await page.waitForSelector( `${ selector } + .select2-container .select2-selection[aria-expanded="false"]` );
 
 };
