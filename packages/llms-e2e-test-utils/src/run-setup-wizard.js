beaa55410e6a5d421db3a9e59cb1832227f7badf
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-09-15 12:09:59 -0700

    Fix index test

--- packages/llms-e2e-test-utils/src/run-setup-wizard.js
+++ packages/llms-e2e-test-utils/src/run-setup-wizard.js
@@ -34,13 +34,13 @@ export async function runSetupWizard( {
 	exit = false,
 } = {} ) {
 
-	await page.waitForTimeout( 3000 );
+	// await page.waitForTimeout( 3000 );
 
 	// Launch the Setup Wizard.
-	await visitAdminPage( 'index.php', 'page=llms-setup' );
+	await visitAdminPage( 'index.php' );
 	// await visitAdminPage( 'admin.php', 'page=llms-setup' );
 
-	await page.waitForTimeout( 3000 );
+	// await page.waitForTimeout( 3000 );
 
 	// // Step One.
 	// expect( await getTitle() ).toBe( 'Welcome to LifterLMS!' );
