8d7ec128dba152872946c2945da8686130f1a734
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-10-18 13:39:46 -0700

    debug 'you are probably offline.' (electric boogaloo)

--- tests/e2e/tests/activate/setup-wizard.test.js
+++ tests/e2e/tests/activate/setup-wizard.test.js
@@ -4,19 +4,14 @@
  * @since 3.37.8
  * @since 3.37.14 Fix package references.
  * @since 4.0.0-rc.1 Use `runSetupWizard()`.
- * @since [version] Ensure all `apiFetch()` requests are finished before completing the test.
  */
 
 import { visitPage, runSetupWizard } from '@lifterlms/llms-e2e-test-utils';
-import { visitAdminPage } from '@wordpress/e2e-test-utils';
 
 describe( 'SetupWizard', () => {
 
-	beforeEach( async () => {
-		await visitPage( '/', '' );
-	} );
-
 	it ( 'should load and run the entire setup wizard.', async () => {
+		// await visitPage( '/', '' );
 		await runSetupWizard();
 	} );
 
