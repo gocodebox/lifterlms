96ca25b034cfc326bcbb5ef4cdb51be40b6d40fe
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-10-19 10:49:34 -0700

    Update import plugin settings to have WP core packages and included libs known

--- packages/scripts/config/.eslintrc.js
+++ packages/scripts/config/.eslintrc.js
@@ -11,7 +11,15 @@ const eslintConfig = {
 	root: true,
 	extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
 	rules: {
-		"jsdoc/tag-lines": [ 0 ]
+		'jsdoc/tag-lines': [ 0 ]
+	},
+	settings: {
+		// Ensure that WordPress core dependencies don't throw errors when importing them.
+		'import/internal-regex': '^@wordpress/',
+		'import/core-modules': [
+			// @todo: This list needs to be expanded to include other WP Core included modules.
+			'jquery',
+		]
 	}
 };
 
