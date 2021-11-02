9ba3cb7b5f4db493f8901e0102527bb0e929361f
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-11-02 16:18:51 -0700

    Deprecate.

--- includes/certificates/class.llms.certificate.user.php
+++ includes/certificates/class.llms.certificate.user.php
@@ -90,19 +90,37 @@ class LLMS_Certificate_User extends LLMS_Certificate {
 	public $user_pass;
 
 	/**
-	 * Constructor
+	 * Alert when deprecated methods are used.
+	 *
+	 * This class as well as core classes extending it have been deprecated. All public and protected methods
+	 * have been changed to private and will be made accessible through this magic method which also emits a
+	 * deprecation warning.
+	 *
+	 * This public method has been intentionally marked as private to denote it's temporary lifespan. It will be
+	 * removed alongside this class in the next major release.
+	 *
+	 * @since [version]
+	 *
+	 * @access private
+	 *
+	 * @param string $name Name of the method being called.
+	 * @param array  $args Arguments provided to the method.
+	 * @return void
 	 */
-	public function __construct() {
-		parent::__construct();
+	public function __call( $name, $args ) {
+		_deprecated_function( __CLASS__ . "::" . $name, '[version]' );
+		if ( method_exists( $this, $name ) ) {
+			$this->$name( ...$args );
+		}
 	}
 
 	/**
-	 * Check if the user has already earned this achievement
-	 * used to prevent duplicates
+	 * Check if the user has already earned this achievement used to prevent duplicates
 	 *
-	 * @return   boolean
-	 * @since    3.4.1
-	 * @version  3.17.4
+	 * @since 3.4.1
+	 * @since 3.17.4 Unknown.
+	 *
+	 * @return boolean
 	 */
 	private function has_user_earned() {
 
@@ -126,10 +144,15 @@ class LLMS_Certificate_User extends LLMS_Certificate {
 		);
 
 		/**
-		 * @filter llms_certificate_has_user_earned
-		 * Allow 3rd parties to override default dupcheck functionality for certificates
+		 * Deprecated.
+		 *
+		 * @since Unknown.
+		 * @deprecated [version] Filter `llms_certificate_has_user_earned` is deprecated in favor of `llms_earned_certificate_dupcheck`.
+		 *
+		 * @param boolean               $has_earned Whether or not the certificate has been earned.
+		 * @param LLMS_Certificate_User $user_cert  The user certificate object.
 		 */
-		return apply_filters( 'llms_certificate_has_user_earned', ( $count >= 1 ), $this );
+		return apply_filters_deprecated( 'llms_certificate_has_user_earned', array( ( $count >= 1 ), $this ), 'llms_earned_certificate_dupcheck' );
 
 	}
 
@@ -138,14 +161,15 @@ class LLMS_Certificate_User extends LLMS_Certificate {
 	 *
 	 * @since Unknown
 	 * @since 3.24.0 Unknown.
-	 * @since [version] Allow empty titles and images.
+	 * @deprecated [version] `LLMS_Certificate_User::init()` is deprecated with no replacement.
 	 *
 	 * @param int $email_id  ID of Certificate.
 	 * @param int $person_id ID of the user receiving the certificate.
 	 * @param int $lesson_id ID of associated lesson.
 	 * @return void
 	 */
-	public function init( $email_id, $person_id, $lesson_id ) {
+	private function init( $email_id, $person_id, $lesson_id ) {
+
 		global $wpdb;
 
 		$email_content = get_post( $email_id );
@@ -172,15 +196,18 @@ class LLMS_Certificate_User extends LLMS_Certificate {
 	}
 
 	/**
-	 * [trigger description]
+	 * Award the cert to a user.
 	 *
-	 * @param  int $user_id   [ID of the user receiving the certificate]
-	 * @param  int $email_id  [ID of the certificate]
-	 * @param  int $lesson_id [ID of the associated lesson]
+	 * @since Unknown
+	 * @deprecated [version] `LLMS_Certificate_User::trigger()` is deprecated with no replacement.
+	 *
+	 * @param int $user_id   ID of the user receiving the certificate.
+	 * @param int $email_id  ID of the certificate.
+	 * @param int $lesson_id ID of the associated lesson.
 	 *
 	 * @return void
 	 */
-	public function trigger( $user_id, $email_id, $lesson_id ) {
+	private function trigger( $user_id, $email_id, $lesson_id ) {
 
 		$this->init( $email_id, $user_id, $lesson_id );
 
@@ -208,10 +235,11 @@ class LLMS_Certificate_User extends LLMS_Certificate {
 	 * @since 1.0.0
 	 * @since 3.17.4 Unknown.
 	 * @since 5.0.0 Merge the [llms-user] (and others) shortcode.
+	 * @deprecated [version] `LLMS_Certificate_User::get_content_html()` is deprecated with no replacement.
 	 *
 	 * @return string
 	 */
-	public function get_content_html() {
+	private function get_content_html() {
 
 		add_filter( 'llms_user_info_shortcode_user_id', array( $this, 'set_shortcode_user' ) );
 
@@ -258,11 +286,12 @@ class LLMS_Certificate_User extends LLMS_Certificate {
 	 * Set the user ID used by [llms-user] to the user earning the certificate.
 	 *
 	 * @since 5.0.0
+	 * @deprecated [version] `LLMS_Certificate_User::set_shortcode_user()` is deprecated with no replacement.
 	 *
 	 * @param int $uid WP_User ID of the current user.
 	 * @return int
 	 */
-	public function set_shortcode_user( $uid ) {
+	private function set_shortcode_user( $uid ) {
 		return $this->userid;
 	}
 
