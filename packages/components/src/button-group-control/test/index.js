ae6759a726fafae456e6f60e7a1faf6b8e29e97a
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-12-01 16:27:49 -0700

    lint

--- packages/components/src/button-group-control/test/index.js
+++ packages/components/src/button-group-control/test/index.js
@@ -9,9 +9,7 @@ import { render } from '@testing-library/react';
 import ButtonGroupControl from '../';
 
 describe( 'ButtonGroupControl', () => {
-
-	it ( 'should render a <ButtonGroupControl> component', () => {
-
+	it( 'should render a <ButtonGroupControl> component', () => {
 		const args = {
 				id: 'group-id',
 				className: 'extra-class',
@@ -24,13 +22,13 @@ describe( 'ButtonGroupControl', () => {
 					{
 						label: 'Button 2',
 						value: 'button-2',
-					}
+					},
 				],
 				selected: 'button-1',
 			},
 			{ container } = render( <ButtonGroupControl { ...args } /> ),
 			control = container.firstChild,
-			label   = control.querySelector( 'label' ),
+			label = control.querySelector( 'label' ),
 			buttons = control.querySelectorAll( 'button' );
 
 		expect( control.classList.contains( 'llms-button-group-control' ) ).toBe( true );
@@ -41,11 +39,9 @@ describe( 'ButtonGroupControl', () => {
 		expect( label.textContent ).toBe( args.label );
 
 		expect( buttons.length ).toBe( args.options.length );
-		expect( buttons[0].textContent ).toBe( args.options[0].label );
-		expect( buttons[0].classList.contains( 'is-primary' ) ).toBe( true );
-		expect( buttons[1].textContent ).toBe( args.options[1].label );
-		expect( buttons[1].classList.contains( 'is-secondary' ) ).toBe( true );
-
+		expect( buttons[ 0 ].textContent ).toBe( args.options[ 0 ].label );
+		expect( buttons[ 0 ].classList.contains( 'is-primary' ) ).toBe( true );
+		expect( buttons[ 1 ].textContent ).toBe( args.options[ 1 ].label );
+		expect( buttons[ 1 ].classList.contains( 'is-secondary' ) ).toBe( true );
 	} );
-
 } );
