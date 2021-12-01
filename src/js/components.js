1ed559064c873a4371d173bf96e48a21e36dd453
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-12-01 16:27:01 -0700

    Components package

---
+++ src/js/components.js
@@ -0,0 +1,9 @@
+import * as components from '../../packages/components/src';
+
+/**
+ * Expose @lifterlms/components via the global `window.llms` object.
+ *
+ * @since [version]
+ */
+window.llms = window.llms || {};
+window.llms.components = components;
