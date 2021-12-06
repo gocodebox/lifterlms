be47360fa1f27466cdce217a959ec9c29201de32
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-12-06 13:20:46 -0700

    Bootstrap page and media library for content protection tests

--- tests/bin/setup-e2e.sh
+++ tests/bin/setup-e2e.sh
@@ -14,6 +14,7 @@ $llmsenv wp plugin activate lifterlms
 $llmsenv wp user create voucher voucher@email.tld --role=student --user_pass=password
 
 # StudentDashboardLogin -> should allow a user with valid credentials to login
+# Settings/CopyPrevention -> StudentUser
 $llmsenv wp user create validcreds validcreds@email.tld --role=student --user_pass=password
 
 
@@ -21,3 +22,11 @@ $llmsenv wp user create validcreds validcreds@email.tld --role=student --user_pa
 #################
 
 $llmsenv wp option update can_compress_scripts 1
+
+
+# 4. Bootstrap posts
+####################
+
+# Settings/CopyPrevention
+COPY_TEST_ID=$( $llmsenv wp post create --post_type=page --post_title="Integrity-Test" --post_status=publish --porcelain )
+$llmsenv wp media import https://raw.githubusercontent.com/gocodebox/lifterlms/trunk/tests/assets/yura-timoshenko-R7ftweJR8ks-unsplash.jpeg --post_id=$COPY_TEST_ID --featured_image
