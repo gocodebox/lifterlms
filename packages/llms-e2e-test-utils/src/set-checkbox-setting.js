dd5c435550ecaac8f1d2cbe43b1aade8fc927f19
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-12-06 13:20:59 -0700

    New helper functions

---
+++ packages/llms-e2e-test-utils/src/set-checkbox-setting.js
@@ -0,0 +1,26 @@
+// Internal dependencies.
+import { clickAndWait } from './click-and-wait';
+
+/**
+ * Toggles a LifterLMS checkbox setting.
+ *
+ * @since [version]
+ *
+ * @param {string}  selector Selector for the setting checkbox.
+ * @param {boolean} status   Requested setting status. Use `true` for checked and `false` for unchecked.
+ * @param {boolean} save     Whether or not to perform a save after updating the setting.
+ * @return {void}
+ */
+export async function setCheckboxSetting( selector, status = true, save = true ) {
+	await page.waitForSelector( selector );
+
+	const currStatus = await page.$eval( selector, ( el ) => el.checked );
+
+	if ( status !== currStatus ) {
+		await page.click( selector );
+
+		if ( save ) {
+			await clickAndWait( '.llms-save .llms-button-primary' );
+		}
+	}
+}
