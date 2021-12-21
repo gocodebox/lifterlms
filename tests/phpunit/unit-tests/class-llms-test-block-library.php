e00c3c99379b6a78721d5fc5926c3f9f29ffc3bb
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-12-21 12:51:25 -0800

    block lib tests

---
+++ tests/phpunit/unit-tests/class-llms-test-block-library.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Test LLMS_Block_Library
+ *
+ * @package LifterLMS/Tests
+ *
+ * @group blocks
+ * @group block_library
+ *
+ * @since [version]
+ */
+class LLMS_Test_Block_Library extends LLMS_UnitTestCase {
+
+	/**
+	 * Setup the test case.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function set_up() {
+
+		parent::set_up();
+		$this->main = new LLMS_Block_Library();
+
+	}
+
+	/**
+	 * Test register()
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_register() {
+
+		$expected = array(
+			'llms/certificate-title',
+		);
+
+		$registry = WP_Block_Type_Registry::get_instance();
+
+		foreach ( $expected as $block ) {
+			$registry->unregister( $block );
+		}
+
+		$this->main->register();
+
+		foreach ( $expected as $block ) {
+			$this->assertTrue( $registry->is_registered( $block ) );
+		}
+
+	}
+
+}
