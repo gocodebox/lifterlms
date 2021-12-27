6877d74677593514157b3bf115c70282626b2365
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-12-27 13:54:17 -0700

    set a default image size if an empty array is passed

--- includes/models/model.llms.user.achievement.php
+++ includes/models/model.llms.user.achievement.php
@@ -54,17 +54,32 @@ class LLMS_User_Achievement extends LLMS_Abstract_User_Engagement {
 	/**
 	 * Retrieve the HTML <img> for the achievement
 	 *
-	 * @param    array $size  dimensions of the image to return (width x height)
-	 * @return   string
-	 * @since    3.14.0
-	 * @version  3.14.0
+	 * @since 3.14.0
+	 *
+	 * @param array $size Dimensions of the image to return passed as [ width, height ] (in pixels).
+	 * @return string
 	 */
 	public function get_image_html( $size = array() ) {
 
+		/**
+		 * Filters the HTML used to display an achievement image.
+		 *
+		 * @since 3.14.0
+		 * @since [version] Added `$size` parameter.
+		 *
+		 * @param string                $html        Image HTML.
+		 * @param LLMS_User_Achievement $achievement The achievement object.
+		 * @param int[]                 $size        Dimensions of the image to return passed as [ width, height ] (in pixels).
+		 */
 		return apply_filters(
 			'llms_achievement_get_image_html',
-			sprintf( '<img alt="%1$s" class="llms-achievement-img" src="%2$s">', esc_attr( $this->get( 'title' ) ), $this->get_image( $size ) ),
-			$this
+			sprintf(
+				'<img alt="%1$s" class="llms-achievement-img" src="%2$s">',
+				esc_attr( $this->get( 'title' ) ),
+				$this->get_image( $size )
+			),
+			$this,
+			$size,
 		);
 
 	}
@@ -73,7 +88,7 @@ class LLMS_User_Achievement extends LLMS_Abstract_User_Engagement {
 	 * Retrieve the image source for the achievement.
 	 *
 	 * @since 3.14.0
-	 * @since [version]
+	 * @since [version] Set a default size when an empty array is passed and use global default image when possible.
 	 *
 	 * @param int[] $size Dimensions of the image to return passed as [ width, height ] (in pixels).
 	 * @return string Image source URL.
@@ -83,15 +98,21 @@ class LLMS_User_Achievement extends LLMS_Abstract_User_Engagement {
 		$id     = $this->get( 'id' );
 		$img_id = get_post_thumbnail_id( $id );
 
+		$size = empty( $size ) ? array( 380, 380 ) : $size;
+
+		/**
+		 * Filters the size used to retrieve an achievement image.
+		 *
+		 * @since [version]
+		 *
+		 * @param int[] $size Dimensions of the image passed as [ width, height ] (in pixels).
+		 */
+		$size = apply_filters( 'llms_achievement_image_size', $size );
+
 		if ( ! $img_id ) {
-
-			// Get the source.
 			$src = llms()->achievements()->get_default_image( $id );
-
 		} else {
-
 			list( $src ) = wp_get_attachment_image_src( $img_id, $size );
-
 		}
 
 		/**
@@ -101,8 +122,9 @@ class LLMS_User_Achievement extends LLMS_Abstract_User_Engagement {
 		 *
 		 * @param string                $src         Image source URL.
 		 * @param LLMS_User_Achievement $achievement The achievement object.
+		 * @param int[]                 $size        Dimensions of the image to return passed as [ width, height ] (in pixels).
 		 */
-		return apply_filters( 'llms_achievement_get_image', $src, $this );
+		return apply_filters( 'llms_achievement_get_image', $src, $this, $size );
 
 	}
 
