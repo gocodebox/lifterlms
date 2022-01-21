4cd776ccf4286baab2769669353b54f2eaef76ca
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-01-21 14:30:31 -0700

    Pass an array

--- includes/forms/class-llms-form-field.php
+++ includes/forms/class-llms-form-field.php
@@ -864,7 +864,7 @@ class LLMS_Form_Field {
 		if ( 'POST' === strtoupper( getenv( 'REQUEST_METHOD' ) ) ) {
 			$posted = wp_unslash( $_POST ); // phpcs:ignore WordPress.Security.NonceVerification.Missing -- nonce is verified prior to reaching this method.
 			if ( isset( $posted[ $this->settings['name'] ] ) ) {
-				$filter_options = is_array( $posted[ $this->settings['name'] ] ) ? FILTER_REQUIRE_ARRAY : array();
+				$filter_options = is_array( $posted[ $this->settings['name'] ] ) ? array( FILTER_REQUIRE_ARRAY ) : array();
 				$user_val       = llms_filter_input_sanitize_string( INPUT_POST, $this->settings['name'], $filter_options );
 			}
 		}
