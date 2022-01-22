7656d4b332e5ba97a3de53acbd4445f2cbfa7593
Rocco Aliberti <raliberti84@gmail.com>
2022-01-22 17:08:45 +0100

    Improve 2022 css

--- includes/theme-support/class-llms-twenty-twenty-two.php
+++ includes/theme-support/class-llms-twenty-twenty-two.php
@@ -35,15 +35,9 @@ class LLMS_Twenty_Twenty_One {
 
 		add_action( 'lifterlms_before_main_content', array( __CLASS__, 'handle_page_header_wrappers' ) );
 
-		// Theme has no extra wrappers, add this class to the main list element to fix the layout.
-		add_filter( 'llms_get_loop_list_classes', array( __CLASS__, 'add_max_width_class' ) );
-		add_filter( 'llms_get_pagination_wrapper_classes', array( __CLASS__, 'add_pagination_classes' ) );
-
 		// Modify catalog & checkout columns when the catalog page isn't full width.
 		add_filter( 'lifterlms_loop_columns', array( __CLASS__, 'modify_columns_count' ) );
 		add_filter( 'llms_checkout_columns', array( __CLASS__, 'modify_columns_count' ) );
-
-		add_filter( 'navigation_markup_template', array( __CLASS__, 'maybe_disable_post_navigation' ) );
 
 		// Use theme colors for various LifterLMS elements.
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'add_inline_styles' ) );
@@ -74,37 +68,6 @@ class LLMS_Twenty_Twenty_One {
 	}
 
 	/**
-	 * Adds the 2021 theme max-width class around the catalog loop element.
-	 *
-	 * This adds catalog theme support.
-	 *
-	 * @since [version]
-	 *
-	 * @param string[] $classes List of CSS classes.
-	 * @return string[]
-	 */
-	public static function add_max_width_class( $classes ) {
-		$classes[] = 'default-max-width';
-		return $classes;
-	}
-
-	/**
-	 * Add 2021 theme classes to the LLMS pagnation element
-	 *
-	 * Makes the pagination on catalogs look like the 2021 pagination on post type archives
-	 *
-	 * @since [version]
-	 *
-	 * @param string[] $classes List of CSS classes.
-	 * @return string[]
-	 */
-	public static function add_pagination_classes( $classes ) {
-		$classes[] = 'navigation';
-		$classes[] = 'pagination';
-		return $classes;
-	}
-
-	/**
 	 * Generate inline CSS for a given context
 	 *
 	 * @since [version]
@@ -121,11 +84,8 @@ class LLMS_Twenty_Twenty_One {
 		// Frontend only.
 		if ( is_null( $context ) ) {
 
-			// Fix alignment of content in an access plan.
-			$styles[] = '.llms-access-plan-description ul { padding-left: 0; }';
-
-			// Make sure the checkout and login form and their children's box-sizing is border-box.
-			//$styles[] = '.llms-person-login-form-wrapper, llms-person-login-form-wrapper *, .llms-checkout-wrapper, .llms-checkout-wrapper * { box-sizing: border-box }';
+			// Fix alignment of content in an access plan, and navigation.
+			$styles[] = '.llms-access-plan-description ul, .llms-pagination ul { padding-left: 0; }';
 
 			// Fix form input padding.
 			$styles[] = '.llms-form-field input, .llms-form-field textarea, .llms-form-field select { padding: 6px 10px }';
@@ -133,128 +93,52 @@ class LLMS_Twenty_Twenty_One {
 			// Fix lesson preview titles:
 			$styles[] = '.llms-lesson-preview h6 { margin: 0 0 10px; }';
 
-			//$styles[] = '.llms-form-field.type-checkbox input { width: 25px; display: inline-block; }';
-
-			// Hide header/footer on certificate pages.
-			$styles[] = '.single-llms_certificate .site-header, .single-llms_my_certificate .site-header, .single-llms_certificate .widget-area, .single-llms_my_certificate .widget-area { display: none; }';
-
 			// Question layout.
 			$styles[] = '.llms-question-wrapper ol.llms-question-choices li.llms-choice .llms-choice-text { width: calc( 100% - 110px); }';
 
 			// Payment gateway stylized radio buttons.
-		/*	$styles[] = LLMS_Theme_Support::get_css(
-				array( '.llms-form-field.type-radio input[type=radio]:checked+label:before' ),
+			$styles[] = LLMS_Theme_Support::get_css(
+				array( '.llms-form-field.type-radio:not(.is-group) input[type=radio]:checked+label:before' ),
 				array(
-					'background-image' => '-webkit-radial-gradient(center,ellipse,var( --global--color-secondary ) 0,var( --global--color-secondary ) 40%,#fafafa 45%)',
-					'background-image' => 'radial-gradient(ellipse at center,var( --global--color-secondary ) 0,var( --global--color-secondary ) 40%,#fafafa 45%)',
+					'background-image' => '-webkit-radial-gradient(center,ellipse,var(--wp--preset--color--primary) 0,var(--wp--preset--color--primary) 40%,#fafafa 45%)',
+					'background-image' => 'radial-gradient(ellipse at center,var(--wp--preset--color--primary) 0,var(--wp--preset--color--primary) 40%,#fafafa 45%)',
 				)
 			);
-			// Darkmode fix.
-			$styles[] = LLMS_Theme_Support::get_css(
-				array( '.is-dark-theme .llms-form-field.type-radio input[type=radio]:checked+label:before' ),
-				array(
-					'background-image' => array(
-						'-webkit-radial-gradient(center,ellipse,var( --global--color-background ) 0,var( --global--color-background ) 40%,#fafafa 45%)',
-						'radial-gradient(ellipse at center,var( --global--color-background ) 0,var( --global--color-background ) 40%,#fafafa 45%)',
-					),
-				)
-			);
-*/
-			// Donuts.
-			$styles[] = '.llms-donut svg path { stroke: var( --global--color-secondary ); }';
-			$styles[] = '.is-dark-theme .llms-donut svg path { stroke: var( --global--color-background ); opacity: 0.5; }';
-			$styles[] = '.is-dark-theme .llms-donut { color: var( --global--color-background ); }';
-		}
-
-		// Editor only.
-		if ( 'editor' === $context ) {
-
-			// Elements with a light background that become unreadable in darkmode in the block editor.
+			// Completed lesson check
 			$styles[] = LLMS_Theme_Support::get_css(
 				array(
-					'.llms-lesson-link .llms-lesson-title',
-					'.llms-lesson-link .llms-main > *',
+					'.llms-lesson-preview.is-free .llms-lesson-complete',
+					'.llms-lesson-preview.is-complete .llms-lesson-complete',
 				),
 				array(
-					'color' => 'var( --global--color-background )',
+					'color' => 'var(--wp--preset--color--primary)',
 				),
-				'.is-dark-theme ' . $selector_prefix
 			);
 		}
 
-		$styles[] = '.llms-quiz-ui { background: transparent; }';
-
-		// Fix anchor buttons.
-		$styles[] = 'a.llms-button-action, a.llms-button-danger, a.llms-button-primary, a.llms-button-secondary { display: inline-block; }';
-
-		// Elements with a light background that become unreadable in darkmode.
-	/*	$styles[] = LLMS_Theme_Support::get_css(
-			array(
-				'.is-dark-theme .llms-notification',
-				'.is-dark-theme .llms-table tbody tr:nth-child(odd) td',
-				'.is-dark-theme .llms-table tbody tr:nth-child(odd) td a',
-				'.is-dark-theme .llms-certificate-container',
-				'.is-dark-theme a.llms-certificate',
-				'.is-dark-theme .llms-instructor-info .llms-instructors',
-				'.is-dark-theme .llms-achievement-loop-item.achievement-item',
-				'.is-dark-theme .llms-achievement',
-				'.is-dark-theme .llms-loop-item-content .llms-loop-title:hover',
-				'.llms-notice a',
-				'.is-dark-theme .llms-question-wrapper ol.llms-question-choices li.llms-choice .llms-marker',
-				'.is-dark-theme .llms-table tbody tr:nth-child(odd) td',
-				'.is-dark-theme .llms-table tbody tr:nth-child(odd) th',
-				'.is-dark-theme .llms-lesson-preview.is-complete .llms-lesson-complete',
-			),
-			array(
-				'color' => 'var( --global--color-background )',
-			),
-			$selector_prefix
-		);
-		$styles[] = LLMS_Theme_Support::get_css(
-			array(
-				'.llms-checkout',
-				'.llms-access-plan .llms-access-plan-footer',
-				'.llms-access-plan .llms-access-plan-content',
-				'.is-dark-theme .llms-progress .progress-bar-complete',
-			),
-			array(
-				'background-color' => 'var( --global--color-background )',
-			),
-			$selector_prefix
-		);
-
-		// Add background color and color to qualifying elements.
-		$styles[] = LLMS_Theme_Support::get_css(
+		// Primary background color.
+		$styles[] = $styles[] = LLMS_Theme_Support::get_css(
 			LLMS_Theme_Support::get_selectors_primary_color_background(),
 			array(
-				'color'            => 'var( --global--color-background )',
-				'background-color' => 'var( --global--color-secondary )',
-			),
-			$selector_prefix
+				'background-color' => 'var(--wp--preset--color--primary)',
+				'color'            => 'var(--wp--preset--color--background)',
+			)
 		);
 
 		// Add border color to qualifying elements.
 		$styles[] = LLMS_Theme_Support::get_css(
 			LLMS_Theme_Support::get_selectors_primary_color_border(),
 			array(
-				'border-color' => 'var( --global--color-secondary )',
-			),
-			$selector_prefix
+				'border-color' => 'var(--wp--preset--color--primary)',
+			)
 		);
 
-		// Add color to qualifying elements.
-		$styles[] = LLMS_Theme_Support::get_css(
-			LLMS_Theme_Support::get_selectors_primary_color_text(),
-			array(
-				'color' => 'var( --global--color-secondary )',
-			),
-			$selector_prefix
-		);
+		$styles[] = '.llms-quiz-ui { background: transparent; }';
 
-		// Fix progress bars.
-		$styles[] = '.llms-progress { color: var( --global--color-background ); }';
-		$styles[] = '.is-dark-theme .llms-progress .progress-bar-complete { opacity: 0.5; }';
-*/
+		// Fix anchor buttons.
+		$styles[] = 'a.llms-button-action, a.llms-button-danger, a.llms-button-primary, a.llms-button-secondary { display: inline-block; }';
+
+
 		return implode( "\r", $styles );
 
 	}
@@ -296,23 +180,6 @@ class LLMS_Twenty_Twenty_One {
 	 */
 	public static function modify_columns_count( $cols ) {
 		return 1;
-	}
-
-	/**
-	 * Disable 2021 theme post navigation on LifterLMS post types
-	 *
-	 * @since [version]
-	 *
-	 * @param string $html Post navigation HTML.
-	 * @return string
-	 */
-	public static function maybe_disable_post_navigation( $html ) {
-
-		if ( in_array( get_post_type(), array( 'course', 'llms_membership', 'lesson', 'llms_quiz', 'llms_assignment', 'llms_group' ), true ) ) {
-			return '';
-		}
-		return $html;
-
 	}
 
 	/**
