84d23b7638e02230e2e988f0c8bca87d70da5f3e
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-01-31 10:03:18 -0700

    Don't show the back link on templates

--- templates/certificates/actions.php
+++ templates/certificates/actions.php
@@ -11,13 +11,16 @@
  * @param string                $back_link         URL for the back link.
  * @param string                $back_text         Text for the back link anchor.
  * @param boolean               $is_shaing_enabled Whether or not sharing is enabled for the certificate.
+ * @param boolean               $is_template       Whether or not a certificate template is being displayed.
  */
 
 defined( 'ABSPATH' ) || exit;
 ?>
 <div class="llms-print-certificate no-print" id="llms-print-certificate">
 
-	<a class="llms-cert-return-link" href="<?php echo esc_url( $back_link ); ?>">&larr; <?php echo $back_text; ?></a>
+	<?php if ( ! $is_template ) : ?>
+		<a class="llms-cert-return-link" href="<?php echo esc_url( $back_link ); ?>">&larr; <?php echo $back_text; ?></a>
+	<?php endif; ?>
 
 	<button class="llms-button-secondary" onClick="window.print()" type="button">
 		<?php echo _e( 'Print', 'lifterlms' ); ?>
@@ -25,11 +28,13 @@ defined( 'ABSPATH' ) || exit;
 	</button>
 
 	<form action="" method="POST">
+
 		<button class="llms-button-secondary" type="submit" name="llms_generate_cert">
-		<?php echo _e( 'Save', 'lifterlms' ); ?>
+			<?php echo _e( 'Save', 'lifterlms' ); ?>
 			<i class="fa fa-cloud-download" aria-hidden="true"></i>
 		</button>
-		<?php if ( get_post_type( $certificate->get( 'id' ) ) === $certificate->get( 'db_post_type' ) ) : ?>
+
+		<?php if ( ! $is_template ) : ?>
 			<button class="llms-button-secondary" type="submit" name="llms_enable_cert_sharing" value="<?php echo ! $is_sharing_enabled; ?>">
 			<?php echo ( $is_sharing_enabled ? _e( 'Disable sharing', 'lifterlms' ) : _e( 'Enable sharing', 'lifterlms' ) ); ?>
 				<i class="fa fa-share-alt" aria-hidden="true"></i>
@@ -38,5 +43,7 @@ defined( 'ABSPATH' ) || exit;
 
 		<input type="hidden" name="certificate_id" value="<?php echo get_the_ID(); ?>">
 		<?php wp_nonce_field( 'llms-cert-actions', '_llms_cert_actions_nonce' ); ?>
+
 	</form>
+
 </div>
