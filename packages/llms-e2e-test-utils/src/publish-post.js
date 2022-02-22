160d0e1715b7f6c2f491f6f64ecd240356e09876
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-02-21 17:00:17 -0700

    Disable prepublish checks and then publish

--- packages/llms-e2e-test-utils/src/publish-post.js
+++ packages/llms-e2e-test-utils/src/publish-post.js
@@ -1,26 +1,25 @@
-import { openPublishPanel } from '@wordpress/e2e-test-utils';
+import {
+	arePrePublishChecksEnabled,
+	disablePrePublishChecks,
+	openPublishPanel,
+} from '@wordpress/e2e-test-utils';
+
+import { updatePost } from './update-post';
 
 /**
- * Opens the post publish panel and publishes the current post.
- *
- * Replaces the function of the same name from `@wordpress/e2e-test-utils` because
- * it constantly errors due to awaiting `.components-snackbar` at the end of it.
+ * Disables prepublish checks and clicks the post publish button.
  *
  * @since [version]
  *
  * @return {Promise} Promise which resolves when the close button element is successfully clicked.
  */
 export async function publishPost() {
-	await openPublishPanel();
 
-	const publishButton = await page.waitForSelector(
-		'.editor-post-publish-button:not([aria-disabled="true"])'
-	);
-	await publishButton.click();
+	const enabled = await arePrePublishChecksEnabled();
+	if ( enabled ) {
+		await page.evaluate( () => window.wp.data.dispatch( 'core/editor' ).disablePublishSidebar() );
+	}
 
-	const closeButton = await page.waitForSelector(
-		'.editor-post-publish-panel__header .components-button[aria-label="Close panel"]'
-	);
+	return updatePost();
 
-	return closeButton.click();
 }
