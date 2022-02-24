5147b43178bb411dce5472a934c584f4e9ca07e0
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-02-24 16:39:23 -0700

    Ensure compatibility with lifterlms-blocks, see https://github.com/gocodebox/lifterlms-blocks/pull/158

--- src/js/components.js
+++ src/js/components.js
@@ -1,9 +1,13 @@
-import * as components from '../../packages/components/src';
+import * as Components from '../../packages/components/src';
+
+window.llms = window.llms || {};
+
+// Preserve components from `lifterlms-blocks`.
+const { components = {} } = window.llms;
 
 /**
  * Expose @lifterlms/components via the global `window.llms` object.
  *
  * @since [version]
  */
-window.llms = window.llms || {};
-window.llms.components = components;
+window.llms.components = { ...Components };
