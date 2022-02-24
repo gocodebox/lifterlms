c76a584827951b2fa77c511522af8624aaa4c9ac
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-02-24 16:27:56 -0700

    Add award cert button component

---
+++ src/js/util/award-certificate-button/test/index.js
@@ -0,0 +1,44 @@
+import getMessage from '../message';
+import { getScratchUrl, getRedirectUrl } from '../urls';
+
+describe( 'AwardCertificateButton', () => {
+
+	describe( 'getMessage', () => {
+
+		const testData = [
+			[ 'No student and no template', null, null ],
+			[ 'Student and no template', 123, null ],
+			[ 'Template and no student', null, 456 ],
+			[ 'Template and student', 1, 2 ],
+		];
+		test.each( testData )( '%s', ( name, studentId, templateId ) => {
+			expect( getMessage( studentId, templateId ) ).toMatchSnapshot();
+		} );
+
+	} );
+
+	describe( 'urls', () => {
+
+		describe( 'getRedirectUrl', () => {
+
+			test( 'Returns the URL', () => {
+				expect( getRedirectUrl( 123 ) ).toMatchSnapshot();
+			} );
+
+		} );
+
+		describe( 'getScratchUrl', () => {
+
+			const testData = [
+				[ 'With student ID', 123 ],
+				[ 'Without student ID', null ],
+			];
+			test.each( testData )( '%s', ( name, studentId ) => {
+				expect( getScratchUrl( studentId ) ).toMatchSnapshot();
+			} );
+
+		} );
+
+	} );
+
+} );
