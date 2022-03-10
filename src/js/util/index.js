cb2197d619b2c11fd4be8014316ee0576765a338
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-03-10 15:24:06 -0700

    Add a utility to load a module into the window.llms object

--- src/js/util/index.js
+++ src/js/util/index.js
@@ -1,2 +1,3 @@
 export { editCertificateTitle } from './edit-certificate-title';
 export { default as AwardCertificateButton } from './award-certificate-button';
+export { default as loadModule } from './load-module';
