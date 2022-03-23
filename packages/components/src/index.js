abb9b81024752639527f3d22fc0c012891eb3a1b
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-03-23 15:36:58 -0700

    Add new components

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,4 +1,7 @@
 export { default as ButtonGroupControl } from './button-group-control';
+export { default as ConfirmButton } from './confirm-button';
 export { default as CopyButton } from './copy-button';
+export { default as DatePickerControl } from './date-picker-control';
 
 export * from './search-control';
+export * from './sortable-list';
