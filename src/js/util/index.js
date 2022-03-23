d5cc6b8a148234a9f1e655e0a249cc0f8f2004aa
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-03-23 13:59:57 -0700

    Don't inclued loadModule in the index

--- src/js/util/index.js
+++ src/js/util/index.js
@@ -1,3 +1,2 @@
 export { editCertificateTitle } from './edit-certificate-title';
 export { default as AwardCertificateButton } from './award-certificate-button';
-export { default as loadModule } from './load-module';
