915cfd7aa38bf95d9404a90fcf2c84014004917b
Rocco Aliberti <raliberti84@gmail.com>
2022-03-29 12:23:49 +0200

    polish

--- includes/class.llms.template.loader.php
+++ includes/class.llms.template.loader.php
@@ -29,26 +29,26 @@ class LLMS_Template_Loader {
 	 * @since 3.20.0 Unknown.
 	 * @since 3.41.1 Predispose posts content restriction in REST requests.
 	 * @since 5.8.0 Handle block templates loading.
-	 * @since [versin] Added 'llms_template_loader_priority' filter.
+	 * @since [version] Added 'llms_template_loader_priority' filter.
 	 */
 	public function __construct() {
 
 		// Template loading for FSE themes.
 		add_action( 'template_redirect', array( $this, 'hook_block_template_loader' ) );
 
+		/**
+		* Filters the template loading priority.
+		*
+		* Callback for the WP core filter `template_include`.
+		*
+		* @since [version]
+		*
+		* @param $priority The filter callback priority.
+		*/
+		$template_loader_cb_priority = apply_filters( 'llms_template_loader_priority', 100 );
+
 		// Do template loading.
-		add_filter(
-			'template_include',
-			array( $this, 'template_loader' ),
-			/**
-			 * Filter the template loader filter callback priority.
-			 *
-			 * @since [version]
-			 *
-			 * @param $priority The filter callback priority.
-			 */
-			apply_filters( 'llms_template_loader_priority', 100 )
-		);
+		add_filter( 'template_include', array( $this, 'template_loader' ), $template_loader_cb_priority );
 
 		add_action( 'rest_api_init', array( $this, 'maybe_prepare_post_content_restriction' ) );
 
