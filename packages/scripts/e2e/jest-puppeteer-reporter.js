2a1450007b7297816d20d64814659496dfa6dbc9
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-03-29 12:08:08 -0700

    Show all input data

--- packages/scripts/e2e/jest-puppeteer-reporter.js
+++ packages/scripts/e2e/jest-puppeteer-reporter.js
@@ -4,7 +4,26 @@ class LLMSReporter extends DefaultReporter {
 
 	async onTestResult( test, testResult, aggregatedResults ) {
 
-		console.dir( testResult, { depth: null } );
+		console.log( '====================' );
+		console.log( '====================' );
+
+		console.log( 'test' );
+		console.log( test );
+
+		console.log( '====================' );
+		console.log( '====================' );
+
+		console.log( 'testResult' );
+		console.log( testResult );
+
+		console.log( '====================' );
+		console.log( '====================' );
+
+		console.log( 'aggregatedResults' );
+		console.log( aggregatedResults );
+
+		console.log( '====================' );
+		console.log( '====================' );
 
 		await super.onTestResult( test, testResult, aggregatedResults );
 
