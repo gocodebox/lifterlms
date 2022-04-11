948352d8d93b750d9d1230254aaf2a311d84ad13
pondermatic <5377968+pondermatic@users.noreply.github.com>
2022-04-11 09:40:19 -0500

    Improve `load_reusable_blocks()` documentation.

--- includes/forms/class-llms-forms.php
+++ includes/forms/class-llms-forms.php
@@ -5,7 +5,7 @@
  * @package LifterLMS/Classes
  *
  * @since 5.0.0
- * @version 6.2.0
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -1024,29 +1024,32 @@ class LLMS_Forms {
 	}
 
 	/**
-	 * Loads reusable blocks into a block list
+	 * Loads reusable blocks into a block list.
 	 *
-	 * By default, a reusable block contains a reference to the block post (which will be
-	 * loaded during rendering). This is problematic for us since we want to review then
-	 * entire block list so we can see all fields for validation purposes and so on.
+	 * A reusable block contains a reference to the block post, e.g. `<!-- wp:block {"ref":2198} /-->`,
+	 * which will be loaded during rendering.
 	 *
-	 * This function will replace each reusable block with the parsed blocks
-	 * from it's reference post.
+	 * Dereferencing the reusable blocks allows the entire block list to be reviewed and to validate all form fields.
+	 * This function will replace each reusable block with the parsed blocks from its reference post.
 	 *
 	 * @since 5.0.0
 	 * @since 5.1.0 Access turned to public.
 	 *
-	 * @param array[] $blocks List of WP_Block arrays.
+	 * @param array[] $blocks An array of blocks from `parse_blocks()`,
+	 *                        where each block is usually an array cast from `WP_Block_Parser_Block`.
+	 *
 	 * @return array[]
 	 */
 	public function load_reusable_blocks( $blocks ) {
 
 		$loaded = array();
 
-		foreach ( $blocks as $index => $block ) {
+		foreach ( $blocks as $block ) {
 
+			// Skip blocks that are not reusable blocks.
 			if ( 'core/block' === $block['blockName'] ) {
 
+				// Skip reusable blocks that do not exist or are not published.
 				$post = get_post( $block['attrs']['ref'] );
 				if ( ! $post || 'publish' !== get_post_status( $post ) ) {
 					continue;
@@ -1054,15 +1057,14 @@ class LLMS_Forms {
 
 				$loaded = array_merge( $loaded, $this->parse_blocks( $post->post_content ) );
 				continue;
-
 			}
 
+			// Does this block's inner blocks have references to reusable blocks?
 			if ( $block['innerBlocks'] ) {
 				$block['innerBlocks'] = $this->load_reusable_blocks( $block['innerBlocks'] );
 			}
 
 			$loaded[] = $block;
-
 		}
 
 		return $loaded;
