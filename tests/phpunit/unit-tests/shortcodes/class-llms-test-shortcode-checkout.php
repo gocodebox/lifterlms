07e72e1ee6d0439c9c1d69ca22b6b2a71ab6ae40
pondermatic <5377968+pondermatic@users.noreply.github.com>
2022-05-26 13:14:27 -0500

    Revert addition of the testing of does_product_not_purchasable_template_print_error_notices(). That function was no good.

--- tests/phpunit/unit-tests/shortcodes/class-llms-test-shortcode-checkout.php
+++ tests/phpunit/unit-tests/shortcodes/class-llms-test-shortcode-checkout.php
@@ -9,39 +9,6 @@
 class LLMS_Test_Shortcode_Checkout extends LLMS_ShortcodeTestCase {
 
 	/**
-	 * Tests the does_product_not_purchasable_template_print_error_notices() method.
-	 *
-	 * @since [version]
-	 *
-	 * @return void
-	 * @throws ReflectionException
-	 */
-	public function test_does_product_not_purchasable_template_print_error_notices() {
-
-		$course  = $this->factory->course->create_and_get();
-		$product = new LLMS_Product( $course );
-		ob_start();
-
-		// Course is purchasable.
-		$this->assertFalse( LLMS_Unit_Test_Util::call_method(
-			'LLMS_Shortcode_Checkout',
-			'does_product_not_purchasable_template_print_error_notices',
-			array( $product )
-		) );
-
-		// Course is not purchasable.
-		$course->set( 'enrollment_end_date', '2022-05-04' );
-		$course->set( 'enrollment_period', 'yes' );
-		$this->assertTrue( LLMS_Unit_Test_Util::call_method(
-			'LLMS_Shortcode_Checkout',
-			'does_product_not_purchasable_template_print_error_notices',
-			array( $product )
-		) );
-
-		ob_end_clean(); // Do not output the template.
-	}
-
-	/**
 	 * Test shortcode registration
 	 *
 	 * @since 5.1.0
