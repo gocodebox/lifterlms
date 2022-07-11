4ac60486533da67aa4641d714015684e3689fa3e
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-07-11 10:48:15 -0700

    eslint

--- packages/components/src/spinner/index.js
+++ packages/components/src/spinner/index.js
@@ -1,7 +1,7 @@
-import $ from 'jquery';
+/* eslint-env jquery */
+import $ from 'jquery'; // eslint-disable-line import/no-unresolved
 
 import { WRAPPER_CLASSNAME, CLASSNAME, SIZE_DEFAULT } from './constants';
-
 import { STYLES } from './styles';
 
 /**
@@ -14,106 +14,97 @@ import { STYLES } from './styles';
  * Elements or selector strings that can be passed into `document.querySelector()` or `document.querySelectorAll()`.
  */
 
-
 /**
  * Loads CSS styles and appends them to the document's <head>.
  *
  * Attaching CSS directly to the `get()` method means that we don't have to worry about loading CSS files (or relying on CSS) included
  * by the LifterLMS core plugin in order to use this styled component.
- * 
+ *
  * @since [version]
  *
  * @return {void}
  */
 function loadStyles() {
-
 	const STYLE_ID = 'llms-spinner-styles';
 
-	if ( ! document.getElementById( STYLE_ID ) ) {
-		
-		const style = document.createElement( 'style' );
-		style.textContent = STYLES.replace( /\n/g, '' ).replace( /\t/g, ' ' ).replace( /\s\s+/g, ' ' );
+	if (!document.getElementById(STYLE_ID)) {
+		const style = document.createElement('style');
+		style.textContent = STYLES.replace(/\n/g, '')
+			.replace(/\t/g, ' ')
+			.replace(/\s\s+/g, ' ');
 		style.id = STYLE_ID;
-		document.head.appendChild( style );
-
+		document.head.appendChild(style);
 	}
-
 }
 
 /**
  * Retrieves spinner(s) inside a given element or element list.
- * 
+ *
  * If the spinner element doesn't already exist it will be created.
  *
- * @since 3.0.0
- * @since [version] Adjusted the `selector` parameter to support additional input types beyond a `jQuery()` selection.
- *               Added the `vanilla` parameter for forwards compatibility.
- * 
+ * @since [version]
+ *
  * @param {jQuery|Element|string} selector A selector to be parsed by `jQuery()`, an existing `jQuery()` selection, a DOM Element. The first spinner
  *                                         found within the element will be returned. If none found it will be created, appended to the element, and
  *                                         then returned.
  * @param {string}                size     Size or the spinner element. Accepts "default" (40px) or "small" (20px).
- * @param {Boolean}               vanilla  If `true`, the return value will be an `Element` as opposed to a `jQuery()` selection. This is added for
+ * @param {boolean}               vanilla  If `true`, the return value will be an `Element` as opposed to a `jQuery()` selection. This is added for
  *                                         forwards compatibility.
  * @return {jQuery|Element} A jQuery selection or DOM Element of the spinner element.
  */
-export function get( selector, size = SIZE_DEFAULT, vanilla = false ) {
-
+export function get(selector, size = SIZE_DEFAULT, vanilla = false) {
 	loadStyles();
 
-	const $el = $( selector ).first();
+	const $el = $(selector).first();
 
 	// Look for an existing spinner.
-	let $spinner = $el.find( `.${ WRAPPER_CLASSNAME }` ).first();
+	let $spinner = $el.find(`.${WRAPPER_CLASSNAME}`).first();
 
 	// No spinner found.
-	if ( ! $spinner.length ) {
-
+	if (!$spinner.length) {
 		// Create the spinner.
-		$spinner = $( `<div class="${ WRAPPER_CLASSNAME }"><i class="${ CLASSNAME } ${ size }"></i></div>` );
+		$spinner = $(
+			`<div class="${WRAPPER_CLASSNAME}"><i class="${CLASSNAME} ${size}"></i></div>`
+		);
 
 		// Add it to the DOM.
-		$el.append( $spinner );
-
+		$el.append($spinner);
 	}
 
 	// Return it.
-	return vanilla ? $spinner[ 0 ] : $spinner;
-
+	return vanilla ? $spinner[0] : $spinner;
 }
 
 /**
  * Starts spinner(s) inside a given element or element list.
- * 
+ *
  * If the spinner element doesn't already exist it will be created.
  *
- * @since 3.0.0
- * @since [version] Adjusted the `selector` parameter to support additional input types beyond a `jQuery()` selection.
- * 
+ * @since [version]
+ *
  * @param {jQuery|Element|Element[]|string} selector A selector to be parsed by `jQuery()`, an existing `jQuery()` selection, a DOM Element, or an
  *                                                   array of DOM Elements. Each element in the list will have it's spinner started. If a spinner doesn't
  *                                                   exist within the element, it will be appended and then started.
  * @param {string}                          size     Size or the spinner element. Accepts "default" (40px) or "small" (20px).
  * @return {void}
  */
-export function start( selector, size = SIZE_DEFAULT ) {
-	$( selector ).each( function() {
-		get( $( this ), size ).show();
-	} );
+export function start(selector, size = SIZE_DEFAULT) {
+	$(selector).each(function () {
+		get($(this), size).show();
+	});
 }
 
 /**
  * Stops spinner(s) inside a given element or element list.
  *
- * @since 3.0.0
- * @since [version] Adjusted the `selector` parameter to support additional input types beyond a `jQuery()` selection.
- * 
+ * @since [version]
+ *
  * @param {jQuery|Element|Element[]|string} selector A selector to be parsed by `jQuery()`, an existing `jQuery()` selection, a DOM Element, or an
  *                                                   array of DOM Elements. Each element in the list will have it's spinner stopped.
  * @return {void}
  */
-export function stop( selector ) {
-	$( selector ).each( function() {
-		get( $( this ) ).hide();
-	} );
+export function stop(selector) {
+	$(selector).each(function () {
+		get($(this)).hide();
+	});
 }
