7d9a005cdcdbcdf26ddf7cf88bed2a0bbea25d0b
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-07-14 10:27:52 -0700

    use already-prepared date, per https://github.com/gocodebox/lifterlms/commit/f370fbbe4f411b212bbde7390547ce2dc5a0e710#r78464009

--- includes/models/model.llms.order.php
+++ includes/models/model.llms.order.php
@@ -1709,9 +1709,8 @@ class LLMS_Order extends LLMS_Post_Model {
 		}
 
 		$datetime = date_i18n( 'Y-m-d H:i:s', current_time( 'timestamp' ) + $current_rule['delay'] );
-		$time     = strtotime( $datetime );
 
-		$this->set_date( 'next_payment', date( 'Y-m-d H:i:s', $time ) );
+		$this->set_date( 'next_payment', date( 'Y-m-d H:i:s', $datetime ) );
 		$this->set_status( $current_rule['status'] );
 		$this->set( 'last_retry_rule', $current_rule_index );
 
@@ -1719,7 +1718,7 @@ class LLMS_Order extends LLMS_Post_Model {
 			sprintf(
 				// Translators: %s = next attempt date.
 				esc_html__( 'Automatic retry attempt scheduled for %s', 'lifterlms' ),
-				date( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), $time )
+				date( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), strtotime( $datetime ) )
 			)
 		);
 
