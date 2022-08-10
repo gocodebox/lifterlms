e3a5f3551171a3314eeb61309df7b5adfc8bf58b
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-08-10 16:07:52 -0700

    Revert "Exclude built blocks from git"
    This reverts commit f21d0f46ac5bcb7d592dfadc58c400c4d182ce9d.

---
+++ blocks/certificate-title/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('llms-components', 'llms-utils', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '096c206ef94b9934b110e350bb39fdda');
