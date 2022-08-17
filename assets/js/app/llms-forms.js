708c4b769c2595ba2312a1091c4d5f205c3f066c
Donalda <46361618+donaldafeith@users.noreply.github.com>
2022-08-17 11:59:15 -0400

    Update assets/js/app/llms-forms.js
    Co-authored-by: Thomas Patrick Levy <thomasplevy@gmail.com>

--- assets/js/app/llms-forms.js
+++ assets/js/app/llms-forms.js
@@ -399,7 +399,7 @@ LLMS.Forms = {
 
 		var placeholder;
 		
-		if ('undefined' === typeof this.address_info[ country_code ] ) {
+		if ( 'undefined' === typeof this.address_info[ country_code ] ) {
 			placeholder = holder.attr( 'placeholder' ).replace( '{REGION}', LLMS.l10n.translate( 'State/Region' ));
 		} else {
 			placeholder = holder.attr( 'placeholder' ).replace( '{REGION}', this.address_info[country_code].state );
