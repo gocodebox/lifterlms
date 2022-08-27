36a3427b20194620713298727e831110d55b09e9
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-08-26 18:39:55 -0700

    Load quill wordcount module v2

---
+++ assets/js/llms-quill-wordcount.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'a5b76b54d86362388d7d');
---
+++ src/js/quill-wordcount.js
@@ -0,0 +1,10 @@
+import quillWordcountModule from '../../packages/quill-wordcount-module/src';
+
+/**
+ * Registers the wordcount module with Quill.
+ *
+ * @since [version]
+ */
+( function() {
+	quillWordcountModule();
+} )();
