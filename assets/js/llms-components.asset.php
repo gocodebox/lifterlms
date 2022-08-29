93096f692371d7f5100c83f8b93c6d779d7f82a9
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-08-29 11:50:05 -0700

    Release v7.0.0-beta.1 [ci skip]

--- assets/js/llms-components.asset.php
+++ assets/js/llms-components.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '3d0f0709791dfeaecb31');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '7877c4b02db507e252bb');
--- class-lifterlms.php
+++ class-lifterlms.php
@@ -34,7 +34,7 @@ final class LifterLMS {
 	 *
 	 * @var string
 	 */
-	public $version = '6.10.0';
+	public $version = '7.0.0-beta.1';
 
 	/**
 	 * LLMS_Assets instance
