920146085ae3df4486b8d9e6bf5fd8522a9819d2
Thomas Patrick Levy <thomasplevy@gmail.com>
2022-08-29 11:04:22 -0700

    Release v6.10.0 [ci skip]

--- blocks/certificate-title/index.asset.php
+++ blocks/certificate-title/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '12729fdf5831d2a8e268');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9bc26c644a1b6c996c1c');
