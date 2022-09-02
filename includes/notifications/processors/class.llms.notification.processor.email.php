625e2dc6b5bee24686222734b782ed7fcb8400f3
Rocco Aliberti <raliberti84@gmail.com>
2022-09-02 11:05:02 +0200

    Catch possible fatal when preparing notifcation email to be sent and in that case remove from the queue the item that produced it

--- includes/notifications/processors/class.llms.notification.processor.email.php
+++ includes/notifications/processors/class.llms.notification.processor.email.php
@@ -5,7 +5,7 @@
  * @package LifterLMS/Notifications/Processors/Classes
  *
  * @since 3.8.0
- * @version 3.33.2
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -27,11 +27,12 @@ class LLMS_Notification_Processor_Email extends LLMS_Abstract_Notification_Proce
 	protected $action = 'llms_notification_processor_email';
 
 	/**
-	 * Processes an item in the queue
+	 * Processes an item in the queue.
 	 *
 	 * @since 3.8.0
 	 * @since 3.10.1 Unknown.
 	 * @since 3.33.2 Log additional data during errors.
+	 * @since [version] Catch possible fatals and in that case remove from the queue the item that produced them.
 	 *
 	 * @param int $notification_id ID of an LLMS_Notification.
 	 * @return bool `false` removes item from queue, `true` retain for further processing.
@@ -39,29 +40,36 @@ class LLMS_Notification_Processor_Email extends LLMS_Abstract_Notification_Proce
 	protected function task( $notification_id ) {
 
 		$this->log( sprintf( 'sending email notification ID #%d', $notification_id ) );
+		try {
 
-		$notification = new LLMS_Notification( $notification_id );
+			$notification = new LLMS_Notification( $notification_id );
 
-		$view = $notification->get_view();
+			$view = $notification->get_view();
 
-		if ( ! $view ) {
-			$this->log( 'ID#' . $notification_id );
+			if ( ! $view ) {
+				$this->log( 'ID#' . $notification_id );
+				return false;
+			}
+
+			// Setup the email.
+			$mailer = llms()->mailer()->get_email( 'notification' );
+
+			if ( ! $mailer->add_recipient( $notification->get( 'subscriber' ), 'to' ) ) {
+				$this->log( sprintf( 'error sending email notification ID #%d - subscriber does not exist', $notification_id ) );
+				$this->log( $notification->toArray() );
+				$notification->set( 'status', 'error' );
+				return false;
+			}
+
+			$mailer->set_subject( $view->get_subject() )->set_heading( $view->get_title() )->set_body( $view->get_html() );
+
+		} catch ( Error $e ) {
+			$this->log( sprintf( 'error sending email notification ID #%d', $notification_id ) );
+			$this->log( sprintf( 'Error caught %1$s in %2$s on line %3$s', $e->getMessage(), $e->getFile(), $e->getLine() ) );
 			return false;
 		}
 
-		// setup the email.
-		$mailer = llms()->mailer()->get_email( 'notification' );
-
-		if ( ! $mailer->add_recipient( $notification->get( 'subscriber' ), 'to' ) ) {
-			$this->log( sprintf( 'error sending email notification ID #%d - subscriber does not exist', $notification_id ) );
-			$this->log( $notification->toArray() );
-			$notification->set( 'status', 'error' );
-			return false;
-		}
-
-		$mailer->set_subject( $view->get_subject() )->set_heading( $view->get_title() )->set_body( $view->get_html() );
-
-		// log when wp_mail fails.
+		// Log when wp_mail fails.
 		if ( $mailer->send() ) {
 			$notification->set( 'status', 'sent' );
 		} else {
