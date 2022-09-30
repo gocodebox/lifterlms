c0b23cfea36f2232dc7b9a0a2e251fff55e38aa6
Rocco Aliberti <raliberti84@gmail.com>
2022-09-30 12:00:54 +0200

    Fixed unclosed checkout div wrapper on empty cart

--- tests/phpunit/unit-tests/shortcodes/class-llms-test-shortcode-checkout.php
+++ tests/phpunit/unit-tests/shortcodes/class-llms-test-shortcode-checkout.php
@@ -5,12 +5,11 @@
  * @group shortcodes
  *
  * @since 5.1.0
- * @version 5.1.0
  */
 class LLMS_Test_Shortcode_Checkout extends LLMS_ShortcodeTestCase {
 
 	/**
-	 * Test shortcode registration
+	 * Test shortcode registration.
 	 *
 	 * @since 5.1.0
 	 *
@@ -21,7 +20,7 @@ class LLMS_Test_Shortcode_Checkout extends LLMS_ShortcodeTestCase {
 	}
 
 	/**
-	 * Test clean_form_fields
+	 * Test clean_form_fields.
 	 *
 	 * @since 5.1.0
 	 *
@@ -43,4 +42,172 @@ class LLMS_Test_Shortcode_Checkout extends LLMS_ShortcodeTestCase {
 		}
 
 	}
+
+	/**
+	 * Test checkout wrapper on empty cart.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_checkout_wrapper_on_empty_cart() {
+
+		$this->assertOutputContains(
+			'Your cart is currently empty.',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'<div class="llms-checkout-wrapper">',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'</div><!-- .llms-checkout-wrapper -->',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+	}
+
+	/**
+	 * Test checkout wrapper on pre checkout error.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_checkout_wrapper_on_pre_checkout_error() {
+
+		$pre_checkout_error = function() {
+			return 'Pre checkout error.';
+		};
+		add_filter( 'lifterlms_pre_checkout_error', $pre_checkout_error );
+
+		$this->assertOutputContains(
+			'Pre checkout error.',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'<div class="llms-checkout-wrapper">',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'</div><!-- .llms-checkout-wrapper -->',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		remove_filter( 'lifterlms_pre_checkout_error', $pre_checkout_error );
+
+	}
+
+
+	/**
+	 * Test checkout wrapper when invalid access plan is supplied.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_checkout_wrapper_confirm_payment_invalid_plan() {
+
+		$this->mockGetRequest( array( 'plan' => $this->factory->post->create() ) );
+
+		$this->assertOutputContains(
+			'Invalid access plan.',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'<div class="llms-checkout-wrapper">',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'</div><!-- .llms-checkout-wrapper -->',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+	}
+
+	/**
+	 * Test checkout wrapper on confirm payment when no order is supplied.
+	 *
+	 * @since [version]
+	 *
+	 * @return void
+	 */
+	public function test_checkout_wrapper_confirm_payment_no_order() {
+		global $wp;
+		$wpt = $wp;
+		$wp->query_vars['confirm-payment'] = true;
+
+		$this->assertOutputContains(
+			'Could not locate an order to confirm.',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'<div class="llms-checkout-wrapper">',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$this->assertOutputContains(
+			'</div><!-- .llms-checkout-wrapper -->',
+			array(
+				'LLMS_Shortcode_Checkout',
+				'output',
+				array( null )
+			)
+		);
+
+		$wp = $wpt;
+
+	}
+
 }
