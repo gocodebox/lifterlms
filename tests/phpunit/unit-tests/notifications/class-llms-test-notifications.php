51f81827db57c3807a10bb1da8bbcb9e892556e1
Rocco Aliberti <raliberti84@gmail.com>
2022-11-21 10:37:55 +0100

    fix tests

--- tests/phpunit/unit-tests/notifications/class-llms-test-notifications.php
+++ tests/phpunit/unit-tests/notifications/class-llms-test-notifications.php
@@ -4,10 +4,10 @@
  *
  * @package LifterLMS/Tests/Notifications
  *
+ * @group notifications
+ *
  * @since 3.8.0
  * @since 3.38.0 "DRY"ed existing tests and added tests for processor scheduling related functions.
- *
- * @group notifications
  */
 class LLMS_Test_Notifications extends LLMS_UnitTestCase {
 
@@ -283,15 +283,28 @@ class LLMS_Test_Notifications extends LLMS_UnitTestCase {
 	 */
 	public function test_email_processor_errored_notification_task() {
 
-		$this->markTestSkipped( "in progress" );
-
 		$email = $this->main->get_processor( 'email' );
 		$user  = $this->factory->user->create();
 
 		$order = $this->get_mock_order();
 		$txn   = $order->record_transaction(
 			array(
-				'amount'             => $order->get_initial_price( array(), 'float' ),
+				'amount'             => $order->get_initial_price(
+					array(),
+					'float'
+				),
+				'source_description' => 'Mock Payment',
+				'transaction_id'     => uniqid( 'mock-' ),
+				'status'             => 'llms-txn-succeeded',
+				'payment_gateway'    => 'manual',
+			)
+		);
+		$txn2  = $order->record_transaction(
+			array(
+				'amount'             => $order->get_initial_price(
+					array(),
+					'float'
+				),
 				'source_description' => 'Mock Payment',
 				'transaction_id'     => uniqid( 'mock-' ),
 				'status'             => 'llms-txn-succeeded',
@@ -299,34 +312,71 @@ class LLMS_Test_Notifications extends LLMS_UnitTestCase {
 			)
 		);
 
-		// Create a notification for a purcahse receipt.
+		// Create a notification for a purchase receipt on txn.
 		$n1    = new LLMS_Notification();
 		$nid_1 = $n1->create(
 			array(
-				'post_id'    => $txn->get('id'),
+				'post_id'    => $txn->get( 'id' ),
 				'subscriber' => $user,
 				'type'       => 'basic',
 				'trigger_id' => 'purchase_receipt',
 				'user_id'    => 1,
 			)
 		);
-		$this->assertEquals( 'new', $n1->get('status') );
-		// Process notification email.
+		$this->assertEquals( 'new', $n1->get( 'status' ) );
+
+		// Create a notification for a purchase receipt on txn2.
+		$n2    = new LLMS_Notification();
+		$nid_2 = $n2->create(
+			array(
+				'post_id'    => $txn2->get( 'id' ),
+				'subscriber' => $user,
+				'type'       => 'basic',
+				'trigger_id' => 'purchase_receipt',
+				'user_id'    => 1,
+			)
+		);
+
+		// Process notification email for the first transaction.
 		$email_processor = $this->main->get_processor( 'email' );
-		$res = LLMS_Unit_Test_Util::call_method( $email_processor, 'task', array( $nid_1 ) );
+		$res = LLMS_Unit_Test_Util::call_method(
+			$email_processor, 'task',
+			array( $nid_1 )
+		);
 		$this->assertEquals( false, $res );
 		$this->assertEquals( 'sent', $n1->get('status') );
 
-		// Delete the order so that a fatal error will be produced.
-		wp_delete_post( $order->get('id') );
+		// Delete the first transaction so that a fatal will be triggered.
+		wp_delete_post( $txn->get( 'id' ) );
+		$res = LLMS_Unit_Test_Util::call_method(
+			$email_processor,
+			'task',
+			array( $nid_1 )
+		);
 
-		$res = LLMS_Unit_Test_Util::call_method( $email_processor, 'task', array( $nid_1 ) );
 		$this->assertEquals( false, $res );
 		$this->assertEquals( 'error', $n1->get('status') );
 
-		$this->assertContains(
-			'Error caught Call to a member function get() on null',
-			$this->logs->get( 'notifications' )
+		$this->assertStringContainsString(
+			'Error caught Call to a member function get_order() on null',
+			implode( $this->logs->get( 'notifications' ) )
+		);
+
+		$this->logs->clear( 'notifications' );
+
+		// Process notification email for the second transaction.
+		$email_processor = $this->main->get_processor( 'email' );
+		$res = LLMS_Unit_Test_Util::call_method(
+			$email_processor, 'task',
+			array( $nid_2 )
+		);
+		$this->assertEquals( false, $res );
+		// Check the previous error didn't prevent this notification to be sent.
+		$this->assertEquals( 'sent', $n2->get('status') );
+
+		$this->assertStringNotContainsString(
+			'Error caught Call to a member function get_order() on null',
+			implode( $this->logs->get( 'notifications' ) )
 		);
 
 		$this->logs->clear( 'notifications' );
