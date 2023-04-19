9e29257a974399f4e95078423c7d88590d55f7cd
actuallyakash <akash_gupta@hotmail.com>
2023-04-19 11:23:40 +0530

    Added post ID parameter to the post title filter

--- includes/functions/llms-functions-forms.php
+++ includes/functions/llms-functions-forms.php
@@ -5,7 +5,7 @@
  * @package LifterLMS/Functions/Forms
  *
  * @since 5.0.0
- * @version 5.10.0
+ * @version [version]
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -76,6 +76,7 @@ function llms_get_form_html( $location, $args = array() ) {
  *
  * @since 5.0.0
  * @since 5.10.0 Return specific form title for checkout forms and free access plans.
+ * @since [version] Added 3rd missing `$post_id` parameter for the Post Title Filter.
  *
  * @param string $location Form location, one of: "checkout", "registration", or "account".
  * @param array  $args Additional arguments passed to the short-circuit filter in `LLMS_Forms->get_form_post()`.
@@ -90,7 +91,7 @@ function llms_get_form_title( $location, $args = array() ) {
 
 	return 'checkout' === $location && isset( $args['plan'] ) && $args['plan']->is_free()
 		?
-		apply_filters( 'the_title', get_post_meta( $post->ID, '_llms_form_title_free_access_plans', true ) )
+		apply_filters( 'the_title', get_post_meta( $post->ID, '_llms_form_title_free_access_plans', true ), $post->ID )
 		:
 		get_the_title( $post->ID );
 
