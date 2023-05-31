e2db3a28cb09a347ae0fad52e8fe9079783c3876
actuallyakash <akash_gupta@hotmail.com>
2023-05-31 14:59:31 +0530

    minor fixes

--- templates/course/lesson-count.php
+++ templates/course/lesson-count.php
@@ -1,24 +1,21 @@
 <?php
 /**
- * Lessons count in a Course.
+ * Template: Lessons count in a Course.
  *
- * @package LifterLMS/Templates
+ * @package LifterLMS/Templates/Course
  *
  * @since [version]
  * @version [version]
- *
- * @author      LifterLMS
- * @package     LifterLMS/Templates
  */
 
 defined( 'ABSPATH' ) || exit;
 
 global $post;
 
-$course = new LLMS_Course( $post );
+$course        = new LLMS_Course( $post );
 $lessons_count = $course->get_lessons_count();
 ?>
 
 <div class="llms-meta llms-lessons-count">
-	<p><?php printf( __( 'Number of Lessons: <span class="lessons-count">%u</span>', 'lifterlms' ), $lessons_count ); ?></p>
+	<?php printf( '<p>%1$s<span class="lessons-count">%2$s</span></p>', esc_html__( 'Number of Lessons: ', 'lifterlms' ), $lessons_count ); ?>
 </div>
