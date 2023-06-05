3064237db17301e532b08ba7fd5f44ee2f62741a
SEO Themes <seothemeswp@gmail.com>
2023-06-05 18:14:10 +0800

    Add `llms-block-icon` class to SVG icons for better styling

--- src/blocks/access-plan-button/icon.jsx
+++ src/blocks/access-plan-button/icon.jsx
@@ -3,7 +3,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 // FontAwesome rectangle-wide regular.
 const Icon = () => (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+	<SVG className="llms-block-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
 		<Path
 			d="M592 416H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h544c26.5 0 48 21.5 48 48v224c0 26.5-21.5 48-48 48z"
 		/>
--- src/blocks/course-continue/icon.jsx
+++ src/blocks/course-continue/icon.jsx
@@ -3,7 +3,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 // FontAwesome bars-progress solid.
 const Icon = () => (
-	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+	<SVG className="llms-block-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 		<Path
 			d="M448 160H320V128H448v32zM48 64C21.5 64 0 85.5 0 112v64c0 26.5 21.5 48 48 48H464c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zM448 352v32H192V352H448zM48 288c-26.5 0-48 21.5-48 48v64c0 26.5 21.5 48 48 48H464c26.5 0 48-21.5 48-48V336c0-26.5-21.5-48-48-48H48z"
 		/>
