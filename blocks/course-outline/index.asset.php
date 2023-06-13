daf11e9210ede4291746137fc977abb1499f86a1
Rocco Aliberti <raliberti84@gmail.com>
2023-06-13 18:21:20 +0200

    add new blocks

---
+++ blocks/course-outline/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '2a24155157244ed4dcee');
---
+++ blocks/course-reviews/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'd28571b1411c332aef62');
