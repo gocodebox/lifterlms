9301fd2962392703d1ab06c391288be2626430d9
SEO Themes <seothemeswp@gmail.com>
2023-06-16 20:50:28 +0800

    Add css and js for wizard checkbox toggles

--- assets/js/llms-admin-setup.js
+++ assets/js/llms-admin-setup.js
@@ -6,28 +6,25 @@
  */
 
 ( function() {
-
-	var
-		currStep = document.getElementById( 'llms-setup-current-step' ),
+	const		currStep = document.getElementById( 'llms-setup-current-step' ),
 		exitLink = document.querySelector( '.llms-exit-setup' ),
-		imports  = document.querySelectorAll( 'input[name="llms_setup_course_import_ids[]"]' );
+		imports = document.querySelectorAll( 'input[name="llms_setup_course_import_ids[]"]' ),
+		checkboxToggle = document.getElementsByClassName( 'llms-checkbox-toggle' )[ 0 ] ?? null;
 
 	if ( imports.length ) {
-
-		var
+		const
 			submit = document.getElementById( 'llms-setup-submit' ),
-			msgs   = document.querySelectorAll( '.llms-importing-msgs .llms-importing-msg' );
+			msgs = document.querySelectorAll( '.llms-importing-msgs .llms-importing-msg' );
 
 		/**
 		 * Retrieve the number of courses to be imported
 		 *
 		 * @since 4.8.0
 		 *
-		 * @return {Integer}
+		 * @return {number} The number of courses to be imported.
 		 */
 		function getSelectedImportCount() {
-
-			var count = 0;
+			let count = 0;
 
 			imports.forEach( function( el ) {
 				if ( el.checked ) {
@@ -36,7 +33,6 @@
 			} );
 
 			return count;
-
 		}
 
 		/**
@@ -45,48 +41,41 @@
 		 * @since 4.8.0
 		 */
 		imports.forEach( function( el ) {
-
 			el.addEventListener( 'change', function() {
-
 				// Hide all messages.
-				msgs.forEach( function( el ) {
-					el.style.display = 'none';
+				msgs.forEach( function( msg ) {
+					msg.style.display = 'none';
 				} );
 
-				var selectedCount = getSelectedImportCount();
+				const selectedCount = getSelectedImportCount();
 
 				// If there's no courses to be imported, disable the submit button.
 				submit.disabled = 0 === getSelectedImportCount() ? 'disabled' : null;
 
 				// Show messages where applicable.
 				if ( 1 === selectedCount ) {
-					msgs[0].style.display = 'block';
+					msgs[ 0 ].style.display = 'block';
 				} else if ( selectedCount >= 2 ) {
-					msgs[1].style.display = 'block';
+					msgs[ 1 ].style.display = 'block';
 					document.getElementById( 'llms-importing-number' ).textContent = selectedCount;
 				}
-
 			} );
-
 		} );
 
 		// Trigger a change event so the UI displays properly on page load.
-		imports[0].dispatchEvent( new Event( 'change' ) );
+		imports[ 0 ].dispatchEvent( new Event( 'change' ) );
 
 		/**
 		 * Start a spinner when the "Import Courses" button is clicked.
 		 *
 		 * @since 4.8.0
 		 */
-		submit.addEventListener( 'click', function( e ) {
+		submit.addEventListener( 'click', function() {
 			LLMS.Spinner.start( jQuery( submit ), 'small' );
 		} );
-
 	}
 
-
 	if ( exitLink && 'finish' !== currStep.value ) {
-
 		/**
 		 * When users click "Exit Setup" prior to setup completion, open a confirmation dialog
 		 *
@@ -97,7 +86,15 @@
 				e.preventDefault();
 			}
 		} );
-
 	}
 
-} )();
+	if ( checkboxToggle ) {
+		checkboxToggle.addEventListener( 'click', function() {
+			const hiddenFields = document.getElementsByClassName( 'llms-setup-date-fields' )[ 0 ];
+
+			if ( hiddenFields ) {
+				hiddenFields.classList.toggle( 'is-hidden' );
+			}
+		} );
+	}
+}() );
