0b20757adf3087d767208d73c8ccd05bb838b0e4
SEO Themes <seothemeswp@gmail.com>
2023-06-28 20:30:43 +0800

    Add `$show_nav` param to `lifterlms_student_dashboard_header` hook

--- templates/myaccount/header.php
+++ templates/myaccount/header.php
@@ -15,10 +15,14 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php
 	/**
+	 * @since [version] Added the $show_nav parameter.
+	 *
 	 * @hooked lifterlms_template_my_account_navigation - 10
 	 * @hooked lifterlms_template_student_dashboard_title - 20
+	 *
+	 * @param bool $show_nav Whether to show the navigation.
 	 */
-	do_action( 'lifterlms_student_dashboard_header' );
+	do_action( 'lifterlms_student_dashboard_header', true );
 	?>
 
 </header>
