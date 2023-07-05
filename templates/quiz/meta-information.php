398a1f573e7ab53880fd22c3ac96190deb213f7b
actuallyakash <akash_gupta@hotmail.com>
2023-07-05 17:36:22 +0530

    escaping

--- templates/quiz/meta-information.php
+++ templates/quiz/meta-information.php
@@ -31,21 +31,21 @@ $passing_percent = $quiz->get( 'passing_percent' );
 <ul class="llms-quiz-meta-info">
 	<?php if ( $passing_percent ) : ?>
 	<li class="llms-quiz-meta-item llms-passing-percent">
-		<?php printf( __( 'Minimum Passing Grade: %s', 'lifterlms' ), '<span class="llms-pass-perc">' . $passing_percent . '%</span>' ); ?>
+		<?php printf( esc_html__( 'Minimum Passing Grade: %s', 'lifterlms' ), '<span class="llms-pass-perc">' . $passing_percent . '%</span>' ); ?>
 	</li>
 	<?php endif; ?>
 
 	<li class="llms-quiz-meta-item llms-attempts">
-		<?php printf( __( 'Remaining Attempts: %s', 'lifterlms' ), '<span class="llms-attempts">' . $student->quizzes()->get_attempts_remaining_for_quiz( $quiz->get( 'id' ) ) . '</span>' ); ?>
+		<?php printf( esc_html__( 'Remaining Attempts: %s', 'lifterlms' ), '<span class="llms-attempts">' . $student->quizzes()->get_attempts_remaining_for_quiz( $quiz->get( 'id' ) ) . '</span>' ); ?>
 	</li>
 
 	<li class="llms-quiz-meta-item llms-question-count">
-		<?php printf( __( 'Questions: %s', 'lifterlms' ), '<span class="llms-question-count">' . $quiz->get_questions_count() . '</span>' ); ?>
+		<?php printf( esc_html__( 'Questions: %s', 'lifterlms' ), '<span class="llms-question-count">' . esc_html( $quiz->get_questions_count() ) . '</span>' ); ?>
 	</li>
 
 	<?php if ( $quiz->has_time_limit() ) : ?>
 	<li class="llms-quiz-meta-item llms-time-limit">
-		<?php printf( __( 'Time Limit: %s', 'lifterlms' ), '<span class="llms-time-limit">' . $quiz->get_time_limit_string() . '</span>' ); ?>
+		<?php printf( esc_html__( 'Time Limit: %s', 'lifterlms' ), '<span class="llms-time-limit">' . $quiz->get_time_limit_string() . '</span>' ); ?>
 	</li>
 	<?php endif; ?>
 </ul>
