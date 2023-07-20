88ac2b66ee98db47410a2bc42a587f242e28cd59
Rocco Aliberti <raliberti84@gmail.com>
2023-07-20 16:24:32 +0200

    Fix cs on sidebar.php

--- templates/global/sidebar.php
+++ templates/global/sidebar.php
@@ -15,7 +15,7 @@ $core_fallback     = ABSPATH . WPINC . '/theme-compat/sidebar.php';
 $sidebar_templates = array( 'sidebar-llms_shop.php', 'sidebar.php' );
 
 // Return early if using block theme with no sidebar template.
-if ( wp_is_block_theme() && $core_fallback === locate_template( $sidebar_templates ) ) {
+if ( wp_is_block_theme() && locate_template( $sidebar_templates ) === $core_fallback ) {
 	return;
 }
 
