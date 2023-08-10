1294e795f125876258e56d99b2a60a1291c7f6b2
SEO Themes <seothemeswp@gmail.com>
2023-08-10 20:04:06 +0800

    Update `setup-wizard/main` template changelog and escape all output

--- includes/admin/views/setup-wizard/main.php
+++ includes/admin/views/setup-wizard/main.php
@@ -4,8 +4,9 @@
  *
  * @package LifterLMS/Views/Admin/SetupWizard
  *
- * @since   4.4.4
- * @version [version]
+ * @since 4.4.4
+ * @since 4.8.0 Unknown.
+ * @since [version] Escape output.
  *
  * @property string[]                $steps     Array of setup wizard steps.
  * @property string                  $current   Slug of the current step.
@@ -24,14 +25,14 @@ defined( 'ABSPATH' ) || exit;
 
 		<h1 id="llms-logo">
 			<a href="https://lifterlms.com/" target="_blank">
-				<img src="<?php echo llms()->plugin_url(); ?>/assets/images/lifterlms-logo-black.png" alt="LifterLMS">
+				<img src="<?php echo esc_url( llms()->plugin_url() . '/assets/images/lifterlms-logo-black.png' ); ?>" alt="LifterLMS">
 			</a>
 		</h1>
 
 		<ul class="llms-setup-progress">
 			<?php foreach ( $steps as $slug => $step ) : ?>
 				<li<?php echo ( $slug === $current ) ? ' class="current"' : ''; ?>>
-					<a href="<?php echo $this->get_step_url( $slug ); ?>"><?php echo esc_html( $step['title'] ?? '' ); ?></a>
+					<a href="<?php echo esc_url( $this->get_step_url( $slug ) ); ?>"><?php echo esc_html( $step['title'] ?? '' ); ?></a>
 				</li>
 			<?php endforeach; ?>
 		</ul>
@@ -42,7 +43,7 @@ defined( 'ABSPATH' ) || exit;
 				<?php echo $step_html; ?>
 
 				<?php if ( is_wp_error( $this->error ) ) : ?>
-					<p class="error"><?php echo $this->error->get_error_message(); ?></p>
+					<p class="error"><?php echo esc_html( $this->error->get_error_message() ); ?></p>
 				<?php endif; ?>
 
 				<p class="llms-setup-actions">
@@ -54,15 +55,19 @@ defined( 'ABSPATH' ) || exit;
 						<a class="llms-exit-setup" data-confirm="<?php esc_attr_e( 'The site setup is incomplete! Are you sure you wish to exit?', 'lifterlms' ); ?>" href="<?php echo esc_url( admin_url( 'admin.php?page=llms-settings' ) ); ?>"><?php esc_html_e( 'Exit Setup', 'lifterlms' ); ?></a>
 
 						<?php if ( $next ) : ?>
-							<a href="<?php echo $this->get_step_url( $next ); ?>" class="llms-button-secondary large"><?php echo $this->get_skip_text( $current ); ?></a>
+							<a href="<?php echo esc_url( $this->get_step_url( $next ) ); ?>" class="llms-button-secondary large">
+								<?php echo esc_html( $this->get_skip_text( $current ) ); ?>
+							</a>
 						<?php endif; ?>
 
 						<?php if ( 'finish' === $current ) : ?>
 							<a href="<?php echo esc_url( admin_url( 'post-new.php?post_type=course' ) ); ?>" class="llms-button-secondary large"><?php esc_html_e( 'Start from Scratch', 'lifterlms' ); ?></a>
 						<?php endif; ?>
 
-						<button class="llms-button-primary large" type="submit" id="llms-setup-submit"><?php echo $this->get_save_text( $current ); ?></button>
-						<input id="llms-setup-current-step" name="llms_setup_save" type="hidden" value="<?php echo $current; ?>">
+						<button class="llms-button-primary large" type="submit" id="llms-setup-submit">
+							<?php echo esc_html( $this->get_save_text( $current ) ); ?>
+						</button>
+						<input id="llms-setup-current-step" name="llms_setup_save" type="hidden" value="<?php echo esc_attr( $current ); ?>">
 						<?php wp_nonce_field( 'llms_setup_save', 'llms_setup_nonce' ); ?>
 					<?php endif; ?>
 				</p>
