e64852bcd90d66eacd1cb02050644b07dcc857e0
Akash <18614782+actuallyakash@users.noreply.github.com>
2023-09-01 18:01:23 +0530

    Apply suggestions from code review
    Co-authored-by: Rocco Aliberti <raliberti84@gmail.com>

--- includes/functions/llms-functions-wrappers.php
+++ includes/functions/llms-functions-wrappers.php
@@ -98,7 +98,7 @@ if ( ! function_exists( 'llms_redirect_and_exit' ) ) {
 	 *
 	 * @since 3.19.4
 	 * @since 5.3.0 Moved location from `includes/llms.functions.core.php`.
-	 * @since [version] Added `nocache_headers()` to prevent caching of redirects.
+	 * @since [version] Added `nocache_headers()` to prevent caching of temporary redirects.
 	 *
 	 * @link https://github.com/gocodebox/lifterlms-tests/blob/472c5a286e9f65e2be0c1d6b7edd8d5340d052ed/framework/functions-llms-tests.php#L178-L199
 	 *
