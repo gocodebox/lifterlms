c55bf04739250b00a8cb0add6776250b476e405c
Jason Coleman <jason@strangerstudios.com>
2024-01-03 09:10:54 -0500

    Added action and description links to the plugins page.

--- includes/class-llms-loader.php
+++ includes/class-llms-loader.php
@@ -332,6 +332,7 @@ class LLMS_Loader {
 		// Admin classes.
 		require_once LLMS_PLUGIN_DIR . 'includes/admin/class-llms-admin-header.php';
 		require_once LLMS_PLUGIN_DIR . 'includes/admin/class-llms-admin-export-download.php';
+		require_once LLMS_PLUGIN_DIR . 'includes/admin/class-llms-admin-plugins.php';
 		require_once LLMS_PLUGIN_DIR . 'includes/admin/class-llms-admin-review.php';
 		require_once LLMS_PLUGIN_DIR . 'includes/admin/class-llms-admin-users-table.php';
 		require_once LLMS_PLUGIN_DIR . 'includes/admin/class-llms-mailhawk.php';
