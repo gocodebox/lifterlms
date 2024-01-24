96b10880a9476aa78900f8bf0fd5e14313665363
Jason Coleman <jason@strangerstudios.com>
2024-01-24 09:51:37 -0500

    Let's only set the major.minor version.

--- lifterlms.php
+++ lifterlms.php
@@ -18,7 +18,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Requires at least: 5.9
- * Tested up to: 6.4.2
+ * Tested up to: 6.4
  * Requires PHP: 7.4
  *
  * * * * * * * * * * * * * * * * * * * * * *
