5c3e28da06bc4d2a02d88594bf9161968a664050
Kim Coleman <kim@strangerstudios.com>
2024-03-06 18:38:08 -0500

    Styling the expand all / collapse all buttons; tightening up sidebar

--- includes/admin/views/builder/utilities.php
+++ includes/admin/views/builder/utilities.php
@@ -11,13 +11,13 @@
 	<ul class="llms-utilities-list">
 		<li>
 			<button id="llms-expand-all" class="llms-utility bulk-toggle" data-action="expand">
-				<span class="fa fa-plus-circle"></span>
+				<span class="fa fa-caret-down"></span>
 				<?php esc_html_e( 'Expand Sections', 'lifterlms' ); ?>
 			</button>
 		</li>
 		<li>
 			<button id="llms-collapse-all" class="llms-utility bulk-toggle" href="#llms-bulk-toggle" data-action="collapse">
-				<span class="fa fa-minus-circle"></span>
+				<span class="fa fa-caret-up"></span>
 				<?php esc_html_e( 'Collapse Sections', 'lifterlms' ); ?>
 			</button>
 		</li>
