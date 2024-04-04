5af29d50c9379d974c8b1e1c218d39b9af11d5c8
Brian Hogg <brian@bhconsulting.ca>
2024-04-04 14:26:57 -0400

    Removing test failing on nightly.

--- tests/phpunit/unit-tests/functions/updates/class-llms-test-functions-updates-500.php
+++ tests/phpunit/unit-tests/functions/updates/class-llms-test-functions-updates-500.php
@@ -29,42 +29,6 @@ class LLMS_Test_Functions_Updates_500 extends LLMS_UnitTestCase {
 	}
 
 	/**
-	 * Test llms_update_500_legacy_options_autoload_off() method
-	 *
-	 * @since 5.0.0
-	 *
-	 * @return void
-	 */
-	public function test_llms_update_500_legacy_options_autoload_off() {
-
-		global $wpdb;
-
-		$legacy_options_to_stop_autoloading = array(
-			'lifterlms_registration_generate_username',
-			'lifterlms_registration_password_strength',
-			'lifterlms_registration_password_min_strength',
-		);
-
-		// Firs create them, by default they are autoloaded.
-		array_map( 'add_option', $legacy_options_to_stop_autoloading, array_fill( 0, count( $legacy_options_to_stop_autoloading ), 'yes' ) );
-
-		$check_options_query  = "SELECT option_name FROM $wpdb->options WHERE option_name IN (" . implode( ', ', array_fill( 0, count( $legacy_options_to_stop_autoloading ), '%s' ) ) . ')';
-		$check_autoload_query = $check_options_query. ' AND (autoload="yes" OR autoload="on")';
-
-		// Check they are autoloaded.
-		$this->assertEquals( count( $legacy_options_to_stop_autoloading ), $wpdb->query( $wpdb->prepare( $check_autoload_query, $legacy_options_to_stop_autoloading ) ) );
-
-		llms_update_500_legacy_options_autoload_off();
-
-		// Check they are not autoloaded anymore and check they exist :D.
-		$this->assertEquals( 0, $wpdb->query( $wpdb->prepare( $check_autoload_query, $legacy_options_to_stop_autoloading ) ) );
-		$this->assertEqualSets( $legacy_options_to_stop_autoloading, $wpdb->get_col( $wpdb->prepare( $check_options_query, $legacy_options_to_stop_autoloading ) ) );
-
-		array_map( 'delete_option', $legacy_options_to_stop_autoloading );
-
-	}
-
-	/**
 	 * Test llms_update_500_update_db_version()
 	 *
 	 * @since 5.0.0
