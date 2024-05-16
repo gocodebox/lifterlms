562f3c14f661dfc5888995c2cd7cccb7e9277393
Brian Hogg <brian@bhconsulting.ca>
2024-05-16 14:50:27 -0400

    Escaping output.

--- includes/admin/views/builder/question-choice.php
+++ includes/admin/views/builder/question-choice.php
@@ -28,13 +28,13 @@
 				<div class="llms-image">
 					<a class="llms-action-icon danger tip--top-left" data-attribute="choice" data-tip="<?php esc_attr_e( 'Remove image', 'lifterlms' ); ?>" href="#llms-remove-image">
 						<i class="fa fa-times-circle" aria-hidden="true"></i>
-						<span class="screen-reader-text"><?php _e( 'Remove image', 'lifterlms' ); ?></span>
+						<span class="screen-reader-text"><?php esc_html_e( 'Remove image', 'lifterlms' ); ?></span>
 					</a>
 					<img alt="<?php esc_attr_e( 'image preview', 'lifterlms' ); ?>" src="{{{ data.get( 'choice' ).get( 'src' ) }}}">
 				</div>
 			<# } else { #>
 				<button class="llms-element-button small llms-add-image" data-attribute="choice" data-image-size="full">
-					<span class="fa fa-picture-o"></span> <?php _e( 'Add Image', 'lifterlms' ); ?>
+					<span class="fa fa-picture-o"></span> <?php esc_html_e( 'Add Image', 'lifterlms' ); ?>
 				</button>
 			<# } #>
 		</div>
@@ -42,14 +42,14 @@
 
 	<div class="llms-action-icons">
 
-		<a class="llms-action-icon circle tip--top-left" data-tip="<?php _e( 'Add Choice', 'lifterlms' ); ?>" href="#llms-add-choice" tabindex="-1">
+		<a class="llms-action-icon circle tip--top-left" data-tip="<?php esc_attr_e( 'Add Choice', 'lifterlms' ); ?>" href="#llms-add-choice" tabindex="-1">
 			<i class="fa fa-plus" aria-hidden="true"></i>
-			<span class="screen-reader-text"><?php _e( 'Add Choice', 'lifterlms' ); ?></span>
+			<span class="screen-reader-text"><?php esc_html_e( 'Add Choice', 'lifterlms' ); ?></span>
 		</a>
 
-		<a class="llms-action-icon circle danger tip--top-left" data-tip="<?php _e( 'Delete Choice', 'lifterlms' ); ?>" href="#llms-del-choice" tabindex="-1">
+		<a class="llms-action-icon circle danger tip--top-left" data-tip="<?php esc_attr_e( 'Delete Choice', 'lifterlms' ); ?>" href="#llms-del-choice" tabindex="-1">
 			<i class="fa fa-minus" aria-hidden="true"></i>
-			<span class="screen-reader-text"><?php _e( 'Delete Choice', 'lifterlms' ); ?></span>
+			<span class="screen-reader-text"><?php esc_html_e( 'Delete Choice', 'lifterlms' ); ?></span>
 		</a>
 
 	</div>
