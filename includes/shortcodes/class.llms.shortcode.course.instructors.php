ae5d26ecfa5aff895b9ea924da2261b0884b8b48
Brian Hogg <brian@bhconsulting.ca>
2024-06-20 11:03:30 -0400

    Adding lifterlms_course_instructors shortcode.

---
+++ includes/shortcodes/class.llms.shortcode.course.instructors.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * LifterLMS Course Instructors
+ *
+ * [lifterlms_course_instructors]
+ *
+ * @package LifterLMS/Shortcodes/Classes
+ *
+ * @since [version]
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * LLMS_Shortcode_Course_Element
+ *
+ * @since 3.11.1
+ */
+class LLMS_Shortcode_Course_Instructors extends LLMS_Shortcode_Course_Element {
+
+	/**
+	 * Shortcode tag
+	 *
+	 * @var string
+	 */
+	public $tag = 'lifterlms_course_instructors';
+
+	/**
+	 * Retrieves an array of default attributes which are automatically merged
+	 * with the user submitted attributes and passed to $this->get_output()
+	 *
+	 * @since [version]]
+	 *
+	 * @return array
+	 */
+	protected function get_default_attributes() {
+		return array();
+	}
+
+	/**
+	 * Call the template function for the course element
+	 *
+	 * @since 3.11.1
+	 *
+	 * @return void
+	 */
+	protected function template_function() {
+
+		llms_template_instructors();
+	}
+}
+
+return LLMS_Shortcode_Course_Instructors::instance();
