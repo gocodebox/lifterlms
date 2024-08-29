c5bdb648ec29e7b58159b9108b08a7320e88228e
Brian Hogg <brian@bhconsulting.ca>
2024-08-29 11:30:52 -0400

    Use parse_url to allow a broader set of urls.

--- includes/abstracts/abstract.llms.post.model.php
+++ includes/abstracts/abstract.llms.post.model.php
@@ -848,7 +848,7 @@ abstract class LLMS_Post_Model implements JsonSerializable {
 
 		$prop = $prop ? $prop : $type . '_embed';
 		$url  = $this->get( $prop );
-		if ( wp_http_validate_url( $url ) ) {
+		if ( trim( $url ) && parse_url( $url ) ) {
 			$this->get_provider_support( $url );
 
 			$ret = wp_oembed_get( $url );
