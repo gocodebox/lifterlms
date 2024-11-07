cdb11c5fef2ba9f8f110cd9eb6eddde6bfbef301
Brian Hogg <brian@bhconsulting.ca>
2024-11-07 10:46:33 -0500

    Fixing checkout URL if the domain isn't included.

--- includes/admin/views/access-plans/access-plan.php
+++ includes/admin/views/access-plans/access-plan.php
@@ -41,6 +41,10 @@ if ( ! isset( $plan ) ) {
 	$on_sale               = $plan->get( 'on_sale' );
 	$availability          = $plan->get( 'availability' );
 	$checkout_redirect_url = $plan->get( 'checkout_redirect_url' );
+	$checkout_url          = $plan->get_checkout_url( false );
+	if ( false === strpos( $checkout_url, home_url() ) ) {
+		$checkout_url = home_url( $checkout_url );
+	}
 }
 ?>
 
@@ -66,7 +70,7 @@ if ( ! isset( $plan ) ) {
 				<span class="dashicons dashicons-warning"></span>
 			</span>
 			<?php if ( $plan ) : ?>
-				<a target="_blank" href="<?php echo esc_url( $plan->get_checkout_url( false ) ); ?>"><span class="dashicons dashicons-admin-links llms-plan-purchase-link"></span></a>
+				<a target="_blank" href="<?php echo esc_url( $checkout_url ); ?>"><span class="dashicons dashicons-admin-links llms-plan-purchase-link"></span></a>
 			<?php endif; ?>
 			<span class="dashicons dashicons-trash llms-plan-delete"></span>
 			<span class="dashicons dashicons-arrow-down"></span>
@@ -91,7 +95,7 @@ if ( ! isset( $plan ) ) {
 
 		<h4><?php esc_html_e( 'General Plan Information', 'lifterlms' ); ?></h4>
 		<?php if ( $plan ) : ?>
-			<p class="llms-plan-link"><?php printf( esc_html__( 'Direct to Checkout Purchase Link: %s', 'lifterlms' ), '<code>' . esc_url( $plan->get_checkout_url( false ) ) . '</code>' ); ?></p>
+			<p class="llms-plan-link"><?php printf( esc_html__( 'Direct to Checkout Purchase Link: %s', 'lifterlms' ), '<code>' . esc_url( $checkout_url ) . '</code>' ); ?></p>
 		<?php endif; ?>
 
 		<div class="llms-plan-row-1">
