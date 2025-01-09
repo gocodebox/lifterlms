4a1b25d1ce72cad9bf3ed9f7352fb35cab9bbf7b
Brian Hogg <brian@bhconsulting.ca>
2025-01-09 15:19:48 -0500

    Removing. Not seeing an issue with translations loading from Loco translate without it.

--- includes/functions/llms-functions-l10n.php
+++ includes/functions/llms-functions-l10n.php
@@ -89,10 +89,6 @@ function llms_load_textdomain( $domain, $plugin_dir = null, $language_dir = null
 	$plugin_dir   = $plugin_dir ? $plugin_dir : LLMS_PLUGIN_DIR;
 	$language_dir = $language_dir ? $language_dir : 'languages';
 
-	if ( 'lifterlms' !== $domain ) {
-		unload_textdomain( $domain );
-	}
-
 	/**
 	 * Load from the custom LifterLMS "safe" directory (if it exists).
 	 *
