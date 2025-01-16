6855ff94dfe0824ede95529f04e1636560b03707
Brian Hogg <brian@bhconsulting.ca>
2025-01-16 11:10:06 -0500

    Moving hover/background styling to the section so the hover effects look better.

--- templates/course/lesson-preview.php
+++ templates/course/lesson-preview.php
@@ -43,9 +43,13 @@ if ( isset( $total_lessons ) && $total_lessons ) {
 ?>
 
 <div class="llms-lesson-preview<?php echo esc_attr( $lesson->get_preview_classes() ); ?>">
-	<section>
+	<section
+	<?php if ( $restrictions['is_restricted'] ) : ?>
+		class="llms-lesson-locked"
+	<?php endif; ?>
+	>
 		<?php if ( $restrictions['is_restricted'] ) : ?>
-			<div class="llms-lesson-link llms-lesson-link-locked" data-tooltip-msg="<?php echo esc_attr( strip_tags( llms_get_restriction_message( $restrictions ) ) ); ?>">
+			<div class="llms-lesson-link" data-tooltip-msg="<?php echo esc_attr( strip_tags( llms_get_restriction_message( $restrictions ) ) ); ?>">
 		<?php else : ?>
 			<a class="llms-lesson-link" href="<?php echo esc_url( get_permalink( $lesson->get( 'id' ) ) ); ?>" aria-label="<?php echo esc_attr( get_the_title( $lesson->get( 'id' ) ) . ' ' . $lesson_screen_reader_msg ); ?>">
 		<?php endif; ?>
