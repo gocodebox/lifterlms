e6ae8d61ed1307b8e69d3ceefec23c24bfc0c0d3
Brian Hogg <brian@bhconsulting.ca>
2025-01-23 13:55:48 -0500

    Updating to use the next day in the date range at 00:00:00 due to increased precision with MySQL, and less than (vs. the inclusive BETWEEN) to avoid including transactions at 00:00:00 on the next day.

--- includes/admin/reporting/widgets/class.llms.analytics.widget.sold.php
+++ includes/admin/reporting/widgets/class.llms.analytics.widget.sold.php
@@ -114,12 +114,12 @@ class LLMS_Analytics_Sold_Widget extends LLMS_Analytics_Widget {
 						WHERE
 						        ( txns.post_status = 'llms-txn-succeeded' )
 						    AND txns.post_type = 'llms_transaction'
-							AND txns.post_date BETWEEN CAST( %s AS DATETIME ) AND CAST( %s AS DATETIME )
+							AND txns.post_date >= %s
+							AND txns.post_date < %s
 							AND sales.meta_key = '_llms_amount'
 							{$txn_meta_where}
 							ORDER BY txns.post_modified ASC
 						;";
-
 	}
 
 	/**
@@ -136,7 +136,5 @@ class LLMS_Analytics_Sold_Widget extends LLMS_Analytics_Widget {
 			return llms_price_raw( floatval( is_array( $results ) ? array_sum( wp_list_pluck( $results, 'amount' ) ) : $results ) );
 
 		}
-
 	}
-
 }
