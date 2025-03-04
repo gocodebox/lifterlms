3db2c0d98bc0b9f92e5557c8037d5f24cb87b309
Brian Hogg <brian@bhconsulting.ca>
2025-03-04 12:12:17 +0100

    WIP: Adding initial field. Need to initialize the select2 later.

---
+++ includes/admin/class-llms-admin-media-protection-attachment-settings.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * LifterLMS Admin Media Protection Attachment Settings.
+ *
+ * @package LifterLMS/Classes/Admin
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class LLMS_Admin_Media_Protection_Attachment_Settings {
+
+	public function __construct() {
+
+		add_filter( 'attachment_fields_to_edit', array( $this, 'attachment_fields_to_edit' ), 10, 2 );
+		add_filter( 'attachment_fields_to_save', array( $this, 'attachment_fields_to_save' ), 10, 2 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
+	}
+
+	public function admin_scripts() {
+		wp_enqueue_script( 'llms-admin-media-protection-attachment-settings', LLMS_PLUGIN_URL . 'assets/js/llms-admin-media-protection-attachment-settings' . LLMS_ASSETS_SUFFIX . '.js', array( 'jquery', 'media-views', 'wp-i18n', 'llms-admin-scripts' ), LLMS_ASSETS_VERSION, true );
+	}
+
+	/**
+	 * Add the media protection settings to the attachment edit screen
+	 *
+	 * @param   array  $form_fields  Array of form fields
+	 * @param   object $post         WP_Post object
+	 * @return  array
+	 */
+	public function attachment_fields_to_edit( $form_fields, $post ) {
+
+		$form_fields['llms_media_protection'] = array(
+			'label' => __( 'LifterLMS Media Protection', 'lifterlms' ),
+			'input' => 'html',
+			// TODO: Add selected course/membership to the select2 dropdown if known for this attachment post.
+			'html'  => "<select id='attachments-" . $post->ID . "-llms_media_protection' class='llms-posts-select2 llms-media-protection-post-select'></select>",
+		);
+
+		return $form_fields;
+	}
+
+	/**
+	 * Save the media protection settings
+	 *
+	 * @param   array $post     Array of post data
+	 * @param   array $attachment  Array of attachment data
+	 * @return  array
+	 */
+	public function attachment_fields_to_save( $post, $attachment ) {
+
+		if ( isset( $attachment['llms_media_protection'] ) ) {
+			update_post_meta( $post['ID'], '_llms_media_protection', 'yes' );
+		} else {
+			delete_post_meta( $post['ID'], '_llms_media_protection' );
+		}
+
+		return $post;
+	}
+}
+
+new LLMS_Admin_Media_Protection_Attachment_Settings();
