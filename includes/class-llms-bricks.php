442999a435f8c6412917de050f05493c1eebf4cf
Brian Hogg <brian@bhconsulting.ca>
2025-03-31 07:28:44 -0400

    Removing commented code.

--- includes/class-llms-bricks.php
+++ includes/class-llms-bricks.php
@@ -24,33 +24,6 @@ class LLMS_Bricks {
 		add_action( 'init', array( $this, 'register_elements' ), 11 );
 		add_action( 'init', array( $this, 'add_builder_css' ), 11 );
 		add_filter( 'bricks/builder/i18n', array( $this, 'i18n' ) );
-
-		// Prevent uneditable llms post types from being enabled for page building.
-		// add_filter( 'fl_builder_admin_settings_post_types', array( $this, 'remove_uneditable_post_types' ) );
-		//
-		// Add migrateable post types to the builder by default.
-		// add_filter( 'fl_builder_post_types', array( $this, 'enable_post_types_by_default' ) );
-
-		// add_action( 'wp', array( $this, 'load_modules' ), 1 );
-		// add_action( 'init', array( $this, 'load_templates' ) );
-		//
-		// add_filter( 'fl_builder_register_module', array( $this, 'register_module' ), 10, 2 );
-		//
-		// add_filter( 'llms_page_restricted', array( $this, 'mod_page_restrictions' ), 999, 2 );
-		//
-		// add_filter( 'fl_builder_register_settings_form', array( $this, 'add_visibility_settings' ), 999, 2 );
-		//
-		// add_filter( 'fl_builder_is_node_visible', array( $this, 'is_node_visible' ), 10, 2 );
-		//
-		// Hide editors when builder is enabled for a post.
-		// add_filter( 'llms_metabox_fields_lifterlms_course_options', array( $this, 'mod_metabox_fields' ) );
-		// add_filter( 'llms_metabox_fields_lifterlms_membership', array( $this, 'mod_metabox_fields' ) );
-		//
-		// add_filter( 'fl_builder_upgrade_url', array( $this, 'upgrade_url' ) );
-		//
-		// LifterLMS Private Areas.
-		// add_action( 'llms_pa_before_do_area_content', array( $this, 'llms_pa_before_content' ) );
-		// add_action( 'llms_pa_after_do_area_content', array( $this, 'llms_pa_after_content' ) );
 	}
 
 	public function register_elements() {
