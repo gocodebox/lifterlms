9ee9a149da505e863bf9bf90bd74d26c5dc6ff38
Brian Hogg <brian@bhconsulting.ca>
2025-05-23 07:23:41 -0400

    Hiding notices except on the dashboard or a lifterlms page. Also hide if we're in the gutenberg editor.

--- includes/llms-notifications.php
+++ includes/llms-notifications.php
@@ -10,3 +10,38 @@ $GLOBALS['lifterlms_banner_notifications'] = new Gocodebox_Banner_Notifier(
 		'notifications_url' => 'https://notifications.lifterlms.com/v1/notifications.json',
 	)
 );
+
+function llms_maybe_hide_notifications( $priority ) {
+	if ( ! is_admin() ) {
+		return 0;
+	}
+
+	$current_screen = get_current_screen();
+
+	error_log( print_r( $current_screen, true ) );
+
+	if ( ! isset( $current_screen->post_type ) ) {
+		return $priority;
+	}
+
+	// Check if we're on the main WP admin dashboard.
+	if ( 'dashboard' === $current_screen->id ) {
+		return $priority;
+	}
+
+	if ( llms_is_block_editor() ) {
+		return 0;
+	}
+
+	if (
+		strpos( $current_screen->post_type, 'llms_' ) !== 0 &&
+		strpos( $current_screen->base, 'lifterlms' ) !== 0 &&
+		! in_array( $current_screen->post_type, array( 'course', 'lesson' ), true )
+	) {
+		return 0;
+	}
+
+	return $priority;
+}
+
+add_filter( 'lifterlms_max_notification_priority', 'llms_maybe_hide_notifications', 10, 1 );
