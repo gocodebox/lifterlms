9a58278905bcf64f921ad57b1ca446a6e30641de
Brian Hogg <brian@bhconsulting.ca>
2025-07-18 08:10:38 -0400

    Changing the test to just do some spot checks.

--- packages/scripts/e2e/bootstrap.js
+++ packages/scripts/e2e/bootstrap.js
@@ -88,7 +88,11 @@ beforeAll( async () => {
 			return;
 		}
 
-		console.log( `[${ log.type() }] ${ log.text() }` );
+		// Ignore message about attribute width negative value is not valid
+		if ( ! log.text().includes( 'A negative value is not valid' ) ) {
+			console.log( `[${ log.type() }] ${ log.text() }` );
+			console.trace();
+		}
 	} );
 
 	page.on( 'pageerror', ( err ) => {
