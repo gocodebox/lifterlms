8a468cc3469d622f846ead61aea2c35c66cd4fa5
Brian Hogg <brian@bhconsulting.ca>
2025-09-04 10:35:38 -0400

    Adding fieldset and legend to choice questions.

--- templates/quiz/questions/content-choice.php
+++ templates/quiz/questions/content-choice.php
@@ -19,20 +19,27 @@ $question_answer = isset( $attempt ) && $attempt ? $attempt->get_question_answer
 ?>
 
 <ol class="llms-question-choices">
-	<?php foreach ( $question->get_choices() as $choice ) : ?>
-		<?php
-		$answer = is_array( $question_answer ) ? in_array( $choice->get( 'id' ), $question_answer, true ) ? $choice->get( 'id' ) : null : null;
-		?>
-		<li class="llms-choice type--text" id="choice-wrapper-<?php echo esc_attr( $choice->get( 'id' ) ); ?>">
-			<label for="choice-<?php echo esc_attr( $choice->get( 'id' ) ); ?>">
-				<input id="choice-<?php echo esc_attr( $choice->get( 'id' ) ); ?>" name="question_<?php echo esc_attr( $question->get( 'id' ) ); ?>[]" type="<?php echo esc_attr( $input_type ); ?>" value="<?php echo esc_attr( $choice->get( 'id' ) ); ?>" <?php checked( $answer, $choice->get( 'id' ) ); ?>>
-				<span class="llms-marker type--<?php echo esc_attr( $input_type ); ?>">
-					<span class="iterator"><?php echo esc_html( $choice->get( 'marker' ) ); ?></span>
-					<i class="fa fa-check"></i>
-				</span>
-				<p class="llms-choice-text"><?php echo esc_html( $choice->get( 'choice' ) ); ?></p>
-			</label>
-		</li>
+	<fieldset class="llms-fieldset" aria-describedby="question-<?php echo esc_attr( $question->get( 'id' ) ); ?>-description">
+		<legend class="sr-only">
+			<?php
+				echo esc_html( strip_tags( $question->get_question( 'html', $attempt ) ) );
+			?>
+		</legend>
+		<?php foreach ( $question->get_choices() as $choice ) : ?>
+			<?php
+			$answer = is_array( $question_answer ) ? in_array( $choice->get( 'id' ), $question_answer, true ) ? $choice->get( 'id' ) : null : null;
+			?>
+			<li class="llms-choice type--text" id="choice-wrapper-<?php echo esc_attr( $choice->get( 'id' ) ); ?>">
+				<label for="choice-<?php echo esc_attr( $choice->get( 'id' ) ); ?>">
+					<input id="choice-<?php echo esc_attr( $choice->get( 'id' ) ); ?>" name="question_<?php echo esc_attr( $question->get( 'id' ) ); ?>[]" type="<?php echo esc_attr( $input_type ); ?>" value="<?php echo esc_attr( $choice->get( 'id' ) ); ?>" <?php checked( $answer, $choice->get( 'id' ) ); ?>>
+					<span class="llms-marker type--<?php echo esc_attr( $input_type ); ?>">
+						<span class="iterator"><?php echo esc_html( $choice->get( 'marker' ) ); ?></span>
+						<i class="fa fa-check"></i>
+					</span>
+					<p class="llms-choice-text"><?php echo esc_html( $choice->get( 'choice' ) ); ?></p>
+				</label>
+			</li>
 
-	<?php endforeach; ?>
+		<?php endforeach; ?>
+	</fieldset>
 </ol>
