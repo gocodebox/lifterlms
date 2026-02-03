c110900b2df9cadbd552829b7367a2b480ed547f
Brian Hogg <brian@bhconsulting.ca>
2026-02-03 11:24:30 +0100

    Release v9.2.1 [ci skip]

---
+++ libraries/lifterlms-helper/includes/model-llms-helper-add-on.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Extends core class to allow interaction with the .com api
+ *
+ * @package LifterLMS_Helper/Classes
+ *
+ * @since 3.0.0
+ * @version 3.2.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Deprecated file.
+ *
+ * @deprecated 3.2.0 File `includes/model-llms-helper-add-on.php` is deprecated, use `includes/models/class-llms-helper-add-on.php` instead.
+ */
+_deprecated_file( __FILE__, '3.2.0', esc_html( LLMS_HELPER_PLUGIN_DIR ) . 'includes/models/class-llms-helper-add-on.php' );
