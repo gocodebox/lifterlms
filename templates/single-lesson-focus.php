6a909762bcbe2357f7420a3774934d2a214b15be
Brian Hogg <brian@bhconsulting.ca>
2026-03-10 15:02:59 +0100

    Move the arrows into the template, and use CSS to show/hide vs. injecting with javascript.

--- templates/single-lesson-focus.php
+++ templates/single-lesson-focus.php
@@ -56,6 +56,10 @@ $progress  = ( $student && $course_id ) ? $student->get_progress( $course_id, 'c
 				}
 				?>
 			</div>
+			<button class="llms-focus-mode-sidebar-toggle" type="button" aria-label="<?php esc_attr_e( 'Toggle sidebar', 'lifterlms' ); ?>">
+				<svg class="llms-chevron-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+				<svg class="llms-chevron-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+			</button>
 		</aside>
 
 		<main class="llms-focus-mode-content">
