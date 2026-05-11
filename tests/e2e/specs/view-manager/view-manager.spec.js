8903435f35f995381af3aefaf575cfddecd16104
Brian Hogg <brian@lifterlms.com>
2026-05-11 12:48:45 +0200

    Attempt at adding playwright tests to replace E2E (#3144)
    * Switching E2E tests to playwright.

---
+++ tests/e2e/specs/view-manager/view-manager.spec.js
@@ -0,0 +1,57 @@
+/**
+ * Test the LifterLMS View Manager
+ *
+ * @since [version]
+ */
+
+import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import { visitPage } from '../../utils/index.js';
+
+test.describe( 'ViewManager', () => {
+
+	test( 'should display the View Manager in the admin bar', async ( { admin, page } ) => {
+		await admin.visitAdminPage( '/' );
+		await visitPage( page, 'dashboard' );
+
+		const viewMenu = page.locator( '#wp-admin-bar-llms-view-as-menu' );
+		await expect( viewMenu ).toBeVisible();
+	} );
+
+	test( 'should switch to visitor view', async ( { admin, page } ) => {
+		await admin.visitAdminPage( '/' );
+		await visitPage( page, 'dashboard' );
+
+		const topLevel = page.locator( '#wp-admin-bar-llms-view-as-menu' );
+		await topLevel.hover();
+
+		const visitorLink = page.locator( '#wp-admin-bar-llms-view-as--visitor a.ab-item' );
+		await visitorLink.click();
+		await page.waitForLoadState( 'networkidle' );
+
+		await expect( topLevel ).toBeVisible();
+	} );
+
+	test( 'should switch to student view and back to self', async ( { admin, page } ) => {
+		await admin.visitAdminPage( '/' );
+		await visitPage( page, 'dashboard' );
+
+		const topLevel = page.locator( '#wp-admin-bar-llms-view-as-menu' );
+		await topLevel.hover();
+
+		const studentLink = page.locator( '#wp-admin-bar-llms-view-as--student a.ab-item' );
+		await studentLink.click();
+		await page.waitForLoadState( 'networkidle' );
+
+		await expect( topLevel ).toBeVisible();
+
+		const topLevel2 = page.locator( '#wp-admin-bar-llms-view-as-menu' );
+		await topLevel2.hover();
+
+		const selfLink = page.locator( '#wp-admin-bar-llms-view-as--self a.ab-item' );
+		await selfLink.click();
+		await page.waitForLoadState( 'networkidle' );
+
+		await expect( topLevel2 ).toBeVisible();
+	} );
+
+} );
