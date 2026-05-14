b01f5fdad870710b8f3f8820be1b2fc8b0da1e8e
Brian Hogg <brian@bhconsulting.ca>
2026-05-14 15:25:27 -0400

    Release v10.0.2 [ci skip]

---
+++ blocks/my-account/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'c14620cf7ca19b04cd21');
---
+++ blocks/pricing-table/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'c7e024a96795d4fa36c8');
