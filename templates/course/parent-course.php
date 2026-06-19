ce99a0a54911f8c3296527ef5e343a2258c7fa18
Faisal Ahammad <faisalahammad24@gmail.com>
2026-06-19 15:00:24 +0600

    Fix NoHtmlWrappedStrings i18n warning in parent-course template
    Move HTML tags outside translatable string in templates/course/parent-course.php to comply with WordPress i18n best practices. The 'Back to:' text is now translatable separately from the HTML wrapper.
    
    Fixes PHPCS warning: WordPress.WP.I18n.NoHtmlWrappedStrings
    
    Changelog: .changelogs/i18n-template-cleanup.yml

--- templates/course/parent-course.php
+++ templates/course/parent-course.php
@@ -6,7 +6,8 @@
  *
  * @since  1.0.0
  * @since 5.7.0 Replaced the call to the deprecated `LLMS_Lesson::get_parent_course()` method with `LLMS_Lesson::get( 'parent_course' )`.
- * @version 5.7.0
+ * @since 10.0.6 Moved HTML outside of translatable string for i18n compliance.
+ * @version 10.0.6
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -15,4 +16,11 @@ global $post;
 
 $lesson = new LLMS_Lesson( $post );
 
-echo wp_kses_post( sprintf( __( '<p class="llms-parent-course-link">Back to: <a class="llms-lesson-link" href="%1$s">%2$s</a></p>', 'lifterlms' ), get_permalink( $lesson->get( 'parent_course' ) ), get_the_title( $lesson->get( 'parent_course' ) ) ) );
+echo wp_kses_post(
+	sprintf(
+		'<p class="llms-parent-course-link">%1$s <a class="llms-lesson-link" href="%2$s">%3$s</a></p>',
+		esc_html__( 'Back to:', 'lifterlms' ),
+		get_permalink( $lesson->get( 'parent_course' ) ),
+		get_the_title( $lesson->get( 'parent_course' ) )
+	)
+);
