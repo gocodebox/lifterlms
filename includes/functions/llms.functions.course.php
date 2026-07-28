a95382d148a1af5bf22c56fa11de687ae3ada240
Usman <38263904+dotance@users.noreply.github.com>
2026-07-28 20:35:26 +0500

    Fix incorrect docblocks on get_course() and get_lesson() (#3276)
    The $the_course/$the_lesson and $args docblocks did not match the
    LLMS_Course and LLMS_Lesson constructors these functions forward to:
    
    - The parameter type omitted 'new' (string) and LLMS_Post_Model, both of
      which the constructors accept and handle.
    - $args was described as "Arguments to pass to the Constructor", which
      implies it always applies. LLMS_Post_Model::__construct() only uses
      $args when $model is 'new'.
    - get_lesson()'s $args description was indented one space too far, so it
      did not line up with the parameter above it.
    
    Wording is taken from the constructor docblocks so the wrapper and the
    thing it wraps now read the same.
    
    Fixes #2175

--- includes/functions/llms.functions.course.php
+++ includes/functions/llms.functions.course.php
@@ -16,8 +16,8 @@ defined( 'ABSPATH' ) || exit;
  * @since Unknown
  * @since 3.37.13 Use `LLMS_Course` in favor of the deprecated `LLMS_Course_Factory::get_course()` method.
  *
- * @param WP_Post|int|false $the_course Course post object or id. If `false` uses the global `$post` object.
- * @param array             $args       Arguments to pass to the LLMS_Course Constructor.
+ * @param string|int|LLMS_Post_Model|WP_Post|false $the_course 'new', WP post id, instance of an extending class, instance of WP_Post. If `false` uses the global `$post` object.
+ * @param array                                    $args       Args to create the post, only applies when `$the_course` is 'new'.
  * @return LLMS_Course
  */
 function get_course( $the_course = false, $args = array() ) {
@@ -37,8 +37,8 @@ function get_course( $the_course = false, $args = array() ) {
  * @since Unknown
  * @since 3.37.13 Use `LLMS_Lesson` in favor of the deprecated `LLMS_Course_Factory::get_lesson()` method.
  *
- * @param WP_Post|int|false $the_lesson Lesson post object or id. If `false` uses the global `$post` object.
- * @param array             $args        Arguments to pass to the LLMS_Lesson Constructor.
+ * @param string|int|LLMS_Post_Model|WP_Post|false $the_lesson 'new', WP post id, instance of an extending class, instance of WP_Post. If `false` uses the global `$post` object.
+ * @param array                                    $args       Args to create the post, only applies when `$the_lesson` is 'new'.
  * @return LLMS_Lesson
  */
 function get_lesson( $the_lesson = false, $args = array() ) {
