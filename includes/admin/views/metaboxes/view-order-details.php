486e345805344f92dea912ab9a0ff59387774b5d
Brian Hogg <brian@bhconsulting.ca>
2026-09-25 10:07:08 -0400

    Switch link back to edit user on order details page.

--- includes/admin/views/metaboxes/view-order-details.php
+++ includes/admin/views/metaboxes/view-order-details.php
@@ -267,8 +267,8 @@ $supports_modify_recurring_payments = $order->supports_modify_recurring_payments
 				<?php echo esc_html( $order->get_customer_name() ); ?>
 			<?php else : ?>
 				<?php
-				$customer_url = $order->get( 'user_id' ) ? llms_get_customers_admin_url( $order->get( 'user_id' ) ) : '';
-				echo ! $customer_url ? esc_html( $order->get_customer_name() ) : '<a href="' . esc_url( $customer_url ) . '">' . esc_html( $order->get_customer_name() ) . '</a>';
+				$edit_user_link = $order->get( 'user_id' ) ? get_edit_user_link( $order->get( 'user_id' ) ) : '';
+				echo ! $edit_user_link ? esc_html( $order->get_customer_name() ) . '<br>' : '<a href="' . esc_url( $edit_user_link ) . '">' . esc_html( $order->get_customer_name() ) . '</a>';
 				?>
 			<?php endif; ?>
 		</div>
